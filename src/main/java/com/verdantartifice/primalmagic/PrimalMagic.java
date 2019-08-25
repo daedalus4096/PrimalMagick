ca3e8f210ada245588abf2140184df25620c3a93
Michael Bunting <michael.a.bunting@gmail.com>
2019-08-24 23:36:36 -0700

    Initial debug command

--- src/main/java/com/verdantartifice/primalmagic/PrimalMagic.java
+++ src/main/java/com/verdantartifice/primalmagic/PrimalMagic.java
@@ -3,24 +3,34 @@ package com.verdantartifice.primalmagic;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.verdantartifice.primalmagic.common.command.PrimalMagicCommand;
 import com.verdantartifice.primalmagic.common.misc.ItemGroupPM;
 
 import net.minecraft.item.ItemGroup;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(PrimalMagic.MODID)
-@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
 public class PrimalMagic {
     public static final String MODID = "primalmagic";
     
     public static final Logger LOGGER = LogManager.getLogger(PrimalMagic.MODID);
     
     public static final ItemGroup ITEM_GROUP = new ItemGroupPM();
+    
+    public PrimalMagic() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::preInit);
+        MinecraftForge.EVENT_BUS.addListener(this::serverStarting);
+    }
 
-    @SubscribeEvent
-    public static void init(FMLCommonSetupEvent event) {
-        LOGGER.info("Hello from Primal Magic init!");
+    private void preInit(FMLCommonSetupEvent event) {
+        LOGGER.info("Hello from Primal Magic pre-init!");
+    }
+    
+    private void serverStarting(FMLServerStartingEvent event) {
+        PrimalMagicCommand.register(event.getCommandDispatcher());
     }
 }
---
+++ src/main/java/com/verdantartifice/primalmagic/common/command/PrimalMagicCommand.java
@@ -0,0 +1,60 @@
+package com.verdantartifice.primalmagic.common.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+
+import net.minecraft.command.CommandSource;
+import net.minecraft.command.Commands;
+import net.minecraft.command.arguments.EntityArgument;
+import net.minecraft.command.arguments.MessageArgument;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+
+public class PrimalMagicCommand {
+    public static void register(CommandDispatcher<CommandSource> dispatcher) {
+        LiteralCommandNode<CommandSource> node = dispatcher.register(Commands.literal("primalmagic")
+            .requires((source) -> { return source.hasPermissionLevel(2); })
+            .then(Commands.literal("help").executes((context) -> { return showHelp(context.getSource()); }))
+            .then(Commands.literal("research")
+                .then(Commands.argument("target", EntityArgument.player())
+                    .then(Commands.literal("list").executes((context) -> { return listResearch(context.getSource(), EntityArgument.getPlayer(context, "target")); }))
+                    .then(Commands.literal("reset").executes((context) -> { return resetResearch(context.getSource(), EntityArgument.getPlayer(context, "target")); }))
+                    .then(Commands.literal("grant")
+                        .then(Commands.argument("research", MessageArgument.message()).executes((context) -> { return grantResearch(context.getSource(), EntityArgument.getPlayer(context, "target"), MessageArgument.getMessage(context, "research")); }))
+                    )
+                )
+            )
+        );
+        dispatcher.register(Commands.literal("pm").requires((source) -> {
+            return source.hasPermissionLevel(2);
+        }).redirect(node));
+    }
+    
+    private static int showHelp(CommandSource source) {
+        source.sendFeedback(new TranslationTextComponent("commands.primalmagic.help.1").applyTextStyle(TextFormatting.DARK_GREEN), true);
+        source.sendFeedback(new TranslationTextComponent("commands.primalmagic.help.2").applyTextStyle(TextFormatting.DARK_GREEN), true);
+        source.sendFeedback(new TranslationTextComponent("commands.primalmagic.help.3"), true);
+        source.sendFeedback(new TranslationTextComponent("commands.primalmagic.help.4").applyTextStyle(TextFormatting.DARK_GREEN), true);
+        source.sendFeedback(new TranslationTextComponent("commands.primalmagic.help.5"), true);
+        source.sendFeedback(new TranslationTextComponent("commands.primalmagic.help.6").applyTextStyle(TextFormatting.DARK_GREEN), true);
+        source.sendFeedback(new TranslationTextComponent("commands.primalmagic.help.7"), true);
+        return 0;
+    }
+    
+    private static int listResearch(CommandSource source, ServerPlayerEntity target) {
+        source.sendFeedback(new TranslationTextComponent("commands.primalmagic.research.list", target.getName(), "DOOM"), true);
+        return 0;
+    }
+    
+    private static int resetResearch(CommandSource source, ServerPlayerEntity target) {
+        source.sendFeedback(new TranslationTextComponent("commands.primalmagic.research.reset", target.getName()), true);
+        return 0;
+    }
+    
+    private static int grantResearch(CommandSource source, ServerPlayerEntity target, ITextComponent research) {
+        source.sendFeedback(new TranslationTextComponent("commands.primalmagic.research.grant", target.getName(), research.getString()), true);
+        return 0;
+    }
+}
