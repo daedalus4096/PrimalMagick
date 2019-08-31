6594e23114b402ca9cb40d4b8059c948175d21a1
Michael Bunting <michael.a.bunting@gmail.com>
2019-08-31 16:31:39 -0700

    Sync knowledge when appropriate on living tick

--- src/main/java/com/verdantartifice/primalmagic/common/events/PlayerEvents.java
+++ src/main/java/com/verdantartifice/primalmagic/common/events/PlayerEvents.java
@@ -3,16 +3,36 @@ package com.verdantartifice.primalmagic.common.events;
 import com.verdantartifice.primalmagic.PrimalMagic;
 import com.verdantartifice.primalmagic.common.capabilities.IPlayerKnowledge;
 import com.verdantartifice.primalmagic.common.capabilities.PrimalMagicCapabilities;
+import com.verdantartifice.primalmagic.common.research.ResearchManager;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber
 public class PlayerEvents {
+    @SubscribeEvent
+    public static void livingTick(LivingEvent.LivingUpdateEvent event) {
+        if (event.getEntity() instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity)event.getEntity();
+            if (!player.world.isRemote) {
+                if (player.ticksExisted % 20 == 0) {
+                    if (ResearchManager.popSyncList(player.getName().getString()) != null) {
+                        IPlayerKnowledge knowledge = PrimalMagicCapabilities.getKnowledge(player);
+                        if (knowledge != null) {
+                            knowledge.sync((ServerPlayerEntity)player);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
     @SubscribeEvent
     public static void playerJoinEvent(EntityJoinWorldEvent event) {
         if (!event.getWorld().isRemote && (event.getEntity() instanceof ServerPlayerEntity)) {
