d3c84bb3937fab37ec5a628b18ffe585eeca687c
Michael Bunting <michael.a.bunting@gmail.com>
2019-09-02 19:19:16 -0700

    Added more test research

--- src/main/java/com/verdantartifice/primalmagic/common/init/InitResearch.java
+++ src/main/java/com/verdantartifice/primalmagic/common/init/InitResearch.java
@@ -1,8 +1,10 @@
 package com.verdantartifice.primalmagic.common.init;
 
 import com.verdantartifice.primalmagic.PrimalMagic;
+import com.verdantartifice.primalmagic.common.research.CompoundResearchKey;
 import com.verdantartifice.primalmagic.common.research.ResearchDisciplines;
 import com.verdantartifice.primalmagic.common.research.ResearchManager;
+import com.verdantartifice.primalmagic.common.research.SimpleResearchKey;
 
 import net.minecraft.util.ResourceLocation;
 
@@ -15,9 +17,13 @@ public class InitResearch {
     
     private static void initDisciplines() {
         ResearchDisciplines.registerDiscipline("BASICS", null);
+        ResearchDisciplines.registerDiscipline("ALCHEMY", CompoundResearchKey.from(SimpleResearchKey.parse("UNLOCK_ALCHEMY")));
+        ResearchDisciplines.registerDiscipline("SORCERY", CompoundResearchKey.from(SimpleResearchKey.parse("UNLOCK_SORCERY")));
     }
     
     private static void initResearchFiles() {
         ResearchDisciplines.registerResearchLocation(new ResourceLocation(PrimalMagic.MODID, "research/basics"));
+        ResearchDisciplines.registerResearchLocation(new ResourceLocation(PrimalMagic.MODID, "research/alchemy"));
+        ResearchDisciplines.registerResearchLocation(new ResourceLocation(PrimalMagic.MODID, "research/sorcery"));
     }
 }
