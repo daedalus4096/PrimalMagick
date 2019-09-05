02d39c9e28d05cf19a5bca27d560f5c42129ae0e
Michael Bunting <michael.a.bunting@gmail.com>
2019-09-04 20:23:02 -0700

    Refactor drawRect

--- src/main/java/com/verdantartifice/primalmagic/client/gui/grimoire/buttons/GrimoireEntryButton.java
+++ src/main/java/com/verdantartifice/primalmagic/client/gui/grimoire/buttons/GrimoireEntryButton.java
@@ -4,13 +4,11 @@ import java.awt.Color;
 
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.verdantartifice.primalmagic.client.gui.grimoire.GrimoireScreen;
+import com.verdantartifice.primalmagic.client.util.GuiUtils;
 import com.verdantartifice.primalmagic.common.research.ResearchEntry;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.widget.button.Button;
-import net.minecraft.client.renderer.BufferBuilder;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 
 public class GrimoireEntryButton extends Button {
     protected GrimoireScreen screen;
@@ -39,7 +37,7 @@ public class GrimoireEntryButton extends Button {
         if (this.isHovered()) {
             int alpha = 0x22;
             int color = (alpha << 24);
-            this.drawRect(this.x - 5, this.y, this.x + this.width + 5, this.y + this.height, color);
+            GuiUtils.drawRect(this.x - 5, this.y, this.x + this.width + 5, this.y + this.height, color);
         }
         int strWidth = mc.fontRenderer.getStringWidth(this.getMessage());
         int dy = (this.height - mc.fontRenderer.FONT_HEIGHT) / 2;
@@ -56,41 +54,6 @@ public class GrimoireEntryButton extends Button {
         GlStateManager.popMatrix();
     }
     
-    private void drawRect(int left, int top, int right, int bottom, int color) {
-        if (left < right)
-        {
-            int i = left;
-            left = right;
-            right = i;
-        }
-
-        if (top < bottom)
-        {
-            int j = top;
-            top = bottom;
-            bottom = j;
-        }
-
-        float f3 = (float)(color >> 24 & 255) / 255.0F;
-        float f = (float)(color >> 16 & 255) / 255.0F;
-        float f1 = (float)(color >> 8 & 255) / 255.0F;
-        float f2 = (float)(color & 255) / 255.0F;
-        Tessellator tessellator = Tessellator.getInstance();
-        BufferBuilder bufferbuilder = tessellator.getBuffer();
-        GlStateManager.enableBlend();
-        GlStateManager.disableTexture();
-        GlStateManager.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-        GlStateManager.color4f(f, f1, f2, f3);
-        bufferbuilder.begin(7, DefaultVertexFormats.POSITION);
-        bufferbuilder.pos((double)left, (double)bottom, 0.0D).endVertex();
-        bufferbuilder.pos((double)right, (double)bottom, 0.0D).endVertex();
-        bufferbuilder.pos((double)right, (double)top, 0.0D).endVertex();
-        bufferbuilder.pos((double)left, (double)top, 0.0D).endVertex();
-        tessellator.draw();
-        GlStateManager.enableTexture();
-        GlStateManager.disableBlend();
-    }
-
     private static class Handler implements IPressable {
         @Override
         public void onPress(Button button) {
---
+++ src/main/java/com/verdantartifice/primalmagic/client/util/GuiUtils.java
@@ -0,0 +1,42 @@
+package com.verdantartifice.primalmagic.client.util;
+
+import com.mojang.blaze3d.platform.GlStateManager;
+
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+
+public class GuiUtils {
+    public static void drawRect(int left, int top, int right, int bottom, int color) {
+        int temp;
+        if (left < right) {
+            temp = left;
+            left = right;
+            right = temp;
+        }
+        if (top < bottom) {
+            temp = top;
+            top = bottom;
+            bottom = temp;
+        }
+
+        float a = (float)(color >> 24 & 255) / 255.0F;
+        float r = (float)(color >> 16 & 255) / 255.0F;
+        float g = (float)(color >> 8 & 255) / 255.0F;
+        float b = (float)(color & 255) / 255.0F;
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder bufferbuilder = tessellator.getBuffer();
+        GlStateManager.enableBlend();
+        GlStateManager.disableTexture();
+        GlStateManager.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+        GlStateManager.color4f(r, g, b, a);
+        bufferbuilder.begin(7, DefaultVertexFormats.POSITION);
+        bufferbuilder.pos((double)left, (double)bottom, 0.0D).endVertex();
+        bufferbuilder.pos((double)right, (double)bottom, 0.0D).endVertex();
+        bufferbuilder.pos((double)right, (double)top, 0.0D).endVertex();
+        bufferbuilder.pos((double)left, (double)top, 0.0D).endVertex();
+        tessellator.draw();
+        GlStateManager.enableTexture();
+        GlStateManager.disableBlend();
+    }
+}
