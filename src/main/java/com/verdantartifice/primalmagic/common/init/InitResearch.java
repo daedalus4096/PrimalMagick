efe0ddf8bd145e4d644f333bec5efdf5190eb7d7
Michael Bunting <michael.a.bunting@gmail.com>
2019-09-08 15:56:33 -0700

    Rename enchantment to manaweaving for clarity

--- src/main/java/com/verdantartifice/primalmagic/common/init/InitResearch.java
+++ src/main/java/com/verdantartifice/primalmagic/common/init/InitResearch.java
@@ -19,7 +19,7 @@ public class InitResearch {
         ResearchDisciplines.registerDiscipline("BASICS", null);
         ResearchDisciplines.registerDiscipline("ALCHEMY", CompoundResearchKey.from(SimpleResearchKey.parse("UNLOCK_ALCHEMY")));
         ResearchDisciplines.registerDiscipline("SORCERY", CompoundResearchKey.from(SimpleResearchKey.parse("UNLOCK_SORCERY")));
-        ResearchDisciplines.registerDiscipline("ENCHANTMENT", CompoundResearchKey.from(SimpleResearchKey.parse("UNLOCK_ENCHANTMENT")));
+        ResearchDisciplines.registerDiscipline("MANAWEAVING", CompoundResearchKey.from(SimpleResearchKey.parse("UNLOCK_MANAWEAVING")));
         ResearchDisciplines.registerDiscipline("RUNEWORKING", CompoundResearchKey.from(SimpleResearchKey.parse("UNLOCK_RUNEWORKING")));
         ResearchDisciplines.registerDiscipline("RITUAL", CompoundResearchKey.from(SimpleResearchKey.parse("UNLOCK_RITUAL")));
         ResearchDisciplines.registerDiscipline("MAGITECH", CompoundResearchKey.from(SimpleResearchKey.parse("UNLOCK_MAGITECH")));
@@ -29,7 +29,7 @@ public class InitResearch {
         ResearchDisciplines.registerResearchLocation(new ResourceLocation(PrimalMagic.MODID, "research/basics"));
         ResearchDisciplines.registerResearchLocation(new ResourceLocation(PrimalMagic.MODID, "research/alchemy"));
         ResearchDisciplines.registerResearchLocation(new ResourceLocation(PrimalMagic.MODID, "research/sorcery"));
-        ResearchDisciplines.registerResearchLocation(new ResourceLocation(PrimalMagic.MODID, "research/enchantment"));
+        ResearchDisciplines.registerResearchLocation(new ResourceLocation(PrimalMagic.MODID, "research/manaweaving"));
         ResearchDisciplines.registerResearchLocation(new ResourceLocation(PrimalMagic.MODID, "research/runeworking"));
         ResearchDisciplines.registerResearchLocation(new ResourceLocation(PrimalMagic.MODID, "research/ritual"));
         ResearchDisciplines.registerResearchLocation(new ResourceLocation(PrimalMagic.MODID, "research/magitech"));
