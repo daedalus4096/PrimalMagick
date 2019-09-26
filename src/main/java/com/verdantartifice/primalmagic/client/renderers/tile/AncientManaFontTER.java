5a5a4832d3b35af573af12dbcf22eee8c1e42f14
Michael Bunting <michael.a.bunting@gmail.com>
2019-09-25 23:32:56 -0700

    Bind test texture to mana font TER

--- src/main/java/com/verdantartifice/primalmagic/client/renderers/tile/AncientManaFontTER.java
+++ src/main/java/com/verdantartifice/primalmagic/client/renderers/tile/AncientManaFontTER.java
@@ -13,11 +13,14 @@ import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class AncientManaFontTER extends TileEntityRenderer<AncientManaFontTileEntity> {
+    private static final ResourceLocation TEXTURE = new ResourceLocation("textures/entity/beacon_beam.png");
+    
     @Override
     public void render(AncientManaFontTileEntity tileEntityIn, double x, double y, double z, float partialTicks, int destroyStage) {
         super.render(tileEntityIn, x, y, z, partialTicks, destroyStage);
@@ -29,43 +32,45 @@ public class AncientManaFontTER extends TileEntityRenderer<AncientManaFontTileEn
             float b = sourceColor.getBlue() / 255.0F;
             double ds = 0.1875D;
             
+            this.bindTexture(TEXTURE);
+            
             GlStateManager.pushMatrix();
             GlStateManager.translated(x + 0.5D, y + 0.5D, z + 0.5D);
             GlStateManager.color4f(r, g, b, 1.0F);
 
             Tessellator tess = Tessellator.getInstance();
             BufferBuilder bb = tess.getBuffer();
-            bb.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION);
+            bb.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
             
-            bb.pos(-ds, ds, ds).endVertex();
-            bb.pos(-ds, -ds, ds).endVertex();
-            bb.pos(ds, -ds, ds).endVertex();
-            bb.pos(ds, ds, ds).endVertex();
+            bb.pos(-ds, ds, ds).tex(0.0D, 1.0D).endVertex();
+            bb.pos(-ds, -ds, ds).tex(0.0D, 0.0D).endVertex();
+            bb.pos(ds, -ds, ds).tex(1.0D, 0.0D).endVertex();
+            bb.pos(ds, ds, ds).tex(1.0D, 1.0D).endVertex();
             
-            bb.pos(-ds, ds, -ds).endVertex();
-            bb.pos(-ds, -ds, -ds).endVertex();
-            bb.pos(ds, -ds, -ds).endVertex();
-            bb.pos(ds, ds, -ds).endVertex();
+            bb.pos(-ds, ds, -ds).tex(0.0D, 1.0D).endVertex();
+            bb.pos(-ds, -ds, -ds).tex(0.0D, 0.0D).endVertex();
+            bb.pos(ds, -ds, -ds).tex(1.0D, 0.0D).endVertex();
+            bb.pos(ds, ds, -ds).tex(1.0D, 1.0D).endVertex();
             
-            bb.pos(ds, ds, -ds).endVertex();
-            bb.pos(ds, -ds, -ds).endVertex();
-            bb.pos(ds, -ds, ds).endVertex();
-            bb.pos(ds, ds, ds).endVertex();
+            bb.pos(ds, ds, -ds).tex(1.0D, 0.0D).endVertex();
+            bb.pos(ds, -ds, -ds).tex(0.0D, 0.0D).endVertex();
+            bb.pos(ds, -ds, ds).tex(0.0D, 1.0D).endVertex();
+            bb.pos(ds, ds, ds).tex(1.0D, 1.0D).endVertex();
             
-            bb.pos(-ds, -ds, ds).endVertex();
-            bb.pos(-ds, -ds, -ds).endVertex();
-            bb.pos(-ds, ds, -ds).endVertex();
-            bb.pos(-ds, ds, ds).endVertex();
+            bb.pos(-ds, -ds, ds).tex(0.0D, 1.0D).endVertex();
+            bb.pos(-ds, -ds, -ds).tex(0.0D, 0.0D).endVertex();
+            bb.pos(-ds, ds, -ds).tex(1.0D, 0.0D).endVertex();
+            bb.pos(-ds, ds, ds).tex(1.0D, 1.0D).endVertex();
             
-            bb.pos(ds, ds, -ds).endVertex();
-            bb.pos(ds, ds, ds).endVertex();
-            bb.pos(-ds, ds, ds).endVertex();
-            bb.pos(-ds, ds, -ds).endVertex();
+            bb.pos(ds, ds, -ds).tex(1.0D, 0.0D).endVertex();
+            bb.pos(ds, ds, ds).tex(1.0D, 1.0D).endVertex();
+            bb.pos(-ds, ds, ds).tex(0.0D, 1.0D).endVertex();
+            bb.pos(-ds, ds, -ds).tex(0.0D, 0.0D).endVertex();
             
-            bb.pos(ds, -ds, -ds).endVertex();
-            bb.pos(ds, -ds, ds).endVertex();
-            bb.pos(-ds, -ds, ds).endVertex();
-            bb.pos(-ds, -ds, -ds).endVertex();
+            bb.pos(ds, -ds, -ds).tex(1.0D, 0.0D).endVertex();
+            bb.pos(ds, -ds, ds).tex(1.0D, 1.0D).endVertex();
+            bb.pos(-ds, -ds, ds).tex(0.0D, 1.0D).endVertex();
+            bb.pos(-ds, -ds, -ds).tex(0.0D, 0.0D).endVertex();
             
             tess.draw();
             
