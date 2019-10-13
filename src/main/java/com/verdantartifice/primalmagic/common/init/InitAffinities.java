6e374645f976ec84db4165a3ef313fda84228045
Michael Bunting <michael.a.bunting@gmail.com>
2019-10-12 17:22:43 -0700

    Register test hidden affinity

--- src/main/java/com/verdantartifice/primalmagic/common/init/InitAffinities.java
+++ src/main/java/com/verdantartifice/primalmagic/common/init/InitAffinities.java
@@ -6,6 +6,7 @@ import com.verdantartifice.primalmagic.common.sources.SourceList;
 
 import net.minecraft.block.Blocks;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 
 public class InitAffinities {
     public static void initAffinities() {
@@ -15,5 +16,7 @@ public class InitAffinities {
     
     protected static void initItemAffinities() {
         AffinityManager.registerAffinities(new ItemStack(Blocks.MYCELIUM), new SourceList().add(Source.EARTH, 5).add(Source.MOON, 10));
+        
+        AffinityManager.registerAffinities(new ItemStack(Items.BONE), new SourceList().add(Source.MOON, 5).add(Source.BLOOD, 10));
     }
 }
