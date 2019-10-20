cc7f55fc760cfb2a4c509078bf83752877c3a867
Michael Bunting <michael.a.bunting@gmail.com>
2019-10-19 23:32:31 -0700

    Raise antennae for scannable stuff

--- src/main/java/com/verdantartifice/primalmagic/common/items/misc/ArcanometerItem.java
+++ src/main/java/com/verdantartifice/primalmagic/common/items/misc/ArcanometerItem.java
@@ -1,40 +1,80 @@
 package com.verdantartifice.primalmagic.common.items.misc;
 
 import com.verdantartifice.primalmagic.PrimalMagic;
+import com.verdantartifice.primalmagic.common.sources.AffinityManager;
 
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Rarity;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.EntityRayTraceResult;
+import net.minecraft.util.math.RayTraceContext;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class ArcanometerItem extends Item {
     public ArcanometerItem() {
         super(new Item.Properties().group(PrimalMagic.ITEM_GROUP).maxStackSize(1).rarity(Rarity.UNCOMMON));
         this.setRegistryName(PrimalMagic.MODID, "arcanometer");
-        this.addPropertyOverride(new ResourceLocation(PrimalMagic.MODID, "scan_state"), (stack, world, entity) -> {
-            if (entity == null) {
-                return 0.0F;
-            } else {
-                return (float)ArcanometerItem.this.getScanState(stack);
+        this.addPropertyOverride(new ResourceLocation(PrimalMagic.MODID, "scan_state"), new IItemPropertyGetter() {
+            @OnlyIn(Dist.CLIENT)
+            protected float scanState = 0;
+
+            @OnlyIn(Dist.CLIENT)
+            @Override
+            public float call(ItemStack stack, World world, LivingEntity entity) {
+                if (entity == null || !(entity instanceof PlayerEntity)) {
+                    return 0.0F;
+                } else {
+                    if (world == null) {
+                        world = entity.world;
+                    }
+                    PlayerEntity player = (PlayerEntity)entity;
+                    RayTraceResult result = rayTrace(world, player, RayTraceContext.FluidMode.ANY);
+                    if (result.getType() == RayTraceResult.Type.MISS) {
+                        this.decrementScanState();
+                    } else if (result.getType() == RayTraceResult.Type.ENTITY) {
+                        EntityRayTraceResult entityResult = (EntityRayTraceResult)result;
+                        if (entityResult.getEntity() instanceof ItemEntity) {
+                            ItemStack entityStack = ((ItemEntity)entityResult.getEntity()).getItem();
+                            if (AffinityManager.isScanned(entityStack, player)) {
+                                this.decrementScanState();
+                            } else {
+                                this.incrementScanState();
+                            }
+                        } else {
+                            // TODO Support scanning non-item entities
+                            this.decrementScanState();
+                        }
+                    } else {
+                        BlockRayTraceResult blockResult = (BlockRayTraceResult)result;
+                        ItemStack blockStack = new ItemStack(world.getBlockState(blockResult.getPos()).getBlock(), 1);
+                        if (AffinityManager.isScanned(blockStack, player)) {
+                            this.decrementScanState();
+                        } else {
+                            this.incrementScanState();
+                        }
+                    }
+                    return scanState;
+                }
+            }
+            
+            @OnlyIn(Dist.CLIENT)
+            protected void incrementScanState() {
+                this.scanState = Math.min(4.0F, this.scanState + 0.25F);
+            }
+            
+            @OnlyIn(Dist.CLIENT)
+            protected void decrementScanState() {
+                this.scanState = Math.max(0.0F, this.scanState - 0.25F);
             }
         });
-    }
-    
-    protected int getScanState(ItemStack stack) {
-        // TODO stub
-        return 0;
-    }
-    
-    @Override
-    public void inventoryTick(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
-        // TODO Auto-generated method stub
-        super.inventoryTick(stack, worldIn, entityIn, itemSlot, isSelected);
-    }
-    
-    @Override
-    public boolean shouldCauseReequipAnimation(ItemStack oldStack, ItemStack newStack, boolean slotChanged) {
-        return false;
     }
 }
