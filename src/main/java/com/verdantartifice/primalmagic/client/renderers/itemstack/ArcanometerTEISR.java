410218c08a45479b75a6be9c3d449de7413804ae
Michael Bunting <michael.a.bunting@gmail.com>
2019-10-21 21:29:40 -0700

    Show test block on arcanometer screen

--- src/main/java/com/verdantartifice/primalmagic/client/renderers/itemstack/ArcanometerTEISR.java
+++ src/main/java/com/verdantartifice/primalmagic/client/renderers/itemstack/ArcanometerTEISR.java
@@ -10,11 +10,16 @@ import net.minecraft.block.Blocks;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.client.renderer.model.IBakedModel;
+import net.minecraft.client.renderer.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.model.ModelResourceLocation;
 import net.minecraft.client.renderer.tileentity.ItemStackTileEntityRenderer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
+@SuppressWarnings("deprecation")
+@OnlyIn(Dist.CLIENT)
 public class ArcanometerTEISR extends ItemStackTileEntityRenderer {
     private static final ModelResourceLocation MRL = new ModelResourceLocation(new ResourceLocation(PrimalMagic.MODID, "arcanometer"), "");
     private static Method RENDER_MODEL_METHOD;
@@ -47,14 +52,12 @@ public class ArcanometerTEISR extends ItemStackTileEntityRenderer {
             // Render the screen display
             GlStateManager.pushLightingAttributes();
             GlStateManager.disableLighting();
-            
             GlStateManager.pushMatrix();
-            GlStateManager.translated(5.46875D, -25.40625D, 0.375D);
+            GlStateManager.translated(0.5D, 0.4375D, 0.405D);
             GlStateManager.rotated(180.0D, 0.0D, 0.0D, 1.0D);
-            GlStateManager.scaled(2.0D, 2.0D, 1D);
-            itemRenderer.renderItemAndEffectIntoGUI(new ItemStack(Blocks.GRASS_BLOCK), 0, 0);
+            GlStateManager.scaled(0.2D, -0.2D, 0.0001D);
+            itemRenderer.renderItem(new ItemStack(Blocks.GRASS_BLOCK), ItemCameraTransforms.TransformType.GUI);
             GlStateManager.popMatrix();
-            
             GlStateManager.enableLighting();
             GlStateManager.popAttributes();
         }
