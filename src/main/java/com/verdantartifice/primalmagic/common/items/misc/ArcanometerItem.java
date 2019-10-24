a83ef83beedc4dad0b9984e74fee3c7e3f86cc78
Michael Bunting <michael.a.bunting@gmail.com>
2019-10-23 20:55:29 -0700

    Detect placeable entities

--- src/main/java/com/verdantartifice/primalmagic/common/items/misc/ArcanometerItem.java
+++ src/main/java/com/verdantartifice/primalmagic/common/items/misc/ArcanometerItem.java
@@ -6,13 +6,16 @@ import com.verdantartifice.primalmagic.common.sources.AffinityManager;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.item.BoatEntity;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ProjectileHelper;
 import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.item.Rarity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.AxisAlignedBB;
@@ -64,20 +67,42 @@ public class ArcanometerItem extends Item {
     public static ItemStack getMouseOverItemStack() {
         ItemStack stack = ItemStack.EMPTY;
         Minecraft mc = Minecraft.getInstance();
-        Entity entity = mc.getRenderViewEntity();
+        Entity viewEntity = mc.getRenderViewEntity();
         double reachDistance = mc.playerController.extendedReach() ? 6.0D : (double)mc.playerController.getBlockReachDistance();
-        Vec3d eyePos = entity.getEyePosition(1.0F);
+        Vec3d eyePos = viewEntity.getEyePosition(1.0F);
         double sqReachDistance = (mc.objectMouseOver != null) ? mc.objectMouseOver.getHitVec().squareDistanceTo(eyePos) : (reachDistance * reachDistance);
-        Vec3d lookVector = entity.getLook(1.0F);
+        Vec3d lookVector = viewEntity.getLook(1.0F);
         Vec3d reachPos = eyePos.add(lookVector.scale(reachDistance));
-        AxisAlignedBB aabb = entity.getBoundingBox().expand(lookVector.scale(reachDistance)).grow(1.0D, 1.0D, 1.0D);
-        EntityRayTraceResult entityResult = ProjectileHelper.func_221273_a(entity, eyePos, reachPos, aabb, (testEntity) -> {
+        AxisAlignedBB aabb = viewEntity.getBoundingBox().expand(lookVector.scale(reachDistance)).grow(1.0D, 1.0D, 1.0D);
+        EntityRayTraceResult entityResult = ProjectileHelper.func_221269_a(mc.world, viewEntity, eyePos, reachPos, aabb, (testEntity) -> {
             return !testEntity.isSpectator();
         }, sqReachDistance);
         if (entityResult != null) {
-            // TODO Support scanning non-item entities
-            if (entityResult.getEntity() instanceof ItemEntity) {
-                stack = ((ItemEntity)entityResult.getEntity()).getItem();
+            Entity entity = entityResult.getEntity();
+            if (entity instanceof ItemEntity) {
+                stack = ((ItemEntity)entity).getItem();
+            } else if (entity instanceof BoatEntity) {
+                stack = new ItemStack(((BoatEntity)entity).getItemBoat());
+            } else if (entity.getType().equals(EntityType.ITEM_FRAME)) {
+                stack = new ItemStack(Items.ITEM_FRAME);
+            } else if (entity.getType().equals(EntityType.ARMOR_STAND)) {
+                stack = new ItemStack(Items.ARMOR_STAND);
+            } else if (entity.getType().equals(EntityType.MINECART)) {
+                stack = new ItemStack(Items.MINECART);
+            } else if (entity.getType().equals(EntityType.CHEST_MINECART)) {
+                stack = new ItemStack(Items.CHEST_MINECART);
+            } else if (entity.getType().equals(EntityType.FURNACE_MINECART)) {
+                stack = new ItemStack(Items.FURNACE_MINECART);
+            } else if (entity.getType().equals(EntityType.HOPPER_MINECART)) {
+                stack = new ItemStack(Items.HOPPER_MINECART);
+            } else if (entity.getType().equals(EntityType.TNT_MINECART)) {
+                stack = new ItemStack(Items.TNT_MINECART);
+            } else if (entity.getType().equals(EntityType.COMMAND_BLOCK_MINECART)) {
+                stack = new ItemStack(Items.COMMAND_BLOCK_MINECART);
+            } else if (entity.getType().equals(EntityType.END_CRYSTAL)) {
+                stack = new ItemStack(Items.END_CRYSTAL);
+            } else if (entity.getType().equals(EntityType.PAINTING)) {
+                stack = new ItemStack(Items.PAINTING);
             }
         } else if (mc.objectMouseOver != null && mc.objectMouseOver.getType() == RayTraceResult.Type.BLOCK) {
             BlockRayTraceResult blockResult = (BlockRayTraceResult)mc.objectMouseOver;
