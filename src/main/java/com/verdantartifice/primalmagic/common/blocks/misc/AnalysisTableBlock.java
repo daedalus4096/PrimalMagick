7ede9a9b2ca663b9e22e7c9dbb7330d3b8f80f7c
Michael Bunting <michael.a.bunting@gmail.com>
2019-10-28 22:59:29 -0700

    Give analysis table a horizontal facing

--- src/main/java/com/verdantartifice/primalmagic/common/blocks/misc/AnalysisTableBlock.java
+++ src/main/java/com/verdantartifice/primalmagic/common/blocks/misc/AnalysisTableBlock.java
@@ -4,8 +4,15 @@ import com.verdantartifice.primalmagic.PrimalMagic;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer.Builder;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
@@ -17,14 +24,37 @@ public class AnalysisTableBlock extends Block {
     protected static final VoxelShape PART_BASE = Block.makeCuboidShape(3.0D, 0.0D, 3.0D, 13.0D, 2.0D, 13.0D);
     protected static final VoxelShape PART_STAND = Block.makeCuboidShape(6.0D, 2.0D, 6.0D, 10.0D, 12.0D, 10.0D);
     protected static final VoxelShape SHAPE = VoxelShapes.or(PART_TOP, PART_BASE, PART_STAND);
+    
+    protected static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
 
     public AnalysisTableBlock() {
         super(Block.Properties.create(Material.WOOD).hardnessAndResistance(1.5F, 6.0F).sound(SoundType.WOOD));
         this.setRegistryName(PrimalMagic.MODID, "analysis_table");
+        this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH));
     }
     
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
         return SHAPE;
+    }
+    
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
+    }
+    
+    @Override
+    public BlockState rotate(BlockState state, Rotation rot) {
+        return state.with(FACING, rot.rotate(state.get(FACING)));
+    }
+    
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirrorIn) {
+        return state.rotate(mirrorIn.toRotation(state.get(FACING)));
+    }
+    
+    @Override
+    protected void fillStateContainer(Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
 }
