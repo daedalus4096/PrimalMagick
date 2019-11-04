067fc4ec29b2daeca884ed0d9b6a0965d8898230
Michael Bunting <michael.a.bunting@gmail.com>
2019-11-03 21:53:02 -0800

    Increase toughness of enchanted marble blocks

--- src/main/java/com/verdantartifice/primalmagic/common/init/InitBlocks.java
+++ src/main/java/com/verdantartifice/primalmagic/common/init/InitBlocks.java
@@ -30,7 +30,7 @@ public class InitBlocks {
         registry.register(new MarblePillarBlock());
         registry.register(new Block(Block.Properties.from(marbleRaw)).setRegistryName(PrimalMagic.MODID, "marble_chiseled"));
         registry.register(new Block(Block.Properties.from(marbleRaw)).setRegistryName(PrimalMagic.MODID, "marble_runed"));
-        registry.register(new Block(Block.Properties.create(Material.ROCK, MaterialColor.QUARTZ).hardnessAndResistance(1.5F, 6.0F)).setRegistryName(PrimalMagic.MODID, "marble_enchanted"));
+        registry.register(new Block(Block.Properties.create(Material.ROCK, MaterialColor.QUARTZ).hardnessAndResistance(3.0F, 12.0F).sound(SoundType.STONE)).setRegistryName(PrimalMagic.MODID, "marble_enchanted"));
         registry.register(new ArcaneWorkbenchBlock());
         registry.register(new AncientManaFontBlock(Source.EARTH));
         registry.register(new AncientManaFontBlock(Source.SEA));
