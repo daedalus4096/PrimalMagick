fb82486b4f397b658cb09f2de5388cf9b9cacdd8
Michael Bunting <michael.a.bunting@gmail.com>
2019-11-03 21:33:09 -0800

    Add runed marble block

--- src/main/java/com/verdantartifice/primalmagic/common/blocks/BlocksPM.java
+++ src/main/java/com/verdantartifice/primalmagic/common/blocks/BlocksPM.java
@@ -15,6 +15,7 @@ public class BlocksPM {
     public static final Block MARBLE_BRICK_STAIRS = null;
     public static final Block MARBLE_PILLAR = null;
     public static final Block MARBLE_CHISELED = null;
+    public static final Block MARBLE_RUNED = null;
     public static final Block MARBLE_ENCHANTED = null;
     public static final Block ARCANE_WORKBENCH = null;
     public static final Block ANCIENT_FONT_EARTH = null;
