859cdd682ad01bcab14c9ac0b67d02fdabc71620
Michael Bunting <michael.a.bunting@gmail.com>
2019-11-10 23:39:49 -0800

    Add sunwood sapling growth

--- src/main/java/com/verdantartifice/primalmagic/common/blocks/trees/SunwoodTree.java
+++ src/main/java/com/verdantartifice/primalmagic/common/blocks/trees/SunwoodTree.java
@@ -4,6 +4,8 @@ import java.util.Random;
 
 import javax.annotation.Nullable;
 
+import com.verdantartifice.primalmagic.common.worldgen.features.SunwoodTreeFeature;
+
 import net.minecraft.block.trees.Tree;
 import net.minecraft.world.gen.feature.AbstractTreeFeature;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
@@ -12,7 +14,6 @@ public class SunwoodTree extends Tree {
     @Override
     @Nullable
     protected AbstractTreeFeature<NoFeatureConfig> getTreeFeature(Random random) {
-        // TODO Auto-generated method stub
-        return null;
+        return new SunwoodTreeFeature(NoFeatureConfig::deserialize, true);
     }
 }
