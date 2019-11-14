a56b447406e803a12d1f913f3d2bf8524b5aa280
Michael Bunting <michael.a.bunting@gmail.com>
2019-11-13 23:02:18 -0800

    Spawn sunwood trees during world gen

--- src/main/java/com/verdantartifice/primalmagic/common/init/InitWorldGen.java
+++ src/main/java/com/verdantartifice/primalmagic/common/init/InitWorldGen.java
@@ -2,25 +2,36 @@ package com.verdantartifice.primalmagic.common.init;
 
 import javax.annotation.Nonnull;
 
+import com.verdantartifice.primalmagic.PrimalMagic;
 import com.verdantartifice.primalmagic.common.blocks.BlocksPM;
+import com.verdantartifice.primalmagic.common.worldgen.features.FeaturesPM;
+import com.verdantartifice.primalmagic.common.worldgen.features.SunwoodTreeFeature;
 
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biomes;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.IFeatureConfig;
+import net.minecraft.world.gen.feature.NoFeatureConfig;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.placement.AtSurfaceWithExtraConfig;
 import net.minecraft.world.gen.placement.CountRangeConfig;
 import net.minecraft.world.gen.placement.Placement;
+import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.IForgeRegistry;
 
 public class InitWorldGen {
-    public static void initWorldGen() {
-        initFeatures();
+    public static void initFeatures(IForgeRegistry<Feature<?>> registry) {
+        registry.register(new SunwoodTreeFeature(NoFeatureConfig::deserialize, false).setRegistryName(PrimalMagic.MODID, "sunwood_tree"));
     }
 
-    private static void initFeatures() {
+    public static void initWorldGen() {
         ForgeRegistries.BIOMES.getValues().stream().filter(InitWorldGen::shouldSpawnMarble).forEach((biome) -> {
             biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlocksPM.MARBLE_RAW.getDefaultState(), 33), Placement.COUNT_RANGE, new CountRangeConfig(10, 0, 0, 80)));
+        });
+        BiomeDictionary.getBiomes(BiomeDictionary.Type.FOREST).stream().filter(InitWorldGen::shouldSpawnSunwood).forEach((biome) -> {
+            biome.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Biome.createDecoratedFeature(FeaturesPM.SUNWOOD_TREE, IFeatureConfig.NO_FEATURE_CONFIG, Placement.COUNT_EXTRA_HEIGHTMAP, new AtSurfaceWithExtraConfig(0, 0.1F, 1)));
         });
     }
 
@@ -32,5 +43,9 @@ public class InitWorldGen {
             Biome.Category cat = biome.getCategory();
             return !Biome.Category.NONE.equals(cat) && !Biome.Category.NETHER.equals(cat) && !Biome.Category.THEEND.equals(cat);
         }
+    }
+    
+    private static boolean shouldSpawnSunwood(@Nonnull Biome biome) {
+        return !biome.equals(Biomes.FLOWER_FOREST);
     }
 }
---
+++ src/main/java/com/verdantartifice/primalmagic/common/worldgen/features/FeaturesPM.java
@@ -0,0 +1,12 @@
+package com.verdantartifice.primalmagic.common.worldgen.features;
+
+import com.verdantartifice.primalmagic.PrimalMagic;
+
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.NoFeatureConfig;
+import net.minecraftforge.registries.ObjectHolder;
+
+@ObjectHolder(PrimalMagic.MODID)
+public class FeaturesPM {
+    public static final Feature<NoFeatureConfig> SUNWOOD_TREE = null;
+}
