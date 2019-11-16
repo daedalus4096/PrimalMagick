8934ba20ab9a04e0de0433d075569494d3e9dc98
Michael Bunting <michael.a.bunting@gmail.com>
2019-11-15 20:17:07 -0800

    Remove BlockTilePM base class

--- src/main/java/com/verdantartifice/primalmagic/common/blocks/mana/AncientManaFontBlock.java
+++ src/main/java/com/verdantartifice/primalmagic/common/blocks/mana/AncientManaFontBlock.java
@@ -1,6 +1,6 @@
 package com.verdantartifice.primalmagic.common.blocks.mana;
 
-import com.verdantartifice.primalmagic.common.blocks.base.BlockTilePM;
+import com.verdantartifice.primalmagic.PrimalMagic;
 import com.verdantartifice.primalmagic.common.sources.Source;
 import com.verdantartifice.primalmagic.common.tiles.mana.AncientManaFontTileEntity;
 
@@ -8,13 +8,15 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 
-public class AncientManaFontBlock extends BlockTilePM<AncientManaFontTileEntity> {
+public class AncientManaFontBlock extends Block {
     protected static final VoxelShape PART_LOWER = Block.makeCuboidShape(2.0D, 0.0D, 2.0D, 14.0D, 2.0D, 14.0D);
     protected static final VoxelShape PART_UPPER = Block.makeCuboidShape(2.0D, 14.0D, 2.0D, 14.0D, 16.0D, 14.0D);
     protected static final VoxelShape SHAPE = VoxelShapes.or(PART_LOWER, PART_UPPER);
@@ -22,7 +24,8 @@ public class AncientManaFontBlock extends BlockTilePM<AncientManaFontTileEntity>
     protected Source source;
     
     public AncientManaFontBlock(Source source) {
-        super("ancient_font_" + source.getTag(), AncientManaFontTileEntity.class, Block.Properties.create(Material.ROCK).hardnessAndResistance(-1.0F, 3600000.0F).sound(SoundType.STONE).lightValue(15).noDrops());
+        super(Block.Properties.create(Material.ROCK).hardnessAndResistance(-1.0F, 3600000.0F).sound(SoundType.STONE).lightValue(15).noDrops());
+        this.setRegistryName(PrimalMagic.MODID, "ancient_font_" + source.getTag());
         this.source = source;
     }
     
@@ -33,5 +36,23 @@ public class AncientManaFontBlock extends BlockTilePM<AncientManaFontTileEntity>
     
     public Source getSource() {
         return this.source;
+    }
+    
+    @Override
+    public TileEntity createTileEntity(BlockState state, IBlockReader world) {
+        return new AncientManaFontTileEntity();
+    }
+    
+    @Override
+    public boolean hasTileEntity(BlockState state) {
+        return true;
+    }
+    
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean eventReceived(BlockState state, World worldIn, BlockPos pos, int id, int param) {
+        super.eventReceived(state, worldIn, pos, id, param);
+        TileEntity tile = worldIn.getTileEntity(pos);
+        return (tile == null) ? false : tile.receiveClientEvent(id, param);
     }
 }
