8a8c72ff9c9b32e3692121d074c69cd8503c8cc8
Michael Bunting <michael.a.bunting@gmail.com>
2019-11-23 20:53:32 -0800

    Display calcinator particles

--- src/main/java/com/verdantartifice/primalmagic/common/blocks/crafting/CalcinatorBlock.java
+++ src/main/java/com/verdantartifice/primalmagic/common/blocks/crafting/CalcinatorBlock.java
@@ -1,5 +1,7 @@
 package com.verdantartifice.primalmagic.common.blocks.crafting;
 
+import java.util.Random;
+
 import com.verdantartifice.primalmagic.PrimalMagic;
 import com.verdantartifice.primalmagic.common.tiles.crafting.CalcinatorTileEntity;
 
@@ -10,6 +12,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer.Builder;
@@ -19,6 +22,8 @@ import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Mirror;
 import net.minecraft.util.Rotation;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.shapes.ISelectionContext;
@@ -26,6 +31,8 @@ import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class CalcinatorBlock extends Block {
     public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
@@ -118,6 +125,35 @@ public class CalcinatorBlock extends Block {
                 worldIn.updateComparatorOutputLevel(pos, this);
             }
             super.onReplaced(state, worldIn, pos, newState, isMoving);
+        }
+    }
+    
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+        if (stateIn.get(LIT)) {
+            double d0 = (double)pos.getX() + 0.5D;
+            double d1 = (double)pos.getY();
+            double d2 = (double)pos.getZ() + 0.5D;
+            if (rand.nextDouble() < 0.1D) {
+                worldIn.playSound(d0, d1, d2, SoundEvents.BLOCK_FURNACE_FIRE_CRACKLE, SoundCategory.BLOCKS, 1.0F, 1.0F, false);
+            }
+
+            // Add smoke and flame at calcinator front
+            Direction direction = stateIn.get(FACING);
+            Direction.Axis axis = direction.getAxis();
+            double d3 = 0.5D;
+            double d4 = rand.nextDouble() * 0.6D - 0.3D;
+            double d5 = axis == Direction.Axis.X ? (double)direction.getXOffset() * d3 : d4;
+            double d6 = ((rand.nextDouble() * 3.0D) + 3.0D) / 16.0D;
+            double d7 = axis == Direction.Axis.Z ? (double)direction.getZOffset() * d3 : d4;
+            worldIn.addParticle(ParticleTypes.SMOKE, d0 + d5, d1 + d6, d2 + d7, 0.0D, 0.0D, 0.0D);
+            worldIn.addParticle(ParticleTypes.FLAME, d0 + d5, d1 + d6, d2 + d7, 0.0D, 0.0D, 0.0D);
+            
+            // Add smoke at calcinator smokestack
+            double d8 = rand.nextDouble() * 0.2D - 0.1D;
+            double d9 = rand.nextDouble() * 0.2D - 0.1D;
+            worldIn.addParticle(ParticleTypes.SMOKE, d0 + d8, d1 + 1.0D, d2 + d9, 0.0D, 0.0D, 0.0D);
         }
     }
 }
