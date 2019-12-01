e5f60d9bc4dcd2cc82e094f68437c6965349eac2
Michael Bunting <michael.a.bunting@gmail.com>
2019-11-30 23:17:55 -0800

    Allow touch spells to target blocks as well

--- src/main/java/com/verdantartifice/primalmagic/common/spells/TouchSpellPackage.java
+++ src/main/java/com/verdantartifice/primalmagic/common/spells/TouchSpellPackage.java
@@ -2,11 +2,15 @@ package com.verdantartifice.primalmagic.common.spells;
 
 import com.verdantartifice.primalmagic.PrimalMagic;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ProjectileHelper;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.EntityRayTraceResult;
+import net.minecraft.util.math.RayTraceContext;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
@@ -38,11 +42,21 @@ public class TouchSpellPackage extends AbstractSpellPackage {
         EntityRayTraceResult entityResult = ProjectileHelper.func_221269_a(world, caster, eyePos, reachPos, aabb, (testEntity) -> {
             return !testEntity.isSpectator();
         }, (reachDistance * reachDistance));
-        
-        if (entityResult == null || entityResult.getEntity() == null) {
-            PrimalMagic.LOGGER.info("No target found for spell {}", this.getName());
+        BlockRayTraceResult blockResult = world.rayTraceBlocks(new RayTraceContext(eyePos, reachPos, RayTraceContext.BlockMode.OUTLINE, RayTraceContext.FluidMode.SOURCE_ONLY, caster));
+        RayTraceResult result;
+        if (entityResult == null) {
+            result = blockResult;
         } else {
-            PrimalMagic.LOGGER.info("Casting spell {} on {}", this.getName(), entityResult.getEntity().getName().getFormattedText());
+            result = (eyePos.squareDistanceTo(entityResult.getHitVec()) <= eyePos.squareDistanceTo(blockResult.getHitVec())) ? entityResult : blockResult;
+        }
+        
+        if (result == null || result.getType() == RayTraceResult.Type.MISS) {
+            PrimalMagic.LOGGER.info("No target found for spell {}", this.getName());
+        } else if (result.getType() == RayTraceResult.Type.ENTITY) {
+            PrimalMagic.LOGGER.info("Casting spell {} on entity {}", this.getName(), entityResult.getEntity().getName().getFormattedText());
+        } else {
+            BlockState state = world.getBlockState(blockResult.getPos());
+            PrimalMagic.LOGGER.info("Casting spell {} on block {} at {}", this.getName(), state.getBlock().getRegistryName().toString(), blockResult.getPos().toString());
         }
     }
 }
