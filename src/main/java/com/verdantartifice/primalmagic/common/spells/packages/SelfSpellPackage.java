d6c305d706b7d5508050f4344856cc05e6b9ff9f
Michael Bunting <michael.a.bunting@gmail.com>
2019-12-13 17:17:54 -0800

    Add particle cloud on self-targeted spells

--- src/main/java/com/verdantartifice/primalmagic/common/spells/packages/SelfSpellPackage.java
+++ src/main/java/com/verdantartifice/primalmagic/common/spells/packages/SelfSpellPackage.java
@@ -1,8 +1,12 @@
 package com.verdantartifice.primalmagic.common.spells.packages;
 
+import com.verdantartifice.primalmagic.common.network.PacketHandler;
+import com.verdantartifice.primalmagic.common.network.packets.fx.SpellImpactPacket;
+
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.EntityRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class SelfSpellPackage extends AbstractSpellPackage {
@@ -26,6 +30,14 @@ public class SelfSpellPackage extends AbstractSpellPackage {
         super.cast(world, caster);
         if (this.payload != null) {
             RayTraceResult result = new EntityRayTraceResult(caster);
+            if (!world.isRemote) {
+                Vec3d hitVec = caster.getEyePosition(1.0F);
+                PacketHandler.sendToAllAround(
+                        new SpellImpactPacket(hitVec.x, hitVec.y, hitVec.z, this.payload.getSource().getColor()), 
+                        world.getDimension().getType(), 
+                        caster.getPosition(), 
+                        64.0D);
+            }
             this.payload.execute(result, this, world, caster);
         }
     }
