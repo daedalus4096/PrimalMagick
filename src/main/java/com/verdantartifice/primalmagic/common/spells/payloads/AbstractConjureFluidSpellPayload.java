4f5bf4b97c9db5cb80b8918c68979c6bd367a524
Michael Bunting <michael.a.bunting@gmail.com>
2020-01-02 21:32:30 -0800

    Finish conjure water spell payload

---
+++ src/main/java/com/verdantartifice/primalmagic/common/spells/payloads/AbstractConjureFluidSpellPayload.java
@@ -0,0 +1,85 @@
+package com.verdantartifice.primalmagic.common.spells.payloads;
+
+import javax.annotation.Nonnull;
+
+import com.verdantartifice.primalmagic.common.spells.SpellPackage;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ILiquidContainer;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.FlowingFluid;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+
+public abstract class AbstractConjureFluidSpellPayload extends AbstractSpellPayload {
+    protected final FlowingFluid fluid;
+    
+    public AbstractConjureFluidSpellPayload(FlowingFluid fluid) {
+        super();
+        this.fluid = fluid;
+    }
+    
+    @Override
+    public void execute(RayTraceResult target, Vec3d burstPoint, SpellPackage spell, World world, LivingEntity caster) {
+        if (target != null && target.getType() == RayTraceResult.Type.BLOCK && caster instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity)caster;
+            BlockRayTraceResult blockTarget = (BlockRayTraceResult)target;
+            BlockPos targetPos = blockTarget.getPos();
+            ItemStack stack = this.getSimulatedItemStack(this.fluid);
+            if (world.isBlockModifiable(player, targetPos) && player.canPlayerEdit(targetPos, blockTarget.getFace(), stack)) {
+                BlockState state = world.getBlockState(targetPos);
+                BlockPos placePos = (state.getBlock() instanceof ILiquidContainer && this.fluid == Fluids.WATER) ? targetPos : targetPos.offset(blockTarget.getFace());
+                this.placeFluid(player, world, placePos, blockTarget);
+            }
+        }
+    }
+
+    protected ItemStack getSimulatedItemStack(@Nonnull Fluid fluid) {
+        if (fluid.isIn(FluidTags.WATER)) {
+            return new ItemStack(Items.WATER_BUCKET);
+        } else if (fluid.isIn(FluidTags.LAVA)) {
+            return new ItemStack(Items.LAVA_BUCKET);
+        } else {
+            return ItemStack.EMPTY;
+        }
+    }
+
+    protected void placeFluid(PlayerEntity player, World world, BlockPos pos, BlockRayTraceResult blockTarget) {
+        BlockState state = world.getBlockState(pos);
+        Block block = state.getBlock();
+        Material material = state.getMaterial();
+        boolean isSolid = material.isSolid();
+        boolean isReplaceable = material.isReplaceable();
+        if (world.isAirBlock(pos) || !isSolid || isReplaceable || (block instanceof ILiquidContainer && ((ILiquidContainer)block).canContainFluid(world, pos, state, this.fluid))) {
+            if (world.dimension.doesWaterVaporize() && this.fluid.isIn(FluidTags.WATER)) {
+                world.playSound(player, pos, SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.BLOCKS, 0.5F, 2.6F + (world.rand.nextFloat() - world.rand.nextFloat()) * 0.8F);
+                for (int index = 0; index < 8; index++) {
+                    world.addParticle(ParticleTypes.LARGE_SMOKE, pos.getX() + world.rand.nextDouble(), pos.getY() + world.rand.nextDouble(), pos.getZ() + world.rand.nextDouble(), 0.0F, 0.0F, 0.0F);
+                }
+            } else if (block instanceof ILiquidContainer && this.fluid == Fluids.WATER) {
+                ((ILiquidContainer)block).receiveFluid(world, pos, state, this.fluid.getStillFluidState(false));
+            } else {
+                if (!world.isRemote && (!isSolid || isReplaceable) && !material.isLiquid()) {
+                    world.destroyBlock(pos, true);
+                }
+                world.setBlockState(pos, this.fluid.getDefaultState().getBlockState(), 0xB);
+            }
+        } else if (blockTarget != null) {
+            this.placeFluid(player, world, blockTarget.getPos().offset(blockTarget.getFace()), null);
+        }
+    }
+}
--- src/main/java/com/verdantartifice/primalmagic/common/spells/payloads/ConjureWaterSpellPayload.java
+++ src/main/java/com/verdantartifice/primalmagic/common/spells/payloads/ConjureWaterSpellPayload.java
@@ -4,32 +4,24 @@ import com.verdantartifice.primalmagic.common.research.CompoundResearchKey;
 import com.verdantartifice.primalmagic.common.research.SimpleResearchKey;
 import com.verdantartifice.primalmagic.common.sources.Source;
 import com.verdantartifice.primalmagic.common.sources.SourceList;
-import com.verdantartifice.primalmagic.common.spells.SpellPackage;
 
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.RayTraceResult;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
-public class ConjureWaterSpellPayload extends AbstractSpellPayload {
+public class ConjureWaterSpellPayload extends AbstractConjureFluidSpellPayload {
     public static final String TYPE = "conjure_water";
     protected static final CompoundResearchKey RESEARCH = CompoundResearchKey.from(SimpleResearchKey.parse("SPELL_PAYLOAD_CONJURE_WATER"));
+    protected static final int MANA_COST = 10;
 
     public ConjureWaterSpellPayload() {
-        super();
+        super(Fluids.WATER);
     }
     
     public static CompoundResearchKey getResearch() {
         return RESEARCH;
-    }
-
-    @Override
-    public void execute(RayTraceResult target, Vec3d burstPoint, SpellPackage spell, World world, LivingEntity caster) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
@@ -39,7 +31,7 @@ public class ConjureWaterSpellPayload extends AbstractSpellPayload {
 
     @Override
     public SourceList getManaCost() {
-        return new SourceList().add(this.getSource(), 10);
+        return new SourceList().add(this.getSource(), MANA_COST);
     }
 
     @Override
