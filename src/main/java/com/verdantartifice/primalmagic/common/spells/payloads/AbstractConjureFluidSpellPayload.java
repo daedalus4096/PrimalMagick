05653eb285890f536e545b07c45709fc49272a58
Michael Bunting <michael.a.bunting@gmail.com>
2020-01-05 16:36:31 -0800

    Fix fluid conjuration to work on entity targets

--- src/main/java/com/verdantartifice/primalmagic/common/spells/payloads/AbstractConjureFluidSpellPayload.java
+++ src/main/java/com/verdantartifice/primalmagic/common/spells/payloads/AbstractConjureFluidSpellPayload.java
@@ -15,8 +15,10 @@ import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.tags.FluidTags;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.EntityRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
@@ -31,14 +33,28 @@ public abstract class AbstractConjureFluidSpellPayload extends AbstractSpellPayl
     
     @Override
     public void execute(RayTraceResult target, Vec3d burstPoint, SpellPackage spell, World world, PlayerEntity caster) {
-        if (target != null && target.getType() == RayTraceResult.Type.BLOCK) {
-            BlockRayTraceResult blockTarget = (BlockRayTraceResult)target;
-            BlockPos targetPos = blockTarget.getPos();
+        if (target != null) {
             ItemStack stack = this.getSimulatedItemStack(this.fluid);
-            if (world.isBlockModifiable(caster, targetPos) && caster.canPlayerEdit(targetPos, blockTarget.getFace(), stack)) {
-                BlockState state = world.getBlockState(targetPos);
-                BlockPos placePos = (state.getBlock() instanceof ILiquidContainer && this.fluid == Fluids.WATER) ? targetPos : targetPos.offset(blockTarget.getFace());
-                this.placeFluid(caster, world, placePos, blockTarget);
+            if (target.getType() == RayTraceResult.Type.BLOCK) {
+                BlockRayTraceResult blockTarget = (BlockRayTraceResult)target;
+                BlockPos targetPos = blockTarget.getPos();
+                if (world.isBlockModifiable(caster, targetPos) && caster.canPlayerEdit(targetPos, blockTarget.getFace(), stack)) {
+                    BlockState state = world.getBlockState(targetPos);
+                    BlockPos placePos = (state.getBlock() instanceof ILiquidContainer && this.fluid == Fluids.WATER) ? targetPos : targetPos.offset(blockTarget.getFace());
+                    this.placeFluid(caster, world, placePos, blockTarget);
+                }
+            } else if (target.getType() == RayTraceResult.Type.ENTITY) {
+                EntityRayTraceResult entityTarget = (EntityRayTraceResult)target;
+                BlockPos targetPos = new BlockPos(entityTarget.getHitVec());
+                Vec3d entityVec = entityTarget.getEntity().getPositionVec();
+                BlockPos entityPos = new BlockPos(entityVec);
+                Vec3d targetVec = new Vec3d(targetPos.getX() + 0.5D, targetPos.getY() + 0.5D, targetPos.getZ() + 0.5D);
+                Vec3d dirVec = entityVec.subtract(targetVec);
+                Direction dir = Direction.getFacingFromVector(dirVec.x, dirVec.y, dirVec.z);
+                BlockRayTraceResult blockTarget = new BlockRayTraceResult(entityTarget.getHitVec(), dir, entityPos, false);
+                if (world.isBlockModifiable(caster, entityPos) && caster.canPlayerEdit(entityPos, dir, stack)) {
+                    this.placeFluid(caster, world, entityPos, blockTarget);
+                }
             }
         }
     }
