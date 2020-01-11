0afb8d9110cde1f6da38507872cd971550fe3a4b
Michael Bunting <michael.a.bunting@gmail.com>
2020-01-10 22:34:50 -0800

    Made entity swappers NBT serializable

--- src/main/java/com/verdantartifice/primalmagic/common/misc/EntitySwapper.java
+++ src/main/java/com/verdantartifice/primalmagic/common/misc/EntitySwapper.java
@@ -11,7 +11,6 @@ import java.util.concurrent.LinkedBlockingQueue;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-import com.verdantartifice.primalmagic.PrimalMagic;
 import com.verdantartifice.primalmagic.common.effects.EffectsPM;
 import com.verdantartifice.primalmagic.common.network.PacketHandler;
 import com.verdantartifice.primalmagic.common.network.packets.fx.WandPoofPacket;
@@ -21,6 +20,7 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec2f;
@@ -28,14 +28,17 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.common.util.INBTSerializable;
 
-public class EntitySwapper {
+public class EntitySwapper implements INBTSerializable<CompoundNBT> {
     protected static final Map<Integer, Queue<EntitySwapper>> REGISTRY = new HashMap<>();
     
-    protected UUID targetId;
-    protected EntityType<?> entityType;
-    protected Optional<Integer> polymorphDuration;
-    protected int delay;
+    protected UUID targetId = null;
+    protected EntityType<?> entityType = null;
+    protected Optional<Integer> polymorphDuration = Optional.empty();
+    protected int delay = 0;
+    
+    protected EntitySwapper() {}
     
     public EntitySwapper(@Nonnull UUID targetId, @Nonnull EntityType<?> entityType, @Nonnull Optional<Integer> polymorphDuration, int delay) {
         this.targetId = targetId;
@@ -70,11 +73,10 @@ public class EntitySwapper {
     
     @Nullable
     public EntitySwapper execute(@Nonnull World world) {
-        PrimalMagic.LOGGER.info("Doing entity swap");
         if (!world.isRemote && world instanceof ServerWorld) {
             ServerWorld serverWorld = (ServerWorld)world;
             Entity target = serverWorld.getEntityByUuid(this.targetId);
-            if (target != null && this.isValidTarget(target)) {
+            if (this.isValid() && target != null && this.isValidTarget(target)) {
                 if (target.isPassenger()) {
                     target.stopRiding();
                 }
@@ -119,5 +121,40 @@ public class EntitySwapper {
     
     public boolean isReady() {
         return (delay <= 0);
+    }
+
+    @Override
+    public CompoundNBT serializeNBT() {
+        CompoundNBT nbt = new CompoundNBT();
+        if (this.targetId != null) {
+            nbt.putUniqueId("TargetId", this.targetId);
+        }
+        if (this.entityType != null) {
+            nbt.putString("EntityType", EntityType.getKey(this.entityType).toString());
+        }
+        if (this.polymorphDuration != null && this.polymorphDuration.isPresent()) {
+            nbt.putInt("PolymorphDuration", this.polymorphDuration.get().intValue());
+        }
+        nbt.putInt("Delay", this.delay);
+        return nbt;
+    }
+
+    @Override
+    public void deserializeNBT(CompoundNBT nbt) {
+        EntitySwapper retVal = new EntitySwapper();
+        if (nbt.contains("TargetId")) {
+            retVal.targetId = nbt.getUniqueId("TargetId");
+        }
+        if (nbt.contains("EntityType")) {
+            retVal.entityType = EntityType.byKey(nbt.getString("EntityType")).orElse(null);
+        }
+        retVal.polymorphDuration = nbt.contains("PolymorphDuration") ? 
+                Optional.of(Integer.valueOf(nbt.getInt("PolymorphDuration"))) : 
+                Optional.empty();
+        retVal.delay = nbt.getInt("Delay");
+    }
+    
+    protected boolean isValid() {
+        return this.targetId != null && this.entityType != null;
     }
 }
