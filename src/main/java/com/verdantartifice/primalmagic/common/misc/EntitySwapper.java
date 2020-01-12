50e89c1d81b6756f13106bf8f5a4c19e46f54f28
Michael Bunting <michael.a.bunting@gmail.com>
2020-01-11 17:10:45 -0800

    Store entity swappers in world capability

--- src/main/java/com/verdantartifice/primalmagic/common/misc/EntitySwapper.java
+++ src/main/java/com/verdantartifice/primalmagic/common/misc/EntitySwapper.java
@@ -1,16 +1,15 @@
 package com.verdantartifice.primalmagic.common.misc;
 
 import java.awt.Color;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Queue;
 import java.util.UUID;
-import java.util.concurrent.LinkedBlockingQueue;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+import com.verdantartifice.primalmagic.common.capabilities.IWorldEntitySwappers;
+import com.verdantartifice.primalmagic.common.capabilities.PrimalMagicCapabilities;
 import com.verdantartifice.primalmagic.common.effects.EffectsPM;
 import com.verdantartifice.primalmagic.common.network.PacketHandler;
 import com.verdantartifice.primalmagic.common.network.packets.fx.WandPoofPacket;
@@ -31,8 +30,6 @@ import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.util.INBTSerializable;
 
 public class EntitySwapper implements INBTSerializable<CompoundNBT> {
-    protected static final Map<Integer, Queue<EntitySwapper>> REGISTRY = new HashMap<>();
-    
     protected UUID targetId = null;
     protected EntityType<?> entityType = null;
     protected Optional<Integer> polymorphDuration = Optional.empty();
@@ -47,28 +44,41 @@ public class EntitySwapper implements INBTSerializable<CompoundNBT> {
         this.delay = delay;
     }
     
+    public EntitySwapper(CompoundNBT tag) {
+        this();
+        this.deserializeNBT(tag);
+    }
+    
     public static boolean enqueue(@Nonnull World world, @Nullable EntitySwapper swapper) {
         if (swapper == null) {
             return false;
         } else {
-            return getWorldSwappers(world).offer(swapper);
+            IWorldEntitySwappers swappers = PrimalMagicCapabilities.getEntitySwappers(world);
+            if (swappers == null) {
+                return false;
+            } else {
+                return swappers.enqueue(swapper);
+            }
         }
     }
     
-    @Nonnull
+    @Nullable
     public static Queue<EntitySwapper> getWorldSwappers(@Nonnull World world) {
-        int dim = world.getDimension().getType().getId();
-        Queue<EntitySwapper> swapperQueue = REGISTRY.get(Integer.valueOf(dim));
-        if (swapperQueue == null) {
-            swapperQueue = new LinkedBlockingQueue<>();
-            REGISTRY.put(Integer.valueOf(dim), swapperQueue);
+        IWorldEntitySwappers swappers = PrimalMagicCapabilities.getEntitySwappers(world);
+        if (swappers == null) {
+            return null;
+        } else {
+            return swappers.getQueue();
         }
-        return swapperQueue;
     }
     
-    public static void setWorldSwapperQueue(@Nonnull World world, @Nonnull Queue<EntitySwapper> swapperQueue) {
-        int dim = world.getDimension().getType().getId();
-        REGISTRY.put(Integer.valueOf(dim), swapperQueue);
+    public static boolean setWorldSwapperQueue(@Nonnull World world, @Nonnull Queue<EntitySwapper> swapperQueue) {
+        IWorldEntitySwappers swappers = PrimalMagicCapabilities.getEntitySwappers(world);
+        if (swappers == null) {
+            return false;
+        } else {
+            return swappers.setQueue(swapperQueue);
+        }
     }
     
     @Nullable
@@ -141,20 +151,19 @@ public class EntitySwapper implements INBTSerializable<CompoundNBT> {
 
     @Override
     public void deserializeNBT(CompoundNBT nbt) {
-        EntitySwapper retVal = new EntitySwapper();
-        if (nbt.contains("TargetId")) {
-            retVal.targetId = nbt.getUniqueId("TargetId");
+        if (nbt.hasUniqueId("TargetId")) {
+            this.targetId = nbt.getUniqueId("TargetId");
         }
         if (nbt.contains("EntityType")) {
-            retVal.entityType = EntityType.byKey(nbt.getString("EntityType")).orElse(null);
+            this.entityType = EntityType.byKey(nbt.getString("EntityType")).orElse(null);
         }
-        retVal.polymorphDuration = nbt.contains("PolymorphDuration") ? 
+        this.polymorphDuration = nbt.contains("PolymorphDuration") ? 
                 Optional.of(Integer.valueOf(nbt.getInt("PolymorphDuration"))) : 
                 Optional.empty();
-        retVal.delay = nbt.getInt("Delay");
+        this.delay = nbt.getInt("Delay");
     }
     
-    protected boolean isValid() {
+    public boolean isValid() {
         return this.targetId != null && this.entityType != null;
     }
 }
