b5977c298b3f65c24e556fdaee358aa46ee06a2c
Michael Bunting <michael.a.bunting@gmail.com>
2020-02-02 21:30:13 -0800

    Add mana costs for arcane recipes to grimoire pages

--- src/main/java/com/verdantartifice/primalmagic/client/gui/grimoire/pages/ShapedArcaneRecipePage.java
+++ src/main/java/com/verdantartifice/primalmagic/client/gui/grimoire/pages/ShapedArcaneRecipePage.java
@@ -1,6 +1,7 @@
 package com.verdantartifice.primalmagic.client.gui.grimoire.pages;
 
 import com.verdantartifice.primalmagic.client.gui.grimoire.GrimoireScreen;
+import com.verdantartifice.primalmagic.client.gui.grimoire.widgets.ManaCostSummaryWidget;
 import com.verdantartifice.primalmagic.common.crafting.ShapedArcaneRecipe;
 
 import net.minecraftforge.api.distmarker.Dist;
@@ -13,8 +14,6 @@ import net.minecraftforge.api.distmarker.OnlyIn;
  */
 @OnlyIn(Dist.CLIENT)
 public class ShapedArcaneRecipePage extends AbstractShapedRecipePage<ShapedArcaneRecipe> {
-    protected ShapedArcaneRecipe recipe;
-    
     public ShapedArcaneRecipePage(ShapedArcaneRecipe recipe) {
         super(recipe);
     }
@@ -26,7 +25,12 @@ public class ShapedArcaneRecipePage extends AbstractShapedRecipePage<ShapedArcan
     
     @Override
     public void initWidgets(GrimoireScreen screen, int side, int x, int y) {
+        // Add base page widgets
         super.initWidgets(screen, side, x, y);
-        // TODO Render mana cost widgets
+        
+        // Add mana cost summary widget
+        int indent = 124;
+        int overlayWidth = 52;
+        screen.addWidgetToScreen(new ManaCostSummaryWidget(this.recipe.getManaCosts(), x + 75 + (side * 140) + (indent / 2) - (overlayWidth / 2), y + 30));
     }
 }
--- src/main/java/com/verdantartifice/primalmagic/client/gui/grimoire/pages/ShapelessArcaneRecipePage.java
+++ src/main/java/com/verdantartifice/primalmagic/client/gui/grimoire/pages/ShapelessArcaneRecipePage.java
@@ -1,6 +1,7 @@
 package com.verdantartifice.primalmagic.client.gui.grimoire.pages;
 
 import com.verdantartifice.primalmagic.client.gui.grimoire.GrimoireScreen;
+import com.verdantartifice.primalmagic.client.gui.grimoire.widgets.ManaCostSummaryWidget;
 import com.verdantartifice.primalmagic.common.crafting.ShapelessArcaneRecipe;
 
 import net.minecraftforge.api.distmarker.Dist;
@@ -24,7 +25,12 @@ public class ShapelessArcaneRecipePage extends AbstractShapelessRecipePage<Shape
     
     @Override
     public void initWidgets(GrimoireScreen screen, int side, int x, int y) {
+        // Add base page widgets
         super.initWidgets(screen, side, x, y);
-        // TODO Render mana cost widgets
+        
+        // Add mana cost summary widget
+        int indent = 124;
+        int overlayWidth = 52;
+        screen.addWidgetToScreen(new ManaCostSummaryWidget(this.recipe.getManaCosts(), x + 75 + (side * 140) + (indent / 2) - (overlayWidth / 2), y + 30));
     }
 }
