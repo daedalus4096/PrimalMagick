51647a11dd3b2978fd0f5a733122ca4433ae42ad
Michael Bunting <michael.a.bunting@gmail.com>
2020-02-11 22:02:37 -0800

    Remove affinities from mundane wands

--- src/main/java/com/verdantartifice/primalmagic/common/init/InitAffinities.java
+++ src/main/java/com/verdantartifice/primalmagic/common/init/InitAffinities.java
@@ -317,6 +317,7 @@ public class InitAffinities {
         
         // Register Primal Magic item affinities
         AffinityManager.registerAffinities(new ItemStack(ItemsPM.GRIMOIRE), AffinityManager.getAffinities(new ItemStack(Items.ENCHANTED_BOOK), server));
+        AffinityManager.registerAffinities(new ItemStack(ItemsPM.MUNDANE_WAND), new SourceList());  // Don't wand mundane wands to have any affinities
         AffinityManager.registerAffinities(new ItemStack(ItemsPM.BLOODY_FLESH), new SourceList().add(Source.BLOOD, 20));
         AffinityManager.registerAffinities(new ItemStack(ItemsPM.HALLOWED_ORB), new SourceList().add(Source.HALLOWED, 20));
 
