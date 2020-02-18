2788782d9264bd72bf0430ec6872d48dc04edb67
Michael Bunting <michael.a.bunting@gmail.com>
2020-02-17 21:49:58 -0800

    Render attunement progress meter

--- src/main/java/com/verdantartifice/primalmagic/client/gui/grimoire/pages/AttunementPage.java
+++ src/main/java/com/verdantartifice/primalmagic/client/gui/grimoire/pages/AttunementPage.java
@@ -1,14 +1,23 @@
 package com.verdantartifice.primalmagic.client.gui.grimoire.pages;
 
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import javax.annotation.Nonnull;
 
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.verdantartifice.primalmagic.PrimalMagic;
 import com.verdantartifice.primalmagic.client.gui.grimoire.GrimoireScreen;
+import com.verdantartifice.primalmagic.common.attunements.AttunementManager;
+import com.verdantartifice.primalmagic.common.attunements.AttunementType;
 import com.verdantartifice.primalmagic.common.sources.Source;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -19,6 +28,8 @@ import net.minecraftforge.api.distmarker.OnlyIn;
  */
 @OnlyIn(Dist.CLIENT)
 public class AttunementPage extends AbstractPage {
+    private static final ResourceLocation TEXTURE = new ResourceLocation(PrimalMagic.MODID, "textures/gui/attunement_meter.png");
+
     protected Source source;
     protected List<IPageElement> contents = new ArrayList<>();
     protected boolean firstPage;
@@ -55,6 +66,42 @@ public class AttunementPage extends AbstractPage {
             y += 25;
         }
         
+        if (this.isFirstPage()) {
+            Color baseColor = new Color(this.source.getColor());
+            Color color;
+            
+            // Render attunement meter
+            RenderSystem.enableBlend();
+            RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+            Minecraft mc = Minecraft.getInstance();
+            mc.getTextureManager().bindTexture(TEXTURE);
+            
+            // Render meter background
+            this.blit(x + 51 + (side * 140), y, 12, 0, 14, 120);
+            
+            int p = AttunementManager.getAttunement(mc.player, this.source, AttunementType.PERMANENT);
+            int i = AttunementManager.getAttunement(mc.player, this.source, AttunementType.INDUCED);
+            int t = AttunementManager.getAttunement(mc.player, this.source, AttunementType.TEMPORARY);
+
+            // Render permanent meter bar
+            color = baseColor.darker();
+            RenderSystem.color4f(color.getRed() / 255.0F, color.getGreen() / 255.0F, color.getBlue() / 255.0F, 1.0F);
+            this.blit(x + 53 + (side * 140), y + 10 + (100 - MathHelper.clamp(p, 0, 100)), 0, 10, 10, MathHelper.clamp(p, 0, 100));
+            
+            // Render induced meter bar
+            color = baseColor;
+            RenderSystem.color4f(color.getRed() / 255.0F, color.getGreen() / 255.0F, color.getBlue() / 255.0F, 1.0F);
+            this.blit(x + 53 + (side * 140), y + 10 + (100 - MathHelper.clamp(p + i, 0, 100)), 0, 10, 10, MathHelper.clamp(i, 0, 100 - p));
+            
+            // Render temporary meter bar
+            color = baseColor.brighter();
+            RenderSystem.color4f(color.getRed() / 255.0F, color.getGreen() / 255.0F, color.getBlue() / 255.0F, 1.0F);
+            this.blit(x + 53 + (side * 140), y + 10 + (100 - MathHelper.clamp(p + i + t, 0, 100)), 0, 10, 10, MathHelper.clamp(t, 0, 100 - p - i));
+
+            // Render meter foreground
+            this.blit(x + 52 + (side * 140), y + 9, 27, 9, 15, 102);
+        }
+
         // Render page contents
         for (IPageElement content : this.contents) {
             content.render(side, x, y);
@@ -68,10 +115,5 @@ public class AttunementPage extends AbstractPage {
     }
 
     @Override
-    public void initWidgets(GrimoireScreen screen, int side, int x, int y) {
-        if (this.isFirstPage()) {
-            // TODO Render attunement meter
-        }
-    }
-
+    public void initWidgets(GrimoireScreen screen, int side, int x, int y) {}
 }
