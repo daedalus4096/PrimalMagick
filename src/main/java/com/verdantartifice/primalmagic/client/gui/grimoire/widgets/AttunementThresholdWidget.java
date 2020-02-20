5719ff94658f17597c084e1753160d3471b844a8
Michael Bunting <michael.a.bunting@gmail.com>
2020-02-19 20:21:20 -0800

    Finish attunement threshold widgets

--- src/main/java/com/verdantartifice/primalmagic/client/gui/grimoire/widgets/AttunementThresholdWidget.java
+++ src/main/java/com/verdantartifice/primalmagic/client/gui/grimoire/widgets/AttunementThresholdWidget.java
@@ -1,6 +1,6 @@
 package com.verdantartifice.primalmagic.client.gui.grimoire.widgets;
 
-import java.util.Arrays;
+import java.util.Collections;
 
 import javax.annotation.Nonnull;
 
@@ -8,10 +8,15 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.verdantartifice.primalmagic.PrimalMagic;
 import com.verdantartifice.primalmagic.client.util.GuiUtils;
 import com.verdantartifice.primalmagic.common.attunements.AttunementThreshold;
+import com.verdantartifice.primalmagic.common.items.ItemsPM;
 import com.verdantartifice.primalmagic.common.sources.Source;
+import com.verdantartifice.primalmagic.common.wands.WandCap;
+import com.verdantartifice.primalmagic.common.wands.WandCore;
+import com.verdantartifice.primalmagic.common.wands.WandGem;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.widget.Widget;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -25,22 +30,26 @@ import net.minecraftforge.api.distmarker.OnlyIn;
  */
 @OnlyIn(Dist.CLIENT)
 public class AttunementThresholdWidget extends Widget {
+    protected static final ItemStack WAND_STACK;
+    
     protected final Source source;
     protected final AttunementThreshold threshold;
     protected final ResourceLocation texture;
-    protected final ITextComponent tooltipHeaderText;
-    protected final ITextComponent tooltipTextText;
+    protected final ITextComponent tooltipText;
+    
+    static {
+        WAND_STACK = new ItemStack(ItemsPM.MODULAR_WAND.get());
+        ItemsPM.MODULAR_WAND.get().setWandCore(WAND_STACK, WandCore.HEARTWOOD);
+        ItemsPM.MODULAR_WAND.get().setWandCap(WAND_STACK, WandCap.IRON);
+        ItemsPM.MODULAR_WAND.get().setWandGem(WAND_STACK, WandGem.APPRENTICE);
+    }
     
     public AttunementThresholdWidget(@Nonnull Source source, @Nonnull AttunementThreshold threshold, int x, int y) {
         super(x, y, 16, 16, "");
         this.source = source;
         this.threshold = threshold;
         this.texture = new ResourceLocation(PrimalMagic.MODID, "textures/attunements/threshold_" + source.getTag() + "_" + threshold.getTag() + ".png");
-        
-        ITextComponent thresholdText = new TranslationTextComponent(this.threshold.getTranslationKey());
-        ITextComponent sourceText = new TranslationTextComponent(this.source.getNameTranslationKey()).applyTextStyle(this.source.getChatColor());
-        this.tooltipHeaderText = new TranslationTextComponent("primalmagic.attunement.threshold.header", thresholdText, sourceText);
-        this.tooltipTextText = new TranslationTextComponent("primalmagic.attunement.threshold." + source.getTag() + "." + threshold.getTag());
+        this.tooltipText = new TranslationTextComponent("primalmagic.attunement.threshold." + source.getTag() + "." + threshold.getTag());
     }
     
     @Override
@@ -50,7 +59,8 @@ public class AttunementThresholdWidget extends Widget {
         RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
         
         if (this.threshold == AttunementThreshold.MINOR) {
-            // TODO Render casting wand into GUI
+            // Render casting wand into GUI
+            mc.getItemRenderer().renderItemIntoGUI(WAND_STACK, this.x, this.y);
         } else {
             // Render the icon appropriate for this widget's source and threshold
             RenderSystem.pushMatrix();
@@ -63,7 +73,7 @@ public class AttunementThresholdWidget extends Widget {
         
         if (this.isHovered()) {
             // Render tooltip
-            GuiUtils.renderCustomTooltip(Arrays.asList(this.tooltipHeaderText, this.tooltipTextText), this.x, this.y);
+            GuiUtils.renderCustomTooltip(Collections.singletonList(this.tooltipText), this.x, this.y);
         }
     }
     
