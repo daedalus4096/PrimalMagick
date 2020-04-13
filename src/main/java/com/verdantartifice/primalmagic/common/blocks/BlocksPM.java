bf6cad8782c8803d8a902895fd7323b2a6ef85a1
Michael Bunting <michael.a.bunting@gmail.com>
2020-04-12 21:49:51 -0700

    Base code for skyglass

--- src/main/java/com/verdantartifice/primalmagic/common/blocks/BlocksPM.java
+++ src/main/java/com/verdantartifice/primalmagic/common/blocks/BlocksPM.java
@@ -14,6 +14,7 @@ import com.verdantartifice.primalmagic.common.blocks.misc.ConsecrationFieldBlock
 import com.verdantartifice.primalmagic.common.blocks.misc.GlowFieldBlock;
 import com.verdantartifice.primalmagic.common.blocks.misc.PillarBlock;
 import com.verdantartifice.primalmagic.common.blocks.misc.ResearchTableBlock;
+import com.verdantartifice.primalmagic.common.blocks.misc.SkyglassBlock;
 import com.verdantartifice.primalmagic.common.blocks.misc.WoodTableBlock;
 import com.verdantartifice.primalmagic.common.blocks.ores.RockSaltOreBlock;
 import com.verdantartifice.primalmagic.common.blocks.rituals.RitualAltarBlock;
@@ -152,4 +153,5 @@ public class BlocksPM {
     public static final RegistryObject<GlowFieldBlock> GLOW_FIELD = BLOCKS.register("glow_field", GlowFieldBlock::new);
     public static final RegistryObject<SaltTrailBlock> SALT_TRAIL = BLOCKS.register("salt_trail", SaltTrailBlock::new);
     public static final RegistryObject<RockSaltOreBlock> ROCK_SALT_ORE = BLOCKS.register("rock_salt_ore", RockSaltOreBlock::new);
+    public static final RegistryObject<SkyglassBlock> SKYGLASS = BLOCKS.register("skyglass", () -> new SkyglassBlock(Block.Properties.create(Material.GLASS).hardnessAndResistance(0.3F).sound(SoundType.GLASS).notSolid()));
 }
---
+++ src/main/java/com/verdantartifice/primalmagic/common/blocks/misc/SkyglassBlock.java
@@ -0,0 +1,65 @@
+package com.verdantartifice.primalmagic.common.blocks.misc;
+
+import net.minecraft.block.AbstractGlassBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer.Builder;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
+
+/**
+ * Block definition for skyglass.  Skyglass is completely transparent except at the edge, connecting
+ * to other adjacent skyglass blocks.  It also is not destroyed upon harvesting.
+ * 
+ * @author Daedalus4096
+ */
+public class SkyglassBlock extends AbstractGlassBlock {
+    public static final BooleanProperty UP = BooleanProperty.create("up");
+    public static final BooleanProperty DOWN = BooleanProperty.create("down");
+    public static final BooleanProperty NORTH = BooleanProperty.create("north");
+    public static final BooleanProperty SOUTH = BooleanProperty.create("south");
+    public static final BooleanProperty WEST = BooleanProperty.create("west");
+    public static final BooleanProperty EAST = BooleanProperty.create("east");
+    
+    public SkyglassBlock(Block.Properties properties) {
+        super(properties);
+        this.setDefaultState(this.getDefaultState().with(UP, Boolean.FALSE).with(DOWN, Boolean.FALSE).with(NORTH, Boolean.FALSE).with(SOUTH, Boolean.FALSE).with(WEST, Boolean.FALSE).with(EAST, Boolean.FALSE));
+    }
+    
+    @Override
+    protected void fillStateContainer(Builder<Block, BlockState> builder) {
+        builder.add(UP, DOWN, NORTH, SOUTH, WEST, EAST);
+    }
+    
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        // Determine the block's connections when it is placed into the world
+        IWorld world = context.getWorld();
+        BlockPos pos = context.getPos();
+        return this.getCurrentState(world.getBlockState(pos), world, pos);
+    }
+    
+    @Override
+    public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
+        // Determine the block's connections when one of its neighbors is updated
+        return this.getCurrentState(stateIn, worldIn, currentPos);
+    }
+    
+    protected BlockState getCurrentState(BlockState state, IWorld world, BlockPos pos) {
+        return this.getDefaultState()
+                .with(UP, this.isSideConnected(state, world, pos, Direction.UP))
+                .with(DOWN, this.isSideConnected(state, world, pos, Direction.DOWN))
+                .with(NORTH, this.isSideConnected(state, world, pos, Direction.NORTH))
+                .with(SOUTH, this.isSideConnected(state, world, pos, Direction.SOUTH))
+                .with(WEST, this.isSideConnected(state, world, pos, Direction.WEST))
+                .with(EAST, this.isSideConnected(state, world, pos, Direction.EAST));
+    }
+    
+    protected boolean isSideConnected(BlockState state, IWorld world, BlockPos pos, Direction dir) {
+        BlockState adjacent = world.getBlockState(pos.offset(dir));
+        return adjacent != null && state.getBlock() == adjacent.getBlock();
+    }
+}
