d35b17c9f56f0e776b108a8818bace713faa2afd
Michael Bunting <michael.a.bunting@gmail.com>
2020-05-06 22:14:51 -0700

    Glow field generation for sunlamps

--- src/main/java/com/verdantartifice/primalmagic/common/tiles/devices/SunlampTileEntity.java
+++ src/main/java/com/verdantartifice/primalmagic/common/tiles/devices/SunlampTileEntity.java
@@ -1,10 +1,16 @@
 package com.verdantartifice.primalmagic.common.tiles.devices;
 
 import com.verdantartifice.primalmagic.PrimalMagic;
+import com.verdantartifice.primalmagic.common.blocks.BlocksPM;
 import com.verdantartifice.primalmagic.common.tiles.TileEntityTypesPM;
 import com.verdantartifice.primalmagic.common.tiles.base.TilePM;
+import com.verdantartifice.primalmagic.common.util.RayTraceUtils;
 
 import net.minecraft.tileentity.ITickableTileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.LightType;
+import net.minecraft.world.gen.Heightmap;
+import net.minecraftforge.common.util.Constants;
 
 /**
  * Definition of a sunlamp tile entity.  Periodically attempts to spawn glow fields in dark air
@@ -23,9 +29,29 @@ public class SunlampTileEntity extends TilePM implements ITickableTileEntity {
     public void tick() {
         this.ticksExisted++;
         if (!this.world.isRemote && this.ticksExisted % 5 == 0) {
-            // TODO pick a random location within 15 blocks
-            // TODO if location is ordinary air and dark enough and in line-of-sight, spawn a glow field there
+            // Pick a random location within 15 blocks
+            int x = this.world.rand.nextInt(16) - this.world.rand.nextInt(16);
+            int y = this.world.rand.nextInt(16) - this.world.rand.nextInt(16);
+            int z = this.world.rand.nextInt(16) - this.world.rand.nextInt(16);
+            BlockPos bp = this.pos.add(x, y, z);
+            
+            // Constrain the selected block pos
+            BlockPos rainHeight = this.world.getHeight(Heightmap.Type.MOTION_BLOCKING, bp);
+            if (bp.getY() > rainHeight.getY() + 4) {
+                bp = rainHeight.up(4);
+            }
+            if (bp.getY() < 1) {
+                bp = new BlockPos(bp.getX(), 1, bp.getZ());
+            }
+
+            // If location is ordinary air and dark enough and in line-of-sight, spawn a glow field there
             PrimalMagic.LOGGER.debug("Trying to spawn glow field");
+            if ( this.world.isAirBlock(bp) &&
+                 this.world.getBlockState(bp) != BlocksPM.GLOW_FIELD.get().getDefaultState() &&
+                 this.world.getLightFor(LightType.BLOCK, bp) < 11 &&
+                 RayTraceUtils.hasLineOfSight(this.world, this.pos, bp) ) {
+                this.world.setBlockState(bp, BlocksPM.GLOW_FIELD.get().getDefaultState(), Constants.BlockFlags.DEFAULT);
+            }
         }
     }
 }
--- src/main/java/com/verdantartifice/primalmagic/common/util/RayTraceUtils.java
+++ src/main/java/com/verdantartifice/primalmagic/common/util/RayTraceUtils.java
@@ -8,6 +8,8 @@ import java.util.function.Predicate;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+import com.verdantartifice.primalmagic.PrimalMagic;
+
 import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
@@ -155,10 +157,13 @@ public class RayTraceUtils {
         BlockRayTraceResult result = RayTraceUtils.rayTraceBlocksIgnoringSource(context);
 
         if (result == null) {
+            PrimalMagic.LOGGER.debug("LOS check from {} to {} got null result!", source, target);
             return true;
         } else if (result.getType() == RayTraceResult.Type.BLOCK) {
+            PrimalMagic.LOGGER.debug("LOS check from {} to {} found hit at {}", source, target, result.getPos());
             return target.equals(result.getPos());
         } else {
+            PrimalMagic.LOGGER.debug("LOS check from {} to {} got non-block result!", source, target);
             return false;
         }
     }
