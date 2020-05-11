5252bbcbf207e3a22095396508ff575073df5c71
Michael Bunting <michael.a.bunting@gmail.com>
2020-05-10 20:04:57 -0700

    Solar damage spells make the target glow

--- src/main/java/com/verdantartifice/primalmagic/common/spells/payloads/SolarDamageSpellPayload.java
+++ src/main/java/com/verdantartifice/primalmagic/common/spells/payloads/SolarDamageSpellPayload.java
@@ -11,6 +11,8 @@ import com.verdantartifice.primalmagic.common.spells.SpellProperty;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.EntityRayTraceResult;
@@ -74,8 +76,9 @@ public class SolarDamageSpellPayload extends AbstractDamageSpellPayload {
         if (target != null && target.getType() == RayTraceResult.Type.ENTITY && duration > 0) {
             EntityRayTraceResult entityTarget = (EntityRayTraceResult)target;
             if (entityTarget.getEntity() != null && entityTarget.getEntity() instanceof LivingEntity) {
-                // Set the entity on fire if undead
+                // Make the entity glow and set it on fire if undead
                 LivingEntity entity = (LivingEntity)entityTarget.getEntity();
+                entity.addPotionEffect(new EffectInstance(Effects.GLOWING, 20 * duration, 0));
                 if (entity.isEntityUndead()) {
                     entity.setFire(duration);
                 }
