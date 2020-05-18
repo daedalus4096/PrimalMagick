5219638ef3ad6b5d62a4e97617d81beded3cee39
Michael Bunting <michael.a.bunting@gmail.com>
2020-05-17 17:19:41 -0700

    Add ritual recipe builder for datagen

---
+++ src/main/java/com/verdantartifice/primalmagic/datagen/RitualRecipeBuilder.java
@@ -0,0 +1,369 @@
+package com.verdantartifice.primalmagic.datagen;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.verdantartifice.primalmagic.common.crafting.BlockIngredient;
+import com.verdantartifice.primalmagic.common.crafting.RecipeSerializersPM;
+import com.verdantartifice.primalmagic.common.research.CompoundResearchKey;
+import com.verdantartifice.primalmagic.common.sources.Source;
+import com.verdantartifice.primalmagic.common.sources.SourceList;
+
+import net.minecraft.block.Block;
+import net.minecraft.data.IFinishedRecipe;
+import net.minecraft.item.Item;
+import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.tags.Tag;
+import net.minecraft.util.IItemProvider;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.registries.ForgeRegistries;
+
+/**
+ * Definition of a recipe data file builder for ritual recipes.
+ * 
+ * @author Daedalus4096
+ */
+public class RitualRecipeBuilder {
+    protected final Item result;
+    protected final int count;
+    protected final List<Ingredient> ingredients = new ArrayList<>();
+    protected final List<BlockIngredient> props = new ArrayList<>();
+    protected String group;
+    protected CompoundResearchKey research;
+    protected SourceList manaCosts;
+
+    protected RitualRecipeBuilder(IItemProvider result, int count) {
+        this.result = result.asItem();
+        this.count = count;
+    }
+    
+    /**
+     * Creates a new builder for a ritual recipe.
+     * 
+     * @param result the output item type
+     * @param count the output item quantity
+     * @return a new builder for a ritual recipe
+     */
+    public static RitualRecipeBuilder ritualRecipe(IItemProvider result, int count) {
+        return new RitualRecipeBuilder(result, count);
+    }
+    
+    /**
+     * Creates a new builder for a ritual recipe.
+     * 
+     * @param result the output item type
+     * @return a new builder for a ritual recipe
+     */
+    public static RitualRecipeBuilder ritualRecipe(IItemProvider result) {
+        return ritualRecipe(result, 1);
+    }
+    
+    /**
+     * Add an ingredient to the recipe multiple times.
+     * 
+     * @param ingredient the ingredient to be added
+     * @param quantity the number of the ingredient to add
+     * @return the modified builder
+     */
+    public RitualRecipeBuilder addIngredient(Ingredient ingredient, int quantity) {
+        for (int index = 0; index < quantity; index++) {
+            this.ingredients.add(ingredient);
+        }
+        return this;
+    }
+    
+    /**
+     * Add an ingredient to the recipe.
+     * 
+     * @param ingredient the ingredient to be added
+     * @return the modified builder
+     */
+    public RitualRecipeBuilder addIngredient(Ingredient ingredient) {
+        return this.addIngredient(ingredient, 1);
+    }
+    
+    /**
+     * Add an ingredient of the given item to the recipe multiple times.
+     * 
+     * @param item the item to be added
+     * @param quantity the number of the item to add
+     * @return the modified builder
+     */
+    public RitualRecipeBuilder addIngredient(IItemProvider item, int quantity) {
+        return this.addIngredient(Ingredient.fromItems(item), quantity);
+    }
+    
+    /**
+     * Add an ingredient of the given item to the recipe.
+     * 
+     * @param item the item to be added
+     * @return the modified builder
+     */
+    public RitualRecipeBuilder addIngredient(IItemProvider item) {
+        return this.addIngredient(item, 1);
+    }
+    
+    /**
+     * Add an ingredient to the recipe multiple times that can be any item in the given tag.
+     * 
+     * @param tag the tag of items to be added
+     * @param quantity the number of the tag to add
+     * @return the modified builder
+     */
+    public RitualRecipeBuilder addIngredient(Tag<Item> tag, int quantity) {
+        return this.addIngredient(Ingredient.fromTag(tag), quantity);
+    }
+    
+    /**
+     * Add an ingredient to the recipe that can be any item in the given tag.
+     * 
+     * @param tag the tag of items to be added
+     * @return the modified builder
+     */
+    public RitualRecipeBuilder addIngredient(Tag<Item> tag) {
+        return this.addIngredient(tag, 1);
+    }
+    
+    /**
+     * Add a prop ingredient to the recipe multiple times.
+     * 
+     * @param ingredient the prop ingredient to be added
+     * @param quantity the number of the prop ingredient to add
+     * @return the modified builder
+     */
+    public RitualRecipeBuilder addProp(BlockIngredient ingredient, int quantity) {
+        for (int index = 0; index < quantity; index++) {
+            this.props.add(ingredient);
+        }
+        return this;
+    }
+    
+    /**
+     * Add a prop ingredient to the recipe.
+     * 
+     * @param ingredient the prop ingredient to be added
+     * @return the modified builder
+     */
+    public RitualRecipeBuilder addProp(BlockIngredient ingredient) {
+        return this.addProp(ingredient, 1);
+    }
+    
+    /**
+     * Add a prop ingredient of the given block to the recipe multiple times.
+     * 
+     * @param block the block to be added
+     * @param quantity the number of the block to add
+     * @return the modified builder
+     */
+    public RitualRecipeBuilder addProp(Block block, int quantity) {
+        return this.addProp(BlockIngredient.fromBlocks(block), quantity);
+    }
+    
+    /**
+     * Add a prop ingredient of the given block to the recipe.
+     * 
+     * @param block the block to be added
+     * @return the modified builder
+     */
+    public RitualRecipeBuilder addProp(Block block) {
+        return this.addProp(block, 1);
+    }
+    
+    /**
+     * Add a prop ingredient to the recipe multiple times that can be any block in the given tag.
+     * 
+     * @param tag the tag of blocks to be added
+     * @param quantity the number of the tag to add
+     * @return the modified builder
+     */
+    public RitualRecipeBuilder addProp(Tag<Block> tag, int quantity) {
+        return this.addProp(BlockIngredient.fromTag(tag), quantity);
+    }
+    
+    /**
+     * Add a prop ingredient to the recipe that can be any block in the given tag.
+     * 
+     * @param tag the tag of blocks to be added
+     * @return the modified builder
+     */
+    public RitualRecipeBuilder addProp(Tag<Block> tag) {
+        return this.addProp(tag, 1);
+    }
+    
+    /**
+     * Adds a group to this recipe.
+     * 
+     * @param group the group to add
+     * @return the modified builder
+     */
+    public RitualRecipeBuilder setGroup(String group) {
+        this.group = group;
+        return this;
+    }
+    
+    /**
+     * Adds a research requirement to this recipe.
+     * 
+     * @param research the research requirement to add
+     * @return the modified builder
+     */
+    public RitualRecipeBuilder research(CompoundResearchKey research) {
+        this.research = research.copy();
+        return this;
+    }
+    
+    /**
+     * Adds a mana cost to this recipe.
+     * 
+     * @param mana the mana cost to add
+     * @return the modified builder
+     */
+    public RitualRecipeBuilder manaCost(SourceList mana) {
+        this.manaCosts = mana.copy();
+        return this;
+    }
+    
+    /**
+     * Builds this recipe into an {@link IFinishedRecipe}.
+     * 
+     * @param consumer a consumer for the finished recipe
+     * @param id the ID of the finished recipe
+     */
+    public void build(Consumer<IFinishedRecipe> consumer, ResourceLocation id) {
+        this.validate(id);
+        consumer.accept(new RitualRecipeBuilder.Result(id, this.result, this.count, this.group == null ? "" : this.group, this.ingredients, this.props, this.research, this.manaCosts));
+    }
+    
+    /**
+     * Builds this recipe into an {@link IFinishedRecipe}. Use {@link #build(Consumer)} if save is the same as the ID for
+     * the result.
+     * 
+     * @param consumer a consumer for the finished recipe
+     * @param save custom ID for the finished recipe
+     */
+    public void build(Consumer<IFinishedRecipe> consumer, String save) {
+        ResourceLocation id = ForgeRegistries.ITEMS.getKey(this.result);
+        ResourceLocation saveLoc = new ResourceLocation(save);
+        if (saveLoc.equals(id)) {
+            throw new IllegalStateException("Ritual Recipe " + save + " should remove its 'save' argument");
+        } else {
+            this.build(consumer, saveLoc);
+        }
+    }
+    
+    /**
+     * Builds this recipe into an {@link IFinishedRecipe}.
+     * 
+     * @param consumer a consumer for the finished recipe
+     */
+    public void build(Consumer<IFinishedRecipe> consumer) {
+        this.build(consumer, ForgeRegistries.ITEMS.getKey(this.result));
+    }
+    
+    /**
+     * Makes sure that this recipe is valid.
+     * 
+     * @param id the ID of the recipe
+     */
+    protected void validate(ResourceLocation id) {
+        if (this.ingredients.isEmpty()) {
+            throw new IllegalStateException("No ingredients defined for ritual recipe " + id + "!");
+        }
+        if (this.research == null) {
+            throw new IllegalStateException("No research is defined for ritual recipe " + id + "!");
+        }
+    }
+    
+    public static class Result implements IFinishedRecipe {
+        protected final ResourceLocation id;
+        protected final Item result;
+        protected final int count;
+        protected final String group;
+        protected final List<Ingredient> ingredients;
+        protected final List<BlockIngredient> props;
+        protected final CompoundResearchKey research;
+        protected final SourceList manaCosts;
+
+        public Result(ResourceLocation id, Item result, int count, String group, List<Ingredient> ingredients, List<BlockIngredient> props, CompoundResearchKey research, SourceList manaCosts) {
+            this.id = id;
+            this.result = result;
+            this.count = count;
+            this.group = group;
+            this.ingredients = ingredients;
+            this.props = props;
+            this.research = research;
+            this.manaCosts = manaCosts;
+        }
+
+        @Override
+        public void serialize(JsonObject json) {
+            // Serialize the recipe group, if present
+            if (this.group != null && !this.group.isEmpty()) {
+                json.addProperty("group", this.group);
+            }
+            
+            // Serialize the recipe research requirement, if present
+            if (this.research != null) {
+                json.addProperty("research", this.research.toString());
+            }
+            
+            // Serialize the recipe mana costs, if present
+            if (this.manaCosts != null && !this.manaCosts.isEmpty()) {
+                JsonObject manaJson = new JsonObject();
+                for (Source source : this.manaCosts.getSourcesSorted()) {
+                    manaJson.addProperty(source.getTag(), this.manaCosts.getAmount(source));
+                }
+                json.add("mana", manaJson);
+            }
+            
+            // Serialize the recipe ingredient list
+            JsonArray ingredientsJson = new JsonArray();
+            for (Ingredient ingredient : this.ingredients) {
+                ingredientsJson.add(ingredient.serialize());
+            }
+            json.add("ingredients", ingredientsJson);
+            
+            // Serialize the recipe prop list, if present
+            if (this.props != null && !this.props.isEmpty()) {
+                JsonArray propsJson = new JsonArray();
+                for (BlockIngredient prop : this.props) {
+                    propsJson.add(prop.serialize());
+                }
+                json.add("props", propsJson);
+            }
+            
+            // Serialize the recipe result
+            JsonObject resultJson = new JsonObject();
+            resultJson.addProperty("item", ForgeRegistries.ITEMS.getKey(this.result).toString());
+            if (this.count > 1) {
+                resultJson.addProperty("count", this.count);
+            }
+            json.add("result", resultJson);
+        }
+
+        @Override
+        public ResourceLocation getID() {
+            return this.id;
+        }
+
+        @Override
+        public IRecipeSerializer<?> getSerializer() {
+            return RecipeSerializersPM.RITUAL.get();
+        }
+
+        @Override
+        public JsonObject getAdvancementJson() {
+            // Ritual recipes don't use the vanilla advancement unlock system, so return null
+            return null;
+        }
+
+        @Override
+        public ResourceLocation getAdvancementID() {
+            return new ResourceLocation("");
+        }
+    }
+}
