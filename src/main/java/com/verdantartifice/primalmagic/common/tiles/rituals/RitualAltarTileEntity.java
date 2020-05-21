6b879b003a64b5a5190abbaf0cfa4ef90ea9f611
Michael Bunting <michael.a.bunting@gmail.com>
2020-05-20 18:47:26 -0700

    Recipe detection for rituals

--- src/main/java/com/verdantartifice/primalmagic/common/tiles/rituals/RitualAltarTileEntity.java
+++ src/main/java/com/verdantartifice/primalmagic/common/tiles/rituals/RitualAltarTileEntity.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Queue;
 import java.util.Set;
 import java.util.UUID;
@@ -12,11 +13,15 @@ import java.util.UUID;
 import javax.annotation.Nullable;
 
 import com.google.common.collect.ImmutableSet;
+import com.verdantartifice.primalmagic.PrimalMagic;
 import com.verdantartifice.primalmagic.common.blocks.BlocksPM;
 import com.verdantartifice.primalmagic.common.blocks.rituals.OfferingPedestalBlock;
 import com.verdantartifice.primalmagic.common.blocks.rituals.RitualAltarBlock;
 import com.verdantartifice.primalmagic.common.blocks.rituals.SaltTrailBlock;
 import com.verdantartifice.primalmagic.common.blockstates.properties.SaltSide;
+import com.verdantartifice.primalmagic.common.containers.FakeContainer;
+import com.verdantartifice.primalmagic.common.crafting.IRitualRecipe;
+import com.verdantartifice.primalmagic.common.crafting.RecipeTypesPM;
 import com.verdantartifice.primalmagic.common.rituals.IRitualProp;
 import com.verdantartifice.primalmagic.common.tiles.TileEntityTypesPM;
 import com.verdantartifice.primalmagic.common.tiles.base.TileInventoryPM;
@@ -27,14 +32,17 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
+import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.NBTUtil;
 import net.minecraft.state.EnumProperty;
 import net.minecraft.tileentity.ITickableTileEntity;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
@@ -53,6 +61,7 @@ public class RitualAltarTileEntity extends TileInventoryPM implements ITickableT
     protected int activeCount = 0;
     protected UUID activePlayerId = null;
     protected PlayerEntity activePlayerCache = null;
+    protected ResourceLocation activeRecipeId = null;
     
     protected boolean scanDirty = false;
     protected Set<BlockPos> saltPositions = new HashSet<>();
@@ -113,6 +122,10 @@ public class RitualAltarTileEntity extends TileInventoryPM implements ITickableT
         } else {
             this.activePlayerId = null;
         }
+        
+        this.activeRecipeId = compound.contains("ActiveRecipeId", Constants.NBT.TAG_STRING) ? 
+                new ResourceLocation(compound.getString("ActiveRecipeId")) : 
+                null;
     }
     
     @Override
@@ -121,6 +134,9 @@ public class RitualAltarTileEntity extends TileInventoryPM implements ITickableT
         compound.putInt("ActiveCount", this.activeCount);
         if (this.activePlayerId != null) {
             compound.put("ActivePlayer", NBTUtil.writeUniqueId(this.activePlayerId));
+        }
+        if (this.activeRecipeId != null) {
+            compound.putString("ActiveRecipeId", this.activeRecipeId.toString());
         }
         return super.write(compound);
     }
@@ -136,10 +152,15 @@ public class RitualAltarTileEntity extends TileInventoryPM implements ITickableT
         }
         if (!this.world.isRemote && this.active) {
             if (this.activeCount >= 100) {
+                if (this.activeRecipeId != null) {
+                    Optional<? extends IRecipe<?>> recipeOpt = this.world.getServer().getRecipeManager().getRecipe(this.activeRecipeId);
+                    if (recipeOpt.isPresent()) {
+                        this.setInventorySlotContents(0, recipeOpt.get().getRecipeOutput().copy());
+                    }
+                    this.activeRecipeId = null;
+                }
                 if (this.getActivePlayer() != null) {
-                    this.setInventorySlotContents(0, new ItemStack(Items.DIAMOND));
-                    String msg = String.format("Ritual complete!  %1$d pedestals, %2$d props, %3$d salt", this.pedestalPositions.size(), this.propPositions.size(), this.saltPositions.size());
-                    this.getActivePlayer().sendStatusMessage(new StringTextComponent(msg), false);
+                    this.getActivePlayer().sendStatusMessage(new StringTextComponent("Ritual complete!"), false);
                 }
                 this.active = false;
                 this.activeCount = 0;
@@ -155,14 +176,18 @@ public class RitualAltarTileEntity extends TileInventoryPM implements ITickableT
         if (!this.world.isRemote && wandStack.getItem() instanceof IWand) {
             if (this.active) {
                 this.active = false;
+                this.activeCount = 0;
                 player.sendStatusMessage(new StringTextComponent("Ritual canceled"), false);
-            } else {
+                this.setActivePlayer(null);
+                this.activeRecipeId = null;
+            } else if (this.startCraft(wandStack, player)) {
                 this.active = true;
-                this.scanDirty = true;
+                this.activeCount = 0;
                 player.sendStatusMessage(new StringTextComponent("Ritual started!"), false);
+                this.setActivePlayer(player);
+            } else {
+                player.sendStatusMessage(new StringTextComponent("No valid ritual recipe found"), false);
             }
-            this.activeCount = 0;
-            this.setActivePlayer(player);
             this.markDirty();
             this.syncTile(false);
             return ActionResultType.SUCCESS;
@@ -174,6 +199,62 @@ public class RitualAltarTileEntity extends TileInventoryPM implements ITickableT
     @Override
     public void onWandUseTick(ItemStack wandStack, PlayerEntity player, int count) {
         // Do nothing; ritual altars don't support wand channeling
+    }
+    
+    protected boolean startCraft(ItemStack wandStack, PlayerEntity player) {
+        // Scan altar surroundings for pedestals and props immediately when starting a ritual
+        this.scanSurroundings();
+        
+        // Determine offerings
+        List<ItemStack> offerings = new ArrayList<>();
+        for (BlockPos offeringPos : this.pedestalPositions) {
+            TileEntity tile = this.world.getTileEntity(offeringPos);
+            if (tile instanceof OfferingPedestalTileEntity) {
+                OfferingPedestalTileEntity pedestalTile = (OfferingPedestalTileEntity)tile;
+                ItemStack stack = pedestalTile.getStackInSlot(0);
+                if (stack != null && !stack.isEmpty()) {
+                    offerings.add(stack);
+                }
+            }
+        }
+        
+        // Determine recipe that corresponds to offerings
+        CraftingInventory inv = new CraftingInventory(new FakeContainer(), offerings.size(), 1);
+        int offeringIndex = 0;
+        for (ItemStack offering : offerings) {
+            PrimalMagic.LOGGER.debug("Found ritual offering: {}", offering.getItem().getRegistryName().toString());
+            inv.setInventorySlotContents(offeringIndex++, offering);
+        }
+        Optional<IRitualRecipe> recipeOpt = this.world.getServer().getRecipeManager().getRecipe(RecipeTypesPM.RITUAL, inv, this.world);
+        if (recipeOpt.isPresent()) {
+            // Determine if the player has the research and mana to start this recipe
+            IRitualRecipe recipe = recipeOpt.get();
+            if (this.canUseRitualRecipe(wandStack, player, recipe)) {
+                this.activeRecipeId = recipe.getId();
+                PrimalMagic.LOGGER.debug("Found recipe {}", this.activeRecipeId.toString());
+                return true;
+            } else {
+                PrimalMagic.LOGGER.debug("Cannot use found recipe {}", recipe.getId().toString());
+                return false;
+            }
+        } else {
+            PrimalMagic.LOGGER.debug("No matching ritual recipe found");
+            return false;
+        }
+    }
+    
+    protected boolean canUseRitualRecipe(ItemStack wandStack, PlayerEntity player, IRitualRecipe recipe) {
+        // Players must know the correct research and the wand must have enough mana in order to use the recipe
+        return (recipe.getRequiredResearch() == null || recipe.getRequiredResearch().isKnownByStrict(player)) &&
+                (recipe.getManaCosts().isEmpty() || this.consumeMana(wandStack, player, recipe));
+    }
+    
+    protected boolean consumeMana(ItemStack wandStack, PlayerEntity player, IRitualRecipe recipe) {
+        if (wandStack == null || wandStack.isEmpty() || !(wandStack.getItem() instanceof IWand)) {
+            return false;
+        }
+        IWand wand = (IWand)wandStack.getItem();
+        return wand.consumeRealMana(wandStack, player, recipe.getManaCosts());
     }
     
     protected void scanSurroundings() {
