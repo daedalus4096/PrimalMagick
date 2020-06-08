d98091d1ebd290b405b044a09a67d49b80847509
Michael Bunting <michael.a.bunting@gmail.com>
2020-06-07 22:27:38 -0700

    Add base runescribing altar block

---
+++ src/main/java/com/verdantartifice/primalmagic/common/blocks/crafting/RunescribingAltarBlock.java
@@ -0,0 +1,36 @@
+package com.verdantartifice.primalmagic.common.blocks.crafting;
+
+import com.verdantartifice.primalmagic.PrimalMagic;
+import com.verdantartifice.primalmagic.common.misc.HarvestLevel;
+import com.verdantartifice.primalmagic.common.util.VoxelShapeUtils;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
+import net.minecraftforge.common.ToolType;
+
+/**
+ * Block definition for the runescribing altar.  May be used to apply combinations of runes to
+ * items in order to imbue them with controllable enchantments.
+ * 
+ * @author Daedalus4096
+ */
+public class RunescribingAltarBlock extends Block {
+    protected static final VoxelShape SHAPE = VoxelShapeUtils.fromModel(new ResourceLocation(PrimalMagic.MODID, "block/runescribing_altar"));
+
+    public RunescribingAltarBlock() {
+        super(Block.Properties.create(Material.ROCK, MaterialColor.QUARTZ).hardnessAndResistance(1.5F, 6.0F).sound(SoundType.STONE).harvestTool(ToolType.PICKAXE).harvestLevel(HarvestLevel.WOOD.getLevel()));
+    }
+    
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return SHAPE;
+    }
+}
