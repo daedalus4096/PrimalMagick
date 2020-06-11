f1e9f5a98b03f92dea6ebc5fa3d297889e18064b
Michael Bunting <michael.a.bunting@gmail.com>
2020-06-11 04:11:01 -0700

    Refactor runescribing altar container construction

--- src/main/java/com/verdantartifice/primalmagic/common/containers/AbstractRunescribingAltarContainer.java
+++ src/main/java/com/verdantartifice/primalmagic/common/containers/AbstractRunescribingAltarContainer.java
@@ -2,9 +2,6 @@ package com.verdantartifice.primalmagic.common.containers;
 
 import javax.annotation.Nonnull;
 
-import com.verdantartifice.primalmagic.common.containers.slots.RuneSlot;
-import com.verdantartifice.primalmagic.common.misc.DeviceTier;
-
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.IInventory;
@@ -22,55 +19,45 @@ import net.minecraft.world.World;
 public abstract class AbstractRunescribingAltarContainer extends Container {
     protected final IInventory altarInv;
     protected final World world;
-    protected final DeviceTier tier;
 
-    public AbstractRunescribingAltarContainer(@Nonnull ContainerType<?> type, int id, @Nonnull PlayerInventory playerInv, @Nonnull IInventory altarInv, @Nonnull DeviceTier tier) {
+    public AbstractRunescribingAltarContainer(@Nonnull ContainerType<?> type, int id, @Nonnull PlayerInventory playerInv, @Nonnull IInventory altarInv) {
         super(type, id);
         this.altarInv = altarInv;
         this.world = playerInv.player.world;
-        this.tier = tier;
         
         // Slot 0: runescribing input
         this.addSlot(new Slot(this.altarInv, 0, 19, 35));
         
         // TODO Slot 1: runescribing output
         
-        // Slots 2-10: runes
-        if (this.tier.compareTo(DeviceTier.BASIC) >= 0) {
-            this.addSlot(new RuneSlot(this.altarInv, 2, 44, 35));
-            this.addSlot(new RuneSlot(this.altarInv, 3, 62, 35));
-            this.addSlot(new RuneSlot(this.altarInv, 4, 80, 35));
-        }
-        if (this.tier.compareTo(DeviceTier.ENCHANTED) >= 0) {
-            this.addSlot(new RuneSlot(this.altarInv, 5, 62, 17));
-            this.addSlot(new RuneSlot(this.altarInv, 6, 62, 53));
-        }
-        if (this.tier.compareTo(DeviceTier.FORBIDDEN) >= 0) {
-            this.addSlot(new RuneSlot(this.altarInv, 7, 44, 17));
-            this.addSlot(new RuneSlot(this.altarInv, 8, 80, 17));
-        }
-        if (this.tier == DeviceTier.HEAVENLY) {
-            this.addSlot(new RuneSlot(this.altarInv, 9, 44, 53));
-            this.addSlot(new RuneSlot(this.altarInv, 10, 80, 53));
-        }
+        // Slots 2-(R+1), where R = rune capacity: runes
+        this.addRuneSlots();
         
-        // Slots 11-37: Player backpack
+        // Slots (R+2)-(R+28), where R = rune capacity: Player backpack
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 9; j++) {
                 this.addSlot(new Slot(playerInv, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
             }
         }
         
-        // Slots 38-46: Player hotbar
+        // Slots (R+29)-(R+37), where R = rune capacity: Player hotbar
         for (int i = 0; i < 9; i++) {
             this.addSlot(new Slot(playerInv, i, 8 + i * 18, 142));
         }
     }
     
-    public DeviceTier getDeviceTier() {
-        return this.tier;
-    }
-
+    /**
+     * Get the number of runes holdable by this container.
+     * 
+     * @return the number of runes holdable by this container
+     */
+    protected abstract int getRuneCapacity();
+    
+    /**
+     * Add slots for runes to this container.
+     */
+    protected abstract void addRuneSlots();
+    
     @Override
     public boolean canInteractWith(PlayerEntity playerIn) {
         return this.altarInv.isUsableByPlayer(playerIn);
--- src/main/java/com/verdantartifice/primalmagic/common/containers/RunescribingAltarBasicContainer.java
+++ src/main/java/com/verdantartifice/primalmagic/common/containers/RunescribingAltarBasicContainer.java
@@ -2,7 +2,7 @@ package com.verdantartifice.primalmagic.common.containers;
 
 import javax.annotation.Nonnull;
 
-import com.verdantartifice.primalmagic.common.misc.DeviceTier;
+import com.verdantartifice.primalmagic.common.containers.slots.RuneSlot;
 
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.IInventory;
@@ -14,11 +14,25 @@ import net.minecraft.inventory.Inventory;
  * @author Daedalus4096
  */
 public class RunescribingAltarBasicContainer extends AbstractRunescribingAltarContainer {
+    protected static final int RUNE_CAPACITY = 3;
+
     public RunescribingAltarBasicContainer(int id, @Nonnull PlayerInventory playerInv) {
-        this(id, playerInv, new Inventory(5));
+        this(id, playerInv, new Inventory(RUNE_CAPACITY + 2));
     }
     
     public RunescribingAltarBasicContainer(int id, @Nonnull PlayerInventory playerInv, @Nonnull IInventory altarInv) {
-        super(ContainersPM.RUNESCRIBING_ALTAR_BASIC.get(), id, playerInv, altarInv, DeviceTier.BASIC);
+        super(ContainersPM.RUNESCRIBING_ALTAR_BASIC.get(), id, playerInv, altarInv);
+    }
+    
+    @Override
+    protected int getRuneCapacity() {
+        return RUNE_CAPACITY;
+    }
+    
+    @Override
+    protected void addRuneSlots() {
+        this.addSlot(new RuneSlot(this.altarInv, 2, 44, 35));
+        this.addSlot(new RuneSlot(this.altarInv, 3, 62, 35));
+        this.addSlot(new RuneSlot(this.altarInv, 4, 80, 35));
     }
 }
