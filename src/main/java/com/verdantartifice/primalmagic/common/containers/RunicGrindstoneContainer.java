5356dfa93c2b7f21ad538c109f79eb4d91ad5e9e
Michael Bunting <michael.a.bunting@gmail.com>
2020-06-17 19:56:30 -0700

    Fix runic grindstone output display bug

--- src/main/java/com/verdantartifice/primalmagic/common/containers/RunicGrindstoneContainer.java
+++ src/main/java/com/verdantartifice/primalmagic/common/containers/RunicGrindstoneContainer.java
@@ -5,8 +5,10 @@ import com.verdantartifice.primalmagic.common.runes.RuneManager;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.container.GrindstoneContainer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.play.server.SSetSlotPacket;
 import net.minecraft.util.IWorldPosCallable;
 
 /**
@@ -16,6 +18,7 @@ import net.minecraft.util.IWorldPosCallable;
  */
 public class RunicGrindstoneContainer extends GrindstoneContainer {
     protected IWorldPosCallable worldPosCallable;
+    protected PlayerEntity player;
 
     public RunicGrindstoneContainer(int windowId, PlayerInventory playerInv) {
         this(windowId, playerInv, IWorldPosCallable.DUMMY);
@@ -24,6 +27,7 @@ public class RunicGrindstoneContainer extends GrindstoneContainer {
     public RunicGrindstoneContainer(int windowId, PlayerInventory playerInv, IWorldPosCallable worldPosCallable) {
         super(windowId, playerInv, worldPosCallable);
         this.worldPosCallable = worldPosCallable;
+        this.player = playerInv.player;
     }
     
     @Override
@@ -31,6 +35,18 @@ public class RunicGrindstoneContainer extends GrindstoneContainer {
         ItemStack retVal = super.removeEnchantments(stack, damage, count);
         RuneManager.clearRunes(retVal);
         return retVal;
+    }
+    
+    @Override
+    public void updateRecipeOutput() {
+        super.updateRecipeOutput();
+        this.worldPosCallable.consume((world, pos) -> {
+            if (!world.isRemote && this.player instanceof ServerPlayerEntity) {
+                ServerPlayerEntity spe = (ServerPlayerEntity)this.player;
+                ItemStack stack = this.outputInventory.getStackInSlot(0);
+                spe.connection.sendPacket(new SSetSlotPacket(this.windowId, 2, stack));
+            }
+        });
     }
     
     @Override
