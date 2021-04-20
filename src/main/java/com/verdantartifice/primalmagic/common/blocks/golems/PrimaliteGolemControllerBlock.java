02e47af3c56d9afff760269039c44599c35ff6f6
daedalus4096 <michael.a.bunting@gmail.com>
2021-04-19 22:10:20 -0700

    Enable wand interaction

--- src/main/java/com/verdantartifice/primalmagic/common/blocks/golems/PrimaliteGolemControllerBlock.java
+++ src/main/java/com/verdantartifice/primalmagic/common/blocks/golems/PrimaliteGolemControllerBlock.java
@@ -1,14 +1,23 @@
 package com.verdantartifice.primalmagic.common.blocks.golems;
 
+import com.verdantartifice.primalmagic.common.wands.IInteractWithWand;
+import com.verdantartifice.primalmagic.common.wands.IWand;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalBlock;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer.Builder;
+import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Mirror;
 import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.world.World;
 
 /**
  * Definition for the "head" block of a primalite golem.  Place it on top of a T shape of
@@ -16,7 +25,7 @@ import net.minecraft.util.Rotation;
  * 
  * @author Daedalus4096
  */
-public class PrimaliteGolemControllerBlock extends Block {
+public class PrimaliteGolemControllerBlock extends Block implements IInteractWithWand {
     protected static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
 
     public PrimaliteGolemControllerBlock(Properties properties) {
@@ -44,5 +53,20 @@ public class PrimaliteGolemControllerBlock extends Block {
     @Override
     protected void fillStateContainer(Builder<Block, BlockState> builder) {
         builder.add(FACING);
+    }
+
+    @Override
+    public ActionResultType onWandRightClick(ItemStack wandStack, World world, PlayerEntity player, BlockPos pos, Direction direction) {
+        if (!world.isRemote && wandStack.getItem() instanceof IWand) {
+            player.sendStatusMessage(new StringTextComponent("It's alive!"), false);
+            return ActionResultType.SUCCESS;
+        } else {
+            return ActionResultType.FAIL;
+        }
+    }
+
+    @Override
+    public void onWandUseTick(ItemStack wandStack, PlayerEntity player, int count) {
+        // Do nothing; golem controllers don't support wand channeling
     }
 }
