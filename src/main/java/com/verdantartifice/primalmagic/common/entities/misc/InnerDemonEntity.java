17246ba55304f43eeb18a7062946a8e3b1f7a2eb
daedalus4096 <michael.a.bunting@gmail.com>
2021-05-16 21:23:12 -0700

    Progress commit for inner demon entity logic

--- src/main/java/com/verdantartifice/primalmagic/common/entities/misc/InnerDemonEntity.java
+++ src/main/java/com/verdantartifice/primalmagic/common/entities/misc/InnerDemonEntity.java
@@ -1,17 +1,36 @@
 package com.verdantartifice.primalmagic.common.entities.misc;
 
+import com.verdantartifice.primalmagic.common.items.ItemsPM;
+
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.IChargeableMob;
 import net.minecraft.entity.IRangedAttackMob;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.goal.HurtByTargetGoal;
+import net.minecraft.entity.ai.goal.LookAtGoal;
+import net.minecraft.entity.ai.goal.LookRandomlyGoal;
+import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
+import net.minecraft.entity.ai.goal.SwimGoal;
+import net.minecraft.entity.ai.goal.WaterAvoidingRandomWalkingGoal;
+import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.BossInfo;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerBossInfo;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.event.ForgeEventFactory;
 
 /**
  * Definition of an inner demon entity, a boss monster summoned via a sanguine crucible.
@@ -21,14 +40,40 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 @OnlyIn(value = Dist.CLIENT, _interface = IChargeableMob.class)
 public class InnerDemonEntity extends MonsterEntity implements IRangedAttackMob, IChargeableMob {
     protected final ServerBossInfo bossInfo = (ServerBossInfo)(new ServerBossInfo(this.getDisplayName(), BossInfo.Color.RED, BossInfo.Overlay.PROGRESS)).setDarkenSky(true);
+    protected boolean isSuffocating = false;
 
     public InnerDemonEntity(EntityType<? extends InnerDemonEntity> type, World worldIn) {
         super(type, worldIn);
+        this.getNavigator().setCanSwim(true);
         this.experienceValue = 50;
     }
     
     public static AttributeModifierMap.MutableAttribute getAttributeModifiers() {
-        return MonsterEntity.func_234295_eP_().createMutableAttribute(Attributes.MAX_HEALTH, 400.0D).createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.23D).createMutableAttribute(Attributes.FOLLOW_RANGE, 40.0D).createMutableAttribute(Attributes.ARMOR, 4.0D);
+        return MonsterEntity.func_234295_eP_().createMutableAttribute(Attributes.MAX_HEALTH, 400.0D).createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.23D).createMutableAttribute(Attributes.FOLLOW_RANGE, 40.0D).createMutableAttribute(Attributes.ARMOR, 4.0D).createMutableAttribute(Attributes.ATTACK_DAMAGE, 15.0D);
+    }
+
+    @Override
+    protected void registerGoals() {
+        this.goalSelector.addGoal(0, new SwimGoal(this));
+        this.goalSelector.addGoal(5, new WaterAvoidingRandomWalkingGoal(this, 1.0D));
+        this.goalSelector.addGoal(6, new LookAtGoal(this, PlayerEntity.class, 8.0F));
+        this.goalSelector.addGoal(7, new LookRandomlyGoal(this));
+        this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, 0, false, false, null));
+    }
+
+    @Override
+    public void readAdditional(CompoundNBT compound) {
+        super.readAdditional(compound);
+        if (this.hasCustomName()) {
+            this.bossInfo.setName(this.getDisplayName());
+        }
+    }
+
+    @Override
+    public void setCustomName(ITextComponent name) {
+        super.setCustomName(name);
+        this.bossInfo.setName(this.getDisplayName());
     }
 
     @Override
@@ -42,4 +87,80 @@ public class InnerDemonEntity extends MonsterEntity implements IRangedAttackMob,
         return true;
     }
 
+    @Override
+    protected void updateAITasks() {
+        if (!this.world.isRemote) {
+            // Explode if suffocating
+            if (this.isSuffocating && this.ticksExisted % 20 == 0) {
+                Explosion.Mode mode = ForgeEventFactory.getMobGriefingEvent(this.world, this) ? Explosion.Mode.DESTROY : Explosion.Mode.NONE;
+                this.world.createExplosion(this, this.getPosX(), this.getPosY(), this.getPosZ(), 7.0F, false, mode);
+                this.isSuffocating = false;
+            }
+            
+            this.bossInfo.setPercent(this.getHealth() / this.getMaxHealth());
+        }
+        super.updateAITasks();
+    }
+
+    @Override
+    public boolean attackEntityFrom(DamageSource source, float amount) {
+        if (source == DamageSource.DROWN || source.getTrueSource() instanceof InnerDemonEntity) {
+            return false;
+        } else {
+            if (source == DamageSource.IN_WALL) {
+                this.isSuffocating = true;
+            }
+            return super.attackEntityFrom(source, amount);
+        }
+    }
+
+    @Override
+    public void addTrackingPlayer(ServerPlayerEntity player) {
+        super.addTrackingPlayer(player);
+        this.bossInfo.addPlayer(player);
+    }
+
+    @Override
+    public void removeTrackingPlayer(ServerPlayerEntity player) {
+        super.removeTrackingPlayer(player);
+        this.bossInfo.removePlayer(player);
+    }
+
+    @Override
+    protected void dropSpecialItems(DamageSource source, int looting, boolean recentlyHitIn) {
+        super.dropSpecialItems(source, looting, recentlyHitIn);
+        ItemEntity itemEntity = this.entityDropItem(ItemsPM.HALLOWED_ORB.get());
+        if (itemEntity != null) {
+            itemEntity.setNoDespawn();
+        }
+    }
+
+    @Override
+    public void checkDespawn() {
+        if (this.world.getDifficulty() == Difficulty.PEACEFUL && this.isDespawnPeaceful()) {
+            this.remove();
+        } else {
+            this.idleTime = 0;
+        }
+    }
+
+    @Override
+    public boolean onLivingFall(float distance, float damageMultiplier) {
+        return false;
+    }
+
+    @Override
+    public boolean isPotionApplicable(EffectInstance potioneffectIn) {
+        return false;
+    }
+
+    @Override
+    protected boolean canBeRidden(Entity entityIn) {
+        return false;
+    }
+
+    @Override
+    public boolean canChangeDimension() {
+        return false;
+    }
 }
