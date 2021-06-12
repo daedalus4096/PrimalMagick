f5cb53aa88fbb6f9b2e995d6f460322dde4ff9df
daedalus4096 <michael.a.bunting@gmail.com>
2021-06-11 22:01:48 -0700

    Disable reverberation effect when sneaking

--- src/main/java/com/verdantartifice/primalmagic/common/events/BlockEvents.java
+++ src/main/java/com/verdantartifice/primalmagic/common/events/BlockEvents.java
@@ -1,8 +1,5 @@
 package com.verdantartifice.primalmagic.common.events;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import com.verdantartifice.primalmagic.PrimalMagic;
 import com.verdantartifice.primalmagic.common.enchantments.EnchantmentsPM;
 import com.verdantartifice.primalmagic.common.misc.BlockBreaker;
@@ -32,13 +29,11 @@ import net.minecraftforge.fml.common.Mod;
  */
 @Mod.EventBusSubscriber(modid=PrimalMagic.MODID)
 public class BlockEvents {
-    protected static final Logger LOGGER = LogManager.getLogger();
-    
     @SubscribeEvent
     public static void onBlockBreak(BlockEvent.BreakEvent event) {
         PlayerEntity player = event.getPlayer();
         World world = (event.getWorld() instanceof World) ? (World)event.getWorld() : null;
-        if (!event.isCanceled() && world != null && !world.isRemote && !BlockBreaker.hasBreakerQueued(world, event.getPos())) {
+        if (!event.isCanceled() && world != null && !world.isRemote && !player.isSecondaryUseActive() && !BlockBreaker.hasBreakerQueued(world, event.getPos())) {
             // Trigger block breakers if the player has a Reverberation tool in the main hand
             int reverbLevel = EnchantmentHelper.getEnchantmentLevel(EnchantmentsPM.REVERBERATION.get(), player.getHeldItemMainhand());
             if (reverbLevel > 0) {
@@ -63,7 +58,6 @@ public class BlockEvents {
         int xLimit = level * (dir.getXOffset() == 0 ? 1 : 0);
         int yLimit = level * (dir.getYOffset() == 0 ? 1 : 0);
         int zLimit = level * (dir.getZOffset() == 0 ? 1 : 0);
-        int count = 0;
         for (int dx = -xLimit; dx <= xLimit; dx++) {
             for (int dy = -yLimit; dy <= yLimit; dy++) {
                 for (int dz = -zLimit; dz <= zLimit; dz++) {
@@ -74,12 +68,10 @@ public class BlockEvents {
                         float targetDurability = (float)Math.sqrt(100.0F * targetState.getBlockHardness(world, pos));
                         float newDurability = Math.max(0.0F, targetDurability - durability);
                         BlockBreaker.schedule(world, 2 * targetPos.manhattanDistance(pos), new BlockBreaker(0, targetPos, targetState, newDurability, targetDurability, player, true, true));
-                        count++;
                     }
                 }
             }
         }
-        LOGGER.debug("Enqueued {} block breakers for reverb at pos ({}, {}, {}), dir {}", count, pos.getX(), pos.getY(), pos.getZ(), dir.getString());
     }
     
     @SubscribeEvent(priority=EventPriority.LOWEST)
