2ecd177eac0c79725428a503b371a282b5796c48
daedalus4096 <michael.a.bunting@gmail.com>
2021-06-27 15:17:44 -0700

    Progress commit for magical metal tridents

---
+++ src/main/java/com/verdantartifice/primalmagic/client/renderers/itemstack/TridentISTER.java
@@ -0,0 +1,55 @@
+package com.verdantartifice.primalmagic.client.renderers.itemstack;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import com.verdantartifice.primalmagic.PrimalMagic;
+import com.verdantartifice.primalmagic.common.items.tools.TridentItemPM;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.client.renderer.entity.model.TridentModel;
+import net.minecraft.client.renderer.model.IBakedModel;
+import net.minecraft.client.renderer.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.model.ModelResourceLocation;
+import net.minecraft.client.renderer.tileentity.ItemStackTileEntityRenderer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+/**
+ * Custom item stack renderer for magical metal tridents.
+ * 
+ * @author Daedalus4096
+ * @see {@link com.verdantartifice.primalmagic.common.items.tools.TridentItemPM}
+ */
+@OnlyIn(Dist.CLIENT)
+public class TridentISTER extends ItemStackTileEntityRenderer {
+    protected static final ModelResourceLocation MRL = new ModelResourceLocation(new ResourceLocation(PrimalMagic.MODID, "primalite_trident"), "inventory");
+    protected final TridentModel model = new TridentModel();
+    
+    @Override
+    public void func_239207_a_(ItemStack stack, ItemCameraTransforms.TransformType transformType, MatrixStack matrixStack, IRenderTypeBuffer buffer, int combinedLight, int combinedOverlay) {
+        if (stack.getItem() instanceof TridentItemPM) {
+            Minecraft mc = Minecraft.getInstance();
+            ItemRenderer itemRenderer = mc.getItemRenderer();
+            
+            boolean render2d = (transformType == ItemCameraTransforms.TransformType.GUI ||
+                                transformType == ItemCameraTransforms.TransformType.GROUND ||
+                                transformType == ItemCameraTransforms.TransformType.FIXED);
+            if (render2d) {
+                IBakedModel bakedModel = mc.getModelManager().getModel(MRL).getBakedModel();
+                matrixStack.push();
+                itemRenderer.renderItem(stack, transformType, true, matrixStack, buffer, combinedLight, combinedOverlay, bakedModel);
+                matrixStack.pop();
+            } else {
+                matrixStack.push();
+                matrixStack.scale(1.0F, -1.0F, -1.0F);
+                IVertexBuilder ivertexbuilder1 = ItemRenderer.getEntityGlintVertexBuilder(buffer, this.model.getRenderType(TridentModel.TEXTURE_LOCATION), false, stack.hasEffect());
+                this.model.render(matrixStack, ivertexbuilder1, combinedLight, combinedOverlay, 1.0F, 1.0F, 1.0F, 1.0F);
+                matrixStack.pop();
+            }
+        }
+    }
+}
