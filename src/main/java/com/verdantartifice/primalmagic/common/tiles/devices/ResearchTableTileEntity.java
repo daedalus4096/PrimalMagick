ffa6caec436c18a97943f974a20ec97902bfcebd
daedalus4096 <michael.a.bunting@gmail.com>
2021-09-20 20:44:08 -0700

    Fix hopper interaction with Research Tables

--- src/main/java/com/verdantartifice/primalmagic/common/tiles/devices/ResearchTableTileEntity.java
+++ src/main/java/com/verdantartifice/primalmagic/common/tiles/devices/ResearchTableTileEntity.java
@@ -5,6 +5,7 @@ import com.verdantartifice.primalmagic.common.tiles.TileEntityTypesPM;
 import com.verdantartifice.primalmagic.common.tiles.base.TileInventoryPM;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.MenuProvider;
@@ -12,6 +13,7 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerLevelAccess;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
 
 /**
@@ -21,6 +23,10 @@ import net.minecraft.world.level.block.state.BlockState;
  * @author Daedalus4096
  */
 public class ResearchTableTileEntity extends TileInventoryPM implements MenuProvider {
+    protected static final int[] SLOTS_FOR_UP = new int[] { 1 };
+    protected static final int[] SLOTS_FOR_DOWN = new int[0];
+    protected static final int[] SLOTS_FOR_SIDES = new int[] { 0 };
+    
     public ResearchTableTileEntity(BlockPos pos, BlockState state) {
         super(TileEntityTypesPM.RESEARCH_TABLE.get(), pos, state, 2);
     }
@@ -35,5 +41,26 @@ public class ResearchTableTileEntity extends TileInventoryPM implements MenuProv
     @Override
     public Component getDisplayName() {
         return new TranslatableComponent(this.getBlockState().getBlock().getDescriptionId());
+    }
+
+    @Override
+    public int[] getSlotsForFace(Direction side) {
+        if (side == Direction.UP) {
+            return SLOTS_FOR_UP;
+        } else if (side == Direction.DOWN) {
+            return SLOTS_FOR_DOWN;
+        } else {
+            return SLOTS_FOR_SIDES;
+        }
+    }
+
+    @Override
+    public boolean canPlaceItemThroughFace(int index, ItemStack itemStackIn, Direction direction) {
+        return this.canPlaceItem(index, itemStackIn);
+    }
+
+    @Override
+    public boolean canTakeItemThroughFace(int index, ItemStack stack, Direction direction) {
+        return true;
     }
 }
