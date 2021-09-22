27d5d181d7c57c52507afffc6a7c68b822867346
daedalus4096 <michael.a.bunting@gmail.com>
2021-09-21 19:15:41 -0700

    Add new damage sources

--- src/main/java/com/verdantartifice/primalmagic/common/misc/DamageSourcesPM.java
+++ src/main/java/com/verdantartifice/primalmagic/common/misc/DamageSourcesPM.java
@@ -2,6 +2,8 @@ package com.verdantartifice.primalmagic.common.misc;
 
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.EntityDamageSource;
+import net.minecraft.world.damagesource.IndirectEntityDamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 
 /**
@@ -11,10 +13,20 @@ import net.minecraft.world.entity.LivingEntity;
  */
 public class DamageSourcesPM {
     public static final String HELLISH_CHAIN_TYPE = "primalmagic.hellish_chain";
+    public static final String DIRECT_SORCERY_TYPE = "primalmagic.directSorcery";
+    public static final String INDIRECT_SORCERY_TYPE = "primalmagic.indirectSorcery";
     
     public static final DamageSource BLEEDING = (new DamageSource("primalmagic.bleeding")).bypassArmor();
     
     public static DamageSource causeHellishChainDamage(LivingEntity mob) {
         return (new EntityDamageSource(HELLISH_CHAIN_TYPE, mob)).setIsFire();
+    }
+    
+    public static DamageSource causeTouchSorceryDamage(LivingEntity caster) {
+        return new EntityDamageSource(DIRECT_SORCERY_TYPE, caster).setMagic();
+    }
+    
+    public static DamageSource causeIndirectSorceryDamage(Entity directSource, LivingEntity indirectSource) {
+        return new IndirectEntityDamageSource(INDIRECT_SORCERY_TYPE, directSource, indirectSource).setMagic();
     }
 }
