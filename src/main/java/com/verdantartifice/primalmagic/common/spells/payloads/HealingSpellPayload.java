2d1312965acc9e2b4fbe5984a25d39d3ff01916d
daedalus4096 <michael.a.bunting@gmail.com>
2021-09-22 22:12:29 -0700

    Remove unused import

--- src/main/java/com/verdantartifice/primalmagic/common/spells/payloads/HealingSpellPayload.java
+++ src/main/java/com/verdantartifice/primalmagic/common/spells/payloads/HealingSpellPayload.java
@@ -14,7 +14,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
