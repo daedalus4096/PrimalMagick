ef25109e87bd8105450d77d0eabc7c592974b05a
daedalus4096 <michael.a.bunting@gmail.com>
2021-09-30 21:31:54 -0700

    Ignyx explodes when thrown

--- src/main/java/com/verdantartifice/primalmagic/common/items/misc/IgnyxItem.java
+++ src/main/java/com/verdantartifice/primalmagic/common/items/misc/IgnyxItem.java
@@ -1,8 +1,19 @@
 package com.verdantartifice.primalmagic.common.items.misc;
 
+import java.util.Random;
+
+import com.verdantartifice.primalmagic.common.entities.projectiles.IgnyxEntity;
+
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.Level;
 
 /**
  * Item definition for ignyx, an alchemical super-coal.  Explodes on impact when thrown.
@@ -17,5 +28,23 @@ public class IgnyxItem extends Item {
     @Override
     public int getBurnTime(ItemStack itemStack, RecipeType<?> recipeType) {
         return 12800;
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        ItemStack stack = player.getItemInHand(hand);
+        Random random = level.getRandom();
+        level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.FIRECHARGE_USE, SoundSource.NEUTRAL, 1.0F, (random.nextFloat() - random.nextFloat()) * 0.2F + 1.0F);
+        if (!level.isClientSide) {
+            IgnyxEntity projectile = new IgnyxEntity(level, player);
+            projectile.setItem(stack);
+            projectile.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 1.5F, 1.0F);
+            level.addFreshEntity(projectile);
+        }
+        player.awardStat(Stats.ITEM_USED.get(this));
+        if (!player.getAbilities().instabuild) {
+            stack.shrink(1);
+        }
+        return InteractionResultHolder.sidedSuccess(stack, level.isClientSide);
     }
 }
