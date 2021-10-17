fb05928c835bfdef23a807a99067b323ee726ca9
daedalus4096 <michael.a.bunting@gmail.com>
2021-10-17 16:03:07 -0700

    Add base dowsing rod item

---
+++ src/main/java/com/verdantartifice/primalmagic/common/items/misc/DowsingRodItem.java
@@ -0,0 +1,14 @@
+package com.verdantartifice.primalmagic.common.items.misc;
+
+import net.minecraft.world.item.Item;
+
+/**
+ * Definition of an item for checking ritual altar and prop status.
+ * 
+ * @author Daedalus4096
+ */
+public class DowsingRodItem extends Item {
+    public DowsingRodItem(Item.Properties properties) {
+        super(properties);
+    }
+}
