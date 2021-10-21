11c9f5e58cf3fb4387fee66bead9ec2e303f5d0a
daedalus4096 <michael.a.bunting@gmail.com>
2021-10-20 21:53:19 -0700

    Integrate arcane recipe book component into concocter screen

--- src/main/java/com/verdantartifice/primalmagic/client/gui/ConcocterScreen.java
+++ src/main/java/com/verdantartifice/primalmagic/client/gui/ConcocterScreen.java
@@ -6,26 +6,33 @@ import org.apache.logging.log4j.Logger;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.verdantartifice.primalmagic.PrimalMagic;
+import com.verdantartifice.primalmagic.client.gui.recipe_book.ArcaneRecipeBookComponent;
+import com.verdantartifice.primalmagic.client.gui.recipe_book.ArcaneRecipeUpdateListener;
 import com.verdantartifice.primalmagic.client.gui.widgets.ManaGaugeWidget;
 import com.verdantartifice.primalmagic.common.containers.ConcocterContainer;
 import com.verdantartifice.primalmagic.common.sources.Source;
 
 import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.ClickType;
+import net.minecraft.world.inventory.Slot;
 
 /**
  * GUI screen for concocter block.
  * 
  * @author Daedalus4096
  */
-public class ConcocterScreen extends AbstractContainerScreen<ConcocterContainer> {
+public class ConcocterScreen extends AbstractContainerScreen<ConcocterContainer> implements ArcaneRecipeUpdateListener {
     protected static final Logger LOGGER = LogManager.getLogger();
     protected static final ResourceLocation TEXTURE = new ResourceLocation(PrimalMagic.MODID, "textures/gui/concocter.png");
     protected static final ResourceLocation RECIPE_BUTTON_LOCATION = new ResourceLocation("textures/gui/recipe_button.png");
     
+    protected final ArcaneRecipeBookComponent recipeBookComponent = new ArcaneRecipeBookComponent();
+    protected boolean widthTooNarrow;
     protected ManaGaugeWidget manaGauge;
 
     public ConcocterScreen(ConcocterContainer screenContainer, Inventory inv, Component titleIn) {
@@ -39,32 +46,97 @@ public class ConcocterScreen extends AbstractContainerScreen<ConcocterContainer>
         this.manaGauge.setCurrentMana(this.menu.getCurrentMana());
         this.manaGauge.setMaxMana(this.menu.getMaxMana());
         this.renderBackground(matrixStack);
-        super.render(matrixStack, mouseX, mouseY, partialTicks);
+        if (this.recipeBookComponent.isVisible() && this.widthTooNarrow) {
+            this.renderBg(matrixStack, partialTicks, mouseX, mouseY);
+            this.recipeBookComponent.render(matrixStack, mouseX, mouseY, partialTicks);
+        } else {
+            this.recipeBookComponent.render(matrixStack, mouseX, mouseY, partialTicks);
+            super.render(matrixStack, mouseX, mouseY, partialTicks);
+            this.recipeBookComponent.renderGhostRecipe(matrixStack, this.leftPos, this.topPos, true, partialTicks);
+        }
         this.renderTooltip(matrixStack, mouseX, mouseY);
+        this.recipeBookComponent.renderTooltip(matrixStack, this.leftPos, this.topPos, mouseX, mouseY);
     }
     
     @Override
     protected void init() {
         super.init();
+        this.widthTooNarrow = this.width < 379;
+        this.recipeBookComponent.init(this.width, this.height, this.minecraft, this.widthTooNarrow, this.menu);
+        this.leftPos = this.recipeBookComponent.updateScreenPosition(this.width, this.imageWidth);
+
         this.manaGauge = this.addRenderableWidget(new ManaGaugeWidget(this.leftPos + 10, this.topPos + 6, Source.INFERNAL, this.menu.getCurrentMana(), this.menu.getMaxMana()));
         
         // Add arcane recipe book button
         this.addRenderableWidget(new ImageButton(this.leftPos + 105, this.topPos + 52, 20, 18, 0, 0, 19, RECIPE_BUTTON_LOCATION, (button) -> {
-            LOGGER.info("Toggling concoter recipe book");
-//            this.recipeBookComponent.toggleVisibility();
-//            this.leftPos = this.recipeBookComponent.updateScreenPosition(this.width, this.imageWidth);
-//            ((ImageButton)button).setPosition(this.leftPos + 105, this.topPos + 52);
+            this.recipeBookComponent.toggleVisibility();
+            this.leftPos = this.recipeBookComponent.updateScreenPosition(this.width, this.imageWidth);
+            ((ImageButton)button).setPosition(this.leftPos + 105, this.topPos + 52);
         }));
+        this.addWidget(this.recipeBookComponent);
+        this.setInitialFocus(this.recipeBookComponent);
+        this.titleLabelX = 29;
+    }
+
+    @Override
+    protected void containerTick() {
+        super.containerTick();
+        this.recipeBookComponent.tick();
     }
 
     @Override
     protected void renderBg(PoseStack matrixStack, float partialTicks, int x, int y) {
         // Render background texture
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         RenderSystem.setShaderTexture(0, TEXTURE);
         this.blit(matrixStack, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight);
         
         // Animate spin progress indicator
         int cook = this.menu.getCookProgressionScaled();
         this.blit(matrixStack, this.leftPos + 103, this.topPos + 34, 176, 0, cook + 1, 16);
+    }
+
+    @Override
+    protected boolean isHovering(int p_97768_, int p_97769_, int p_97770_, int p_97771_, double p_97772_, double p_97773_) {
+        return (!this.widthTooNarrow || !this.recipeBookComponent.isVisible()) && super.isHovering(p_97768_, p_97769_, p_97770_, p_97771_, p_97772_, p_97773_);
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int buttonIndex) {
+        if (this.recipeBookComponent.mouseClicked(mouseX, mouseY, buttonIndex)) {
+            this.setFocused(this.recipeBookComponent);
+            return true;
+        } else {
+            return this.widthTooNarrow && this.recipeBookComponent.isVisible() ? true : super.mouseClicked(mouseX, mouseY, buttonIndex);
+        }
+    }
+
+    @Override
+    protected boolean hasClickedOutside(double mouseX, double mouseY, int leftPos, int topPos, int buttonIndex) {
+        boolean flag = mouseX < (double)leftPos || mouseY < (double)topPos || mouseX >= (double)(leftPos + this.imageWidth) || mouseY >= (double)(topPos + this.imageHeight);
+        return this.recipeBookComponent.hasClickedOutside(mouseX, mouseY, this.leftPos, this.topPos, this.imageWidth, this.imageHeight, buttonIndex) && flag;
+    }
+
+    @Override
+    protected void slotClicked(Slot p_97778_, int p_97779_, int p_97780_, ClickType p_97781_) {
+        super.slotClicked(p_97778_, p_97779_, p_97780_, p_97781_);
+        this.recipeBookComponent.slotClicked(p_97778_);
+    }
+
+    @Override
+    public void removed() {
+        super.removed();
+        this.recipeBookComponent.removed();
+    }
+
+    @Override
+    public void recipesUpdated() {
+        this.recipeBookComponent.recipesUpdated();
+    }
+
+    @Override
+    public ArcaneRecipeBookComponent getRecipeBookComponent() {
+        return this.recipeBookComponent;
     }
 }
