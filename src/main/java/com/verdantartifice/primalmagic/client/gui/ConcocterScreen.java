41ad3f513d6e67d4426ce38d1bba6e73b5b3c5ef
daedalus4096 <michael.a.bunting@gmail.com>
2021-10-20 20:59:57 -0700

    Add stub recipe book button

--- src/main/java/com/verdantartifice/primalmagic/client/gui/ConcocterScreen.java
+++ src/main/java/com/verdantartifice/primalmagic/client/gui/ConcocterScreen.java
@@ -1,5 +1,8 @@
 package com.verdantartifice.primalmagic.client.gui;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.verdantartifice.primalmagic.PrimalMagic;
@@ -7,6 +10,7 @@ import com.verdantartifice.primalmagic.client.gui.widgets.ManaGaugeWidget;
 import com.verdantartifice.primalmagic.common.containers.ConcocterContainer;
 import com.verdantartifice.primalmagic.common.sources.Source;
 
+import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -18,7 +22,9 @@ import net.minecraft.world.entity.player.Inventory;
  * @author Daedalus4096
  */
 public class ConcocterScreen extends AbstractContainerScreen<ConcocterContainer> {
+    protected static final Logger LOGGER = LogManager.getLogger();
     protected static final ResourceLocation TEXTURE = new ResourceLocation(PrimalMagic.MODID, "textures/gui/concocter.png");
+    protected static final ResourceLocation RECIPE_BUTTON_LOCATION = new ResourceLocation("textures/gui/recipe_button.png");
     
     protected ManaGaugeWidget manaGauge;
 
@@ -41,6 +47,14 @@ public class ConcocterScreen extends AbstractContainerScreen<ConcocterContainer>
     protected void init() {
         super.init();
         this.manaGauge = this.addRenderableWidget(new ManaGaugeWidget(this.leftPos + 10, this.topPos + 6, Source.INFERNAL, this.menu.getCurrentMana(), this.menu.getMaxMana()));
+        
+        // Add arcane recipe book button
+        this.addRenderableWidget(new ImageButton(this.leftPos + 105, this.topPos + 52, 20, 18, 0, 0, 19, RECIPE_BUTTON_LOCATION, (button) -> {
+            LOGGER.info("Toggling concoter recipe book");
+//            this.recipeBookComponent.toggleVisibility();
+//            this.leftPos = this.recipeBookComponent.updateScreenPosition(this.width, this.imageWidth);
+//            ((ImageButton)button).setPosition(this.leftPos + 105, this.topPos + 52);
+        }));
     }
 
     @Override
