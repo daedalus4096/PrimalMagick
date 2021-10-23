93c43b8fcfbde7103f6281854fbd4dcee0a2a7a0
daedalus4096 <michael.a.bunting@gmail.com>
2021-10-22 21:10:29 -0700

    Add base tile entity

--- src/main/java/com/verdantartifice/primalmagic/common/blocks/devices/EssenceTransmuterBlock.java
+++ src/main/java/com/verdantartifice/primalmagic/common/blocks/devices/EssenceTransmuterBlock.java
@@ -2,14 +2,23 @@ package com.verdantartifice.primalmagic.common.blocks.devices;
 
 import java.util.List;
 
+import com.verdantartifice.primalmagic.common.sources.IManaContainer;
 import com.verdantartifice.primalmagic.common.sources.Source;
 import com.verdantartifice.primalmagic.common.sources.SourceList;
+import com.verdantartifice.primalmagic.common.tiles.TileEntityTypesPM;
+import com.verdantartifice.primalmagic.common.tiles.devices.EssenceTransmuterTileEntity;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.Containers;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.BlockPlaceContext;
@@ -29,6 +38,8 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition.Builder;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.fmllegacy.network.NetworkHooks;
 
 /**
  * Block definition for an essence transmuter.  Uses moon mana to turn one type of essence into another.
@@ -90,13 +101,51 @@ public class EssenceTransmuterBlock extends BaseEntityBlock {
 
     @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
-        // TODO Auto-generated method stub
-        return null;
+        return new EssenceTransmuterTileEntity(pos, state);
     }
 
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
-        // TODO Auto-generated method stub
-        return super.getTicker(level, state, type);
+        return createTickerHelper(type, TileEntityTypesPM.ESSENCE_TRANSMUTER.get(), EssenceTransmuterTileEntity::tick);
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level worldIn, BlockPos pos, Player player, InteractionHand handIn, BlockHitResult hit) {
+        if (!worldIn.isClientSide && player instanceof ServerPlayer serverPlayer) {
+            // Open the GUI for the essence transmuter
+            BlockEntity tile = worldIn.getBlockEntity(pos);
+            if (tile instanceof EssenceTransmuterTileEntity transmuterTile) {
+                NetworkHooks.openGui(serverPlayer, transmuterTile);
+            }
+        }
+        return InteractionResult.SUCCESS;
+    }
+    
+    @SuppressWarnings("deprecation")
+    @Override
+    public void onRemove(BlockState state, Level worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
+        // Drop the tile entity's inventory into the world when the block is replaced
+        if (state.getBlock() != newState.getBlock()) {
+            BlockEntity tile = worldIn.getBlockEntity(pos);
+            if (tile instanceof EssenceTransmuterTileEntity transmuterTile) {
+                Containers.dropContents(worldIn, pos, transmuterTile);
+                worldIn.updateNeighbourForOutputSignal(pos, this);
+            }
+            super.onRemove(state, worldIn, pos, newState, isMoving);
+        }
+    }
+
+    @Override
+    public void setPlacedBy(Level worldIn, BlockPos pos, BlockState state, LivingEntity placer, ItemStack stack) {
+        super.setPlacedBy(worldIn, pos, state, placer, stack);
+        BlockEntity tile = worldIn.getBlockEntity(pos);
+        if (tile instanceof IManaContainer manaTile) {
+            CompoundTag nbt = stack.getTagElement("ManaContainerTag");
+            if (nbt != null) {
+                SourceList mana = new SourceList();
+                mana.deserializeNBT(nbt);
+                manaTile.setMana(mana);
+            }
+        }
     }
 }
