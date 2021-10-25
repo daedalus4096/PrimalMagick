e51261ab5035c54ebb69774a11033e0dc02c771b
daedalus4096 <michael.a.bunting@gmail.com>
2021-10-24 22:09:33 -0700

    Allow librarians to buy relics

--- src/main/java/com/verdantartifice/primalmagic/common/events/VillagerEvents.java
+++ src/main/java/com/verdantartifice/primalmagic/common/events/VillagerEvents.java
@@ -5,6 +5,7 @@ import com.verdantartifice.primalmagic.common.items.ItemsPM;
 
 import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.common.BasicTrade;
 import net.minecraftforge.event.village.VillagerTradesEvent;
 import net.minecraftforge.event.village.WandererTradesEvent;
@@ -22,7 +23,9 @@ public class VillagerEvents {
     public static void onVillagerTradeSetup(VillagerTradesEvent event) {
         // Allow librarians to trade certain knowledge granting items
         if (VillagerProfession.LIBRARIAN.equals(event.getType())) {
+            event.getTrades().get(1).add(new BasicTrade(new ItemStack(ItemsPM.MYSTICAL_RELIC_FRAGMENT.get(), 3), new ItemStack(Items.EMERALD, 1), 12, 2, 0.05F));
             event.getTrades().get(2).add(new BasicTrade(8, new ItemStack(ItemsPM.OBSERVATION_NOTES.get()), 12, 5, 0.05F));
+            event.getTrades().get(3).add(new BasicTrade(new ItemStack(ItemsPM.MYSTICAL_RELIC.get(), 1), new ItemStack(Items.EMERALD, 5), 12, 20, 0.05F));
             event.getTrades().get(4).add(new BasicTrade(16, new ItemStack(ItemsPM.THEORY_NOTES.get()), 12, 15, 0.05F));
         }
     }
