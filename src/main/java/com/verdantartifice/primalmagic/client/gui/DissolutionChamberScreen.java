b32f3e3af18c0f1a0f3aeab0e083d25a9a86f8d5
daedalus4096 <michael.a.bunting@gmail.com>
2021-11-01 21:44:27 -0700

    Add stub recipe book button for dissolution chamber

--- src/main/java/com/verdantartifice/primalmagic/client/gui/DissolutionChamberScreen.java
+++ src/main/java/com/verdantartifice/primalmagic/client/gui/DissolutionChamberScreen.java
@@ -1,5 +1,8 @@
 package com.verdantartifice.primalmagic.client.gui;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.verdantartifice.primalmagic.PrimalMagic;
@@ -7,6 +10,7 @@ import com.verdantartifice.primalmagic.client.gui.widgets.ManaGaugeWidget;
 import com.verdantartifice.primalmagic.common.containers.DissolutionChamberContainer;
 import com.verdantartifice.primalmagic.common.sources.Source;
 
+import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -18,7 +22,9 @@ import net.minecraft.world.entity.player.Inventory;
  * @author Daedalus4096
  */
 public class DissolutionChamberScreen extends AbstractContainerScreen<DissolutionChamberContainer> {
+    protected static final Logger LOGGER = LogManager.getLogger();
     protected static final ResourceLocation TEXTURE = new ResourceLocation(PrimalMagic.MODID, "textures/gui/dissolution_chamber.png");
+    protected static final ResourceLocation RECIPE_BUTTON_LOCATION = new ResourceLocation("textures/gui/recipe_button.png");
     
     protected ManaGaugeWidget manaGauge;
 
@@ -39,6 +45,10 @@ public class DissolutionChamberScreen extends AbstractContainerScreen<Dissolutio
     protected void init() {
         super.init();
         this.manaGauge = this.addRenderableWidget(new ManaGaugeWidget(this.leftPos + 10, this.topPos + 6, Source.EARTH, this.menu.getCurrentMana(), this.menu.getMaxMana()));
+        
+        this.addRenderableWidget(new ImageButton(this.leftPos + 80, this.topPos + 52, 20, 18, 0, 0, 19, RECIPE_BUTTON_LOCATION, (button) -> {
+            LOGGER.info("Toggling dissolution chamber recipe book visibility");
+        }));
     }
 
     @Override
