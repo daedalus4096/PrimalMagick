8c6a8410bb583734e9326e7f053997941237d66a
daedalus4096 <michael.a.bunting@gmail.com>
2021-12-18 17:40:10 -0800

    Move structure-biome association to world load event

--- src/main/java/com/verdantartifice/primalmagick/common/events/BiomeEvents.java
+++ src/main/java/com/verdantartifice/primalmagick/common/events/BiomeEvents.java
@@ -4,8 +4,6 @@ import javax.annotation.Nonnull;
 
 import com.verdantartifice.primalmagick.PrimalMagick;
 import com.verdantartifice.primalmagick.common.entities.EntityTypesPM;
-import com.verdantartifice.primalmagick.common.worldgen.features.ConfiguredStructureFeaturesPM;
-import com.verdantartifice.primalmagick.common.worldgen.features.StructureFeaturesPM;
 import com.verdantartifice.primalmagick.common.worldgen.features.OrePlacementsPM;
 import com.verdantartifice.primalmagick.common.worldgen.features.VegetationPlacementsPM;
 
@@ -40,33 +38,12 @@ public class BiomeEvents {
             event.getGeneration().addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, OrePlacementsPM.ORE_QUARTZ);
         }
         
-        // Add earth shrines to flatlands
-        if (Biome.BiomeCategory.PLAINS.equals(cat) || Biome.BiomeCategory.SAVANNA.equals(cat)) {
-            event.getGeneration().getStructures().add(() -> ConfiguredStructureFeaturesPM.CONFIGURED_EARTH_SHRINE);
-        }
-        
-        // Add sea shrines to wet biomes
-        if (Biome.BiomeCategory.RIVER.equals(cat) || Biome.BiomeCategory.BEACH.equals(cat) || Biome.BiomeCategory.SWAMP.equals(cat) || Biome.BiomeCategory.ICY.equals(cat)) {
-            event.getGeneration().getStructures().add(() -> ConfiguredStructureFeaturesPM.CONFIGURED_SEA_SHRINE);
-        }
-        
-        // Add sky shrines to mountains
-        if (Biome.BiomeCategory.EXTREME_HILLS.equals(cat)) {
-            event.getGeneration().getStructures().add(() -> ConfiguredStructureFeaturesPM.CONFIGURED_SKY_SHRINE);
-        }
-        
-        // Add sun shrines to deserts
-        if (Biome.BiomeCategory.DESERT.equals(cat)) {
-            event.getGeneration().getStructures().add(() -> ConfiguredStructureFeaturesPM.CONFIGURED_SUN_SHRINE);
-        }
-        
-        // Add moon shrines to forests
+        // Add treefolk spawns and sunwood/moonwood trees to forests
         if (Biome.BiomeCategory.FOREST.equals(cat)) {
             event.getSpawns().getSpawner(MobCategory.CREATURE).add(new MobSpawnSettings.SpawnerData(EntityTypesPM.TREEFOLK.get(), 100, 1, 3));
             // TODO Phase sunwood and moonwood trees appropriately
             event.getGeneration().addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, VegetationPlacementsPM.TREES_WILD_SUNWOOD);
             event.getGeneration().addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, VegetationPlacementsPM.TREES_WILD_MOONWOOD);
-            event.getGeneration().getStructures().add(() -> ConfiguredStructureFeaturesPM.CONFIGURED_MOON_SHRINE);
         }
     }
 
