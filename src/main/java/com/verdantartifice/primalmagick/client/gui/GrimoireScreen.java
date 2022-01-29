2e865ad5960bdff5ef1649936fe76cfdf8e57258
daedalus4096 <michael.a.bunting@gmail.com>
2022-01-28 21:31:58 -0800

    Remove unused import

--- src/main/java/com/verdantartifice/primalmagick/client/gui/GrimoireScreen.java
+++ src/main/java/com/verdantartifice/primalmagick/client/gui/GrimoireScreen.java
@@ -3,7 +3,6 @@ package com.verdantartifice.primalmagick.client.gui;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.NavigableMap;
 import java.util.Optional;
