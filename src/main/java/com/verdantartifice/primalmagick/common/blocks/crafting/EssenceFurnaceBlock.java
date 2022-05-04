1bffbf8ed23b760aba473bc85c201f7b210049f7
daedalus4096 <michael.a.bunting@gmail.com>
2022-05-03 19:11:10 -0700

    Update base model

--- src/main/java/com/verdantartifice/primalmagick/common/blocks/crafting/EssenceFurnaceBlock.java
+++ src/main/java/com/verdantartifice/primalmagick/common/blocks/crafting/EssenceFurnaceBlock.java
@@ -2,19 +2,17 @@ package com.verdantartifice.primalmagick.common.blocks.crafting;
 
 import java.util.Random;
 
-import com.verdantartifice.primalmagick.PrimalMagick;
 import com.verdantartifice.primalmagick.common.tiles.TileEntityTypesPM;
 import com.verdantartifice.primalmagick.common.tiles.crafting.EssenceFurnaceTileEntity;
-import com.verdantartifice.primalmagick.common.util.VoxelShapeUtils;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -29,7 +27,7 @@ import net.minecraft.world.phys.shapes.VoxelShape;
  * @author Daedalus4096
  */
 public class EssenceFurnaceBlock extends AbstractCalcinatorBlock {
-    protected static final VoxelShape SHAPE = VoxelShapeUtils.fromModel(new ResourceLocation(PrimalMagick.MODID, "block/essence_furnace"));
+    protected static final VoxelShape SHAPE = Block.box(1D, 0D, 1D, 15D, 12D, 15D);
 
     @Override
     public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
