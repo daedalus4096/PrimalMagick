94ee0591bc14197ea02a30e5780507a9691e5760
daedalus4096 <michael.a.bunting@gmail.com>
2022-06-11 22:04:45 -0700

    Start void turbine particles ahead of the block

--- src/main/java/com/verdantartifice/primalmagick/common/blocks/devices/VoidTurbineBlock.java
+++ src/main/java/com/verdantartifice/primalmagick/common/blocks/devices/VoidTurbineBlock.java
@@ -1,11 +1,16 @@
 package com.verdantartifice.primalmagick.common.blocks.devices;
 
+import java.util.Random;
+
 import com.verdantartifice.primalmagick.client.fx.particles.ParticleTypesPM;
 import com.verdantartifice.primalmagick.common.sources.Source;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 
 /**
  * Definition of a wind generator block that pulls entities in.
@@ -26,5 +31,29 @@ public class VoidTurbineBlock extends AbstractWindGeneratorBlock {
     @Override
     public ParticleOptions getParticleType() {
         return ParticleTypesPM.VOID_SMOKE.get();
+    }
+
+    @Override
+    protected Vec3 getParticleStartPoint(BlockState state, Level level, BlockPos pos, Random random) {
+        Vec3 startPoint = super.getParticleStartPoint(state, level, pos, random);
+        
+        // Shift the start point forward along the block's facing vector
+        int power = level.getBestNeighborSignal(pos);
+        Direction facing = state.getValue(AbstractWindGeneratorBlock.FACING);
+        if (power < 1) {
+            return startPoint;
+        }
+        
+        // Calculate the distance of the shift, based on redstone power unless obstructed by a solid block
+        int lineOfSightPower = 0;
+        while (lineOfSightPower < power) {
+            if (!level.isEmptyBlock(pos.relative(facing, lineOfSightPower + 1))) {
+                break;
+            } else {
+                lineOfSightPower++;
+            }
+        }
+        
+        return startPoint.add(new Vec3(facing.step()).scale(lineOfSightPower));
     }
 }
