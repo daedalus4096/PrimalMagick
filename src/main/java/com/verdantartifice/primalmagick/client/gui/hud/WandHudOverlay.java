2508dd98aa6d89be5a30fdba9fd50b9a81495fcc
daedalus4096 <michael.a.bunting@gmail.com>
2022-07-02 22:25:30 -0700

    Render mana text near HUD gauges

--- src/main/java/com/verdantartifice/primalmagick/client/gui/hud/WandHudOverlay.java
+++ src/main/java/com/verdantartifice/primalmagick/client/gui/hud/WandHudOverlay.java
@@ -1,5 +1,7 @@
 package com.verdantartifice.primalmagick.client.gui.hud;
 
+import java.awt.Color;
+
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.verdantartifice.primalmagick.PrimalMagick;
@@ -7,7 +9,10 @@ import com.verdantartifice.primalmagick.common.sources.Source;
 import com.verdantartifice.primalmagick.common.wands.IWand;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
@@ -28,31 +33,52 @@ public class WandHudOverlay implements IIngameOverlay {
         if (!mc.options.hideGui && !mc.player.isSpectator()) {
             ItemStack stack = mc.player.getItemBySlot(EquipmentSlot.MAINHAND);
             if (stack.getItem() instanceof IWand wand) {
-                this.renderHud(poseStack, stack, wand, partialTick);
+                this.renderHud(mc, poseStack, stack, wand, partialTick);
             }
         }
     }
 
-    private void renderHud(PoseStack poseStack, ItemStack stack, IWand wand, float partialTick) {
+    private void renderHud(Minecraft mc, PoseStack poseStack, ItemStack stack, IWand wand, float partialTick) {
         poseStack.pushPose();
         
         int posY = 0;
+        int maxMana = wand.getMaxMana(stack);
+        Component maxText = wand.getMaxManaText(stack);
         for (Source source : Source.SORTED_SOURCES) {
-            posY += this.renderManaGauge(poseStack, 0, posY, stack, wand, source.getColor(), partialTick);
+            int curMana = wand.getMana(stack, source);
+            Component curText = wand.getManaText(stack, source);
+            
+            double ratio = (double)curMana / (double)maxMana;
+            Component ratioText = Component.translatable("primalmagick.source.mana_summary_fragment", curText, maxText);
+            
+            posY += this.renderManaGauge(poseStack, 0, posY, ratioText, ratio, source.getColor(), partialTick, mc.font);
         }
         
         poseStack.popPose();
     }
 
-    private int renderManaGauge(PoseStack poseStack, int x, int y, ItemStack stack, IWand wand, int color, float partialTick) {
+    private int renderManaGauge(PoseStack poseStack, int x, int y, Component text, double ratio, int color, float partialTick, Font font) {
         RenderSystem.enableBlend();
         RenderSystem.defaultBlendFunc();
         RenderSystem.setShaderTexture(0, HUD_TEXTURE);
+        
+        // Render the gauge background
         RenderSystem.setShaderColor(1, 1, 1, 1);
         GuiComponent.blit(poseStack, x, y, 0, 0, 50, 12, 256, 256);
+        
+        // Render the gauge mana bar
         RenderSystem.setShaderColor(getRed(color), getGreen(color), getBlue(color), 1);
         GuiComponent.blit(poseStack, x + 5, y + 2, 5, 12, 40, 8, 256, 256);
+        
+        // Render the mana text by the gauge if holding shift
         RenderSystem.setShaderColor(1, 1, 1, 1);
+        if (Screen.hasShiftDown()) {
+            poseStack.pushPose();
+            poseStack.translate(52, 2, 0);
+            font.drawShadow(poseStack, text, x, y, Color.WHITE.getRGB());
+            poseStack.popPose();
+        }
+        
         return 12;
     }
     
