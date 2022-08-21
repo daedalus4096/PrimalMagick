b3ed5de78c0c35741ab9ffee240021f1597f6469
daedalus4096 <michael.a.bunting@gmail.com>
2022-08-21 16:38:03 -0700

    Show required research for arcane workbench recipes

--- src/main/java/com/verdantartifice/primalmagick/client/compat/jei/arcane_crafting/ArcaneCraftingRecipeCategory.java
+++ src/main/java/com/verdantartifice/primalmagick/client/compat/jei/arcane_crafting/ArcaneCraftingRecipeCategory.java
@@ -9,6 +9,12 @@ import com.verdantartifice.primalmagick.client.compat.jei.RecipeCategoryPM;
 import com.verdantartifice.primalmagick.client.compat.jei.JeiRecipeTypesPM;
 import com.verdantartifice.primalmagick.common.crafting.IArcaneRecipe;
 import com.verdantartifice.primalmagick.common.items.ItemsPM;
+import com.verdantartifice.primalmagick.common.research.CompoundResearchKey;
+import com.verdantartifice.primalmagick.common.research.ResearchDiscipline;
+import com.verdantartifice.primalmagick.common.research.ResearchDisciplines;
+import com.verdantartifice.primalmagick.common.research.ResearchEntries;
+import com.verdantartifice.primalmagick.common.research.ResearchEntry;
+import com.verdantartifice.primalmagick.common.research.SimpleResearchKey;
 import com.verdantartifice.primalmagick.common.sources.Source;
 import com.verdantartifice.primalmagick.common.sources.SourceList;
 
@@ -21,6 +27,7 @@ import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeType;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.crafting.IShapedRecipe;
@@ -33,16 +40,21 @@ import net.minecraftforge.common.crafting.IShapedRecipe;
 public class ArcaneCraftingRecipeCategory extends RecipeCategoryPM<IArcaneRecipe> {
     public static final ResourceLocation UID = new ResourceLocation(PrimalMagick.MODID, "arcane_workbench");
     private static final ResourceLocation BACKGROUND_TEXTURE = new ResourceLocation(PrimalMagick.MODID, "textures/gui/jei/arcane_workbench.png");
+    private static final ResourceLocation RESEARCH_TEXTURE = new ResourceLocation("textures/item/book.png");
     private static final int MANA_COST_X_OFFSET = 64;
     private static final int MANA_COST_Y_OFFSET = 1;
+    private static final int RESEARCH_X_OFFSET = 64;
+    private static final int RESEARCH_Y_OFFSET = 36;
     
     private final ICraftingGridHelper craftingGridHelper;
     private final IDrawableStatic manaCostIcon;
+    private final IDrawableStatic researchIcon;
 
     public ArcaneCraftingRecipeCategory(IGuiHelper guiHelper) {
         super(guiHelper, UID, "block.primalmagick.arcane_workbench");
         this.craftingGridHelper = guiHelper.createCraftingGridHelper();
         this.manaCostIcon = guiHelper.createDrawable(BACKGROUND_TEXTURE, 116, 0, 16, 16);
+        this.researchIcon = guiHelper.drawableBuilder(RESEARCH_TEXTURE, 0, 0, 16, 16).setTextureSize(16, 16).build();
         this.setBackground(guiHelper.createDrawable(BACKGROUND_TEXTURE, 0, 0, 116, 54));
         this.setIcon(new ItemStack(ItemsPM.ARCANE_WORKBENCH.get()));
     }
@@ -64,11 +76,15 @@ public class ArcaneCraftingRecipeCategory extends RecipeCategoryPM<IArcaneRecipe
         if (recipe.getManaCosts() != null && !recipe.getManaCosts().isEmpty()) {
             this.manaCostIcon.draw(stack, MANA_COST_X_OFFSET, MANA_COST_Y_OFFSET);
         }
+        if (recipe.getRequiredResearch() != null && !recipe.getRequiredResearch().getKeys().isEmpty()) {
+            this.researchIcon.draw(stack, RESEARCH_X_OFFSET, RESEARCH_Y_OFFSET);
+        }
     }
 
     @Override
     public List<Component> getTooltipStrings(IArcaneRecipe recipe, IRecipeSlotsView recipeSlotsView, double mouseX, double mouseY) {
         SourceList manaCosts = recipe.getManaCosts();
+        CompoundResearchKey compoundResearch = recipe.getRequiredResearch();
         if ( manaCosts != null && !manaCosts.isEmpty() && 
              mouseX >= MANA_COST_X_OFFSET && mouseX < MANA_COST_X_OFFSET + this.manaCostIcon.getWidth() &&
              mouseY >= MANA_COST_Y_OFFSET && mouseY < MANA_COST_Y_OFFSET + this.manaCostIcon.getHeight() ) {
@@ -76,6 +92,27 @@ public class ArcaneCraftingRecipeCategory extends RecipeCategoryPM<IArcaneRecipe
             tooltip.add(Component.translatable("primalmagick.crafting.mana_cost_header"));
             for (Source source : manaCosts.getSourcesSorted()) {
                 tooltip.add(Component.translatable("primalmagick.crafting.mana_tooltip", manaCosts.getAmount(source), source.getNameText()));
+            }
+            return tooltip;
+        } else if ( compoundResearch != null && !compoundResearch.getKeys().isEmpty() &&
+                    mouseX >= RESEARCH_X_OFFSET && mouseX < RESEARCH_X_OFFSET + this.researchIcon.getWidth() &&
+                    mouseY >= RESEARCH_Y_OFFSET && mouseY < RESEARCH_Y_OFFSET + this.researchIcon.getHeight() ) {
+            List<Component> tooltip = new ArrayList<>();
+            tooltip.add(Component.translatable("primalmagick.crafting.research_header"));
+            for (SimpleResearchKey key : compoundResearch.getKeys()) {
+                ResearchEntry entry = ResearchEntries.getEntry(key);
+                if (entry == null) {
+                    tooltip.add(Component.translatable("primalmagick.research." + key.getRootKey() + ".text"));
+                } else {
+                    MutableComponent comp = Component.translatable(entry.getNameTranslationKey());
+                    ResearchDiscipline disc = ResearchDisciplines.getDiscipline(entry.getDisciplineKey());
+                    if (disc != null) {
+                        comp.append(Component.literal(" ("));
+                        comp.append(Component.translatable(disc.getNameTranslationKey()));
+                        comp.append(Component.literal(")"));
+                    }
+                    tooltip.add(comp);
+                }
             }
             return tooltip;
         } else {
