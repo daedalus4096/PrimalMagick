b0c74502da2493458abd6659a88759f4f6de1955
daedalus4096 <michael.a.bunting@gmail.com>
2022-08-21 17:08:21 -0700

    Show required research for ritual recipes

--- src/main/java/com/verdantartifice/primalmagick/client/compat/jei/ritual/RitualRecipeCategory.java
+++ src/main/java/com/verdantartifice/primalmagick/client/compat/jei/ritual/RitualRecipeCategory.java
@@ -9,6 +9,12 @@ import com.verdantartifice.primalmagick.client.compat.jei.RecipeCategoryPM;
 import com.verdantartifice.primalmagick.client.compat.jei.JeiRecipeTypesPM;
 import com.verdantartifice.primalmagick.common.crafting.IRitualRecipe;
 import com.verdantartifice.primalmagick.common.items.ItemsPM;
+import com.verdantartifice.primalmagick.common.research.CompoundResearchKey;
+import com.verdantartifice.primalmagick.common.research.ResearchDiscipline;
+import com.verdantartifice.primalmagick.common.research.ResearchDisciplines;
+import com.verdantartifice.primalmagick.common.research.ResearchEntries;
+import com.verdantartifice.primalmagick.common.research.ResearchEntry;
+import com.verdantartifice.primalmagick.common.research.SimpleResearchKey;
 import com.verdantartifice.primalmagick.common.sources.Source;
 import com.verdantartifice.primalmagick.common.sources.SourceList;
 
@@ -21,7 +27,7 @@ import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
-
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
@@ -34,14 +40,19 @@ import net.minecraft.world.item.crafting.Ingredient;
 public class RitualRecipeCategory extends RecipeCategoryPM<IRitualRecipe> {
     public static final ResourceLocation UID = new ResourceLocation(PrimalMagick.MODID, "ritual_altar");
     private static final ResourceLocation BACKGROUND_TEXTURE = new ResourceLocation(PrimalMagick.MODID, "textures/gui/jei/ritual_altar.png");
+    private static final ResourceLocation RESEARCH_TEXTURE = new ResourceLocation(PrimalMagick.MODID, "textures/item/grimoire.png");
     private static final int MANA_COST_X_OFFSET = 118;
     private static final int MANA_COST_Y_OFFSET = 14;
+    private static final int RESEARCH_X_OFFSET = 118;
+    private static final int RESEARCH_Y_OFFSET = 49;
 
     private final IDrawableStatic manaCostIcon;
-    
+    private final IDrawableStatic researchIcon;
+
     public RitualRecipeCategory(IGuiHelper guiHelper) {
         super(guiHelper, UID, "block.primalmagick.ritual_altar");
         this.manaCostIcon = guiHelper.createDrawable(BACKGROUND_TEXTURE, 170, 0, 16, 16);
+        this.researchIcon = guiHelper.drawableBuilder(RESEARCH_TEXTURE, 0, 0, 32, 32).setTextureSize(32, 32).build();
         this.setBackground(guiHelper.createDrawable(BACKGROUND_TEXTURE, 0, 0, 170, 80));
         this.setIcon(new ItemStack(ItemsPM.RITUAL_ALTAR.get()));
     }
@@ -71,11 +82,18 @@ public class RitualRecipeCategory extends RecipeCategoryPM<IRitualRecipe> {
         if (recipe.getManaCosts() != null && !recipe.getManaCosts().isEmpty()) {
             this.manaCostIcon.draw(stack, MANA_COST_X_OFFSET, MANA_COST_Y_OFFSET);
         }
+        if (recipe.getRequiredResearch() != null && !recipe.getRequiredResearch().getKeys().isEmpty()) {
+            stack.pushPose();
+            stack.scale(0.5F, 0.5F, 0.5F);
+            this.researchIcon.draw(stack, RESEARCH_X_OFFSET * 2, RESEARCH_Y_OFFSET * 2);
+            stack.popPose();
+        }
     }
 
     @Override
     public List<Component> getTooltipStrings(IRitualRecipe recipe, IRecipeSlotsView recipeSlotsView, double mouseX, double mouseY) {
         SourceList manaCosts = recipe.getManaCosts();
+        CompoundResearchKey compoundResearch = recipe.getRequiredResearch();
         if ( manaCosts != null && !manaCosts.isEmpty() && 
              mouseX >= MANA_COST_X_OFFSET && mouseX < MANA_COST_X_OFFSET + this.manaCostIcon.getWidth() &&
              mouseY >= MANA_COST_Y_OFFSET && mouseY < MANA_COST_Y_OFFSET + this.manaCostIcon.getHeight() ) {
@@ -83,6 +101,27 @@ public class RitualRecipeCategory extends RecipeCategoryPM<IRitualRecipe> {
             tooltip.add(Component.translatable("primalmagick.crafting.mana_cost_header"));
             for (Source source : manaCosts.getSourcesSorted()) {
                 tooltip.add(Component.translatable("primalmagick.crafting.mana_tooltip", manaCosts.getAmount(source), source.getNameText()));
+            }
+            return tooltip;
+        } else if ( compoundResearch != null && !compoundResearch.getKeys().isEmpty() &&
+                    mouseX >= RESEARCH_X_OFFSET && mouseX < RESEARCH_X_OFFSET + this.researchIcon.getWidth() &&
+                    mouseY >= RESEARCH_Y_OFFSET && mouseY < RESEARCH_Y_OFFSET + this.researchIcon.getHeight() ) {
+            List<Component> tooltip = new ArrayList<>();
+            tooltip.add(Component.translatable("primalmagick.crafting.research_header"));
+            for (SimpleResearchKey key : compoundResearch.getKeys()) {
+                ResearchEntry entry = ResearchEntries.getEntry(key);
+                if (entry == null) {
+                    tooltip.add(Component.translatable("primalmagick.research." + key.getRootKey() + ".text"));
+                } else {
+                    MutableComponent comp = Component.translatable(entry.getNameTranslationKey());
+                    ResearchDiscipline disc = ResearchDisciplines.getDiscipline(entry.getDisciplineKey());
+                    if (disc != null) {
+                        comp.append(Component.literal(" ("));
+                        comp.append(Component.translatable(disc.getNameTranslationKey()));
+                        comp.append(Component.literal(")"));
+                    }
+                    tooltip.add(comp);
+                }
             }
             return tooltip;
         } else {
