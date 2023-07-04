5471b1454f44b4901fa1f649718c9e5f440a2f46
daedalus4096 <michael.a.bunting@gmail.com>
2023-07-03 17:43:29 -0700

    Remove unused import

--- src/main/java/com/verdantartifice/primalmagick/datagen/biome_modifiers/BiomeModifierProvider.java
+++ src/main/java/com/verdantartifice/primalmagick/datagen/biome_modifiers/BiomeModifierProvider.java
@@ -22,7 +22,6 @@ import net.minecraft.data.CachedOutput;
 import net.minecraft.data.DataProvider;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.RegistryOps;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.PackType;
 import net.minecraft.tags.BiomeTags;
