0ac3107dfb91660eb0f4ea2b81be69af0cd51916
daedalus4096 <michael.a.bunting@gmail.com>
2023-07-09 14:16:24 -0700

    Remove unwanted text shadows

--- src/main/java/com/verdantartifice/primalmagick/client/toast/ResearchToast.java
+++ src/main/java/com/verdantartifice/primalmagick/client/toast/ResearchToast.java
@@ -38,7 +38,7 @@ public class ResearchToast implements Toast {
         
         // Render the toast title text
         Component titleText = this.isComplete ? Component.translatable("primalmagick.toast.completed.title") : Component.translatable("primalmagick.toast.revealed.title");
-        guiGraphics.drawString(mc.font, titleText, 6, 7, this.isComplete ? Source.VOID.getColor() : Source.INFERNAL.getColor());
+        guiGraphics.drawString(mc.font, titleText, 6, 7, this.isComplete ? Source.VOID.getColor() : Source.INFERNAL.getColor(), false);
         
         // Render the description of the completed research
         Component descText = Component.translatable(this.entry.getNameTranslationKey());
