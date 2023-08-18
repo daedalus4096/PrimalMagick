9dce692d3269d8f101e3344c97fabef352fbe6c1
daedalus4096 <michael.a.bunting@gmail.com>
2023-08-18 16:05:10 -0700

    Generate primalite weapon assets

--- src/main/java/com/verdantartifice/primalmagick/datagen/items/ItemModelProviderPM.java
+++ src/main/java/com/verdantartifice/primalmagick/datagen/items/ItemModelProviderPM.java
@@ -3,10 +3,14 @@ package com.verdantartifice.primalmagick.datagen.items;
 import com.verdantartifice.primalmagick.PrimalMagick;
 import com.verdantartifice.primalmagick.common.items.ItemsPM;
 
+import net.minecraft.client.renderer.block.model.BlockModel.GuiLight;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BowItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.TridentItem;
 import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.client.model.generators.ModelFile;
@@ -50,17 +54,109 @@ public class ItemModelProviderPM extends ItemModelProvider {
         this.basicItem(ItemsPM.HEXIUM_NUGGET.get());
         this.basicItem(ItemsPM.HALLOWSTEEL_NUGGET.get());
         this.basicItem(ItemsPM.QUARTZ_NUGGET.get());
+        
+        // Generate tool items
+        this.handheldItem(ItemsPM.PRIMALITE_SWORD.get());
+        this.tridentItem(ItemsPM.PRIMALITE_TRIDENT.get());
+        this.bowItem(ItemsPM.PRIMALITE_BOW.get());
     }
     
     private ResourceLocation key(Item item) {
         return ForgeRegistries.ITEMS.getKey(item);
     }
     
-    private ResourceLocation defaultModel(Item item) {
+    private ItemModelBuilder builder(Item item) {
+        return this.builder(this.key(item));
+    }
+    
+    private ItemModelBuilder builder(ResourceLocation loc) {
+        return this.getBuilder(loc.toString());
+    }
+    
+    private ResourceLocation defaultModelLoc(Item item) {
         return this.key(item).withPrefix(ModelProvider.ITEM_FOLDER + "/");
     }
     
+    private ModelFile existingModel(ResourceLocation modelLoc) {
+        return new ModelFile.ExistingModelFile(modelLoc, this.existingFileHelper);
+    }
+    
+    private ModelFile uncheckedModel(ResourceLocation modelLoc) {
+        return new ModelFile.UncheckedModelFile(modelLoc);
+    }
+    
     private ItemModelBuilder itemWithParent(Item item, Item parent) {
-        return this.getBuilder(this.key(item).toString()).parent(new ModelFile.ExistingModelFile(this.defaultModel(parent), this.existingFileHelper));
+        return this.itemWithParent(item, this.defaultModelLoc(parent));
+    }
+    
+    private ItemModelBuilder itemWithParent(Item item, ResourceLocation parent) {
+        return this.builder(item).parent(this.existingModel(parent));
+    }
+    
+    private ItemModelBuilder handheldItem(Item item) {
+        return this.builder(item).parent(this.existingModel(new ResourceLocation("item/handheld"))).texture("layer0", this.defaultModelLoc(item));
+    }
+    
+    private ItemModelBuilder tridentItem(TridentItem item) {
+        ItemModelBuilder throwingModel = this.tridentThrowingModel(item);
+        ItemModelBuilder inHandModel = this.tridentInHandModel(item, throwingModel);
+        return this.basicItem(item)
+                .override().predicate(new ResourceLocation("throwing"), 0).model(inHandModel).end()
+                .override().predicate(new ResourceLocation("throwing"), 1).model(throwingModel).end();
+    }
+    
+    private ItemModelBuilder tridentInHandModel(TridentItem item, ModelFile throwingModel) {
+        return this.builder(this.key(item).withSuffix("_in_hand"))
+                .parent(new ModelFile.UncheckedModelFile("builtin/entity"))
+                .guiLight(GuiLight.FRONT)
+                .texture("particle", this.defaultModelLoc(item))
+                .override().predicate(new ResourceLocation("throwing"), 1).model(throwingModel).end()
+                .transforms()
+                        .transform(ItemDisplayContext.THIRD_PERSON_RIGHT_HAND).rotation(0, 60, 0).translation(11, 17, -2).scale(1).end()
+                        .transform(ItemDisplayContext.THIRD_PERSON_LEFT_HAND).rotation(0, 60, 0).translation(3, 17, 12).scale(1).end()
+                        .transform(ItemDisplayContext.FIRST_PERSON_RIGHT_HAND).rotation(0, -90, 25).translation(-3, 17, 1).scale(1).end()
+                        .transform(ItemDisplayContext.FIRST_PERSON_LEFT_HAND).rotation(0, 90, -25).translation(13, 17, 1).scale(1).end()
+                        .transform(ItemDisplayContext.GUI).rotation(0, 0, 0).translation(8, 8, 8).scale(1).end()
+                        .transform(ItemDisplayContext.FIXED).rotation(0, 0, 0).translation(8, 8, 8).scale(1).end()
+                        .transform(ItemDisplayContext.GROUND).rotation(0, 0, 0).translation(8, 8, 8).scale(1).end()
+                        .end();
+    }
+    
+    private ItemModelBuilder tridentThrowingModel(TridentItem item) {
+        return this.builder(this.key(item).withSuffix("_throwing"))
+                .parent(new ModelFile.UncheckedModelFile("builtin/entity"))
+                .guiLight(GuiLight.FRONT)
+                .texture("particle", this.defaultModelLoc(item))
+                .transforms()
+                        .transform(ItemDisplayContext.THIRD_PERSON_RIGHT_HAND).rotation(0, 90, 180).translation(8, -17, 9).scale(1).end()
+                        .transform(ItemDisplayContext.THIRD_PERSON_LEFT_HAND).rotation(0, 90, 180).translation(8, -17, -7).scale(1).end()
+                        .transform(ItemDisplayContext.FIRST_PERSON_RIGHT_HAND).rotation(0, -90, 25).translation(-3, 17, 1).scale(1).end()
+                        .transform(ItemDisplayContext.FIRST_PERSON_LEFT_HAND).rotation(0, 90, -25).translation(13, 17, 1).scale(1).end()
+                        .transform(ItemDisplayContext.GUI).rotation(0, 0, 0).translation(8, 8, 8).scale(1).end()
+                        .transform(ItemDisplayContext.FIXED).rotation(0, 0, 0).translation(8, 8, 8).scale(1).end()
+                        .transform(ItemDisplayContext.GROUND).rotation(0, 0, 0).translation(8, 8, 8).scale(1).end()
+                        .end();
+    }
+    
+    private ItemModelBuilder bowItem(BowItem item) {
+        ResourceLocation pulling = new ResourceLocation("pulling");
+        ResourceLocation pull = new ResourceLocation("pull");
+        return this.builder(item)
+                .parent(new ModelFile.UncheckedModelFile("item/generated"))
+                .texture("layer0", this.defaultModelLoc(item))
+                .override().predicate(pulling, 1).model(this.bowPullingModel(item, 0)).end()
+                .override().predicate(pulling, 1).predicate(pull, 0.65F).model(this.bowPullingModel(item, 1)).end()
+                .override().predicate(pulling, 1).predicate(pull, 0.9F).model(this.bowPullingModel(item, 2)).end()
+                .transforms()
+                        .transform(ItemDisplayContext.THIRD_PERSON_RIGHT_HAND).rotation(-80, 260, -40).translation(-1, -2, 2.5F).scale(0.9F).end()
+                        .transform(ItemDisplayContext.THIRD_PERSON_LEFT_HAND).rotation(-80, -280, 40).translation(-1, -2, 2.5F).scale(0.9F).end()
+                        .transform(ItemDisplayContext.FIRST_PERSON_RIGHT_HAND).rotation(0, -90, 25).translation(1.13F, 3.2F, 1.13F).scale(0.68F).end()
+                        .transform(ItemDisplayContext.FIRST_PERSON_LEFT_HAND).rotation(0, 90, -25).translation(1.13F, 3.2F, 1.13F).scale(0.68F).end()
+                        .end();
+    }
+    
+    private ItemModelBuilder bowPullingModel(BowItem item, int stage) {
+        ResourceLocation stageKey = this.key(item).withSuffix("_pulling_" + stage);
+        return this.builder(stageKey).parent(this.uncheckedModel(this.defaultModelLoc(item))).texture("layer0", stageKey.withPrefix(ModelProvider.ITEM_FOLDER + "/"));
     }
 }
