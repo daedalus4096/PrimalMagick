53064d098117431d99564d0a58950b012dedcbaa
daedalus4096 <michael.a.bunting@gmail.com>
2023-08-19 23:52:04 -0700

    Test generation for wand cap assets

--- src/main/java/com/verdantartifice/primalmagick/datagen/blocks/WandComponentBlockStateProvider.java
+++ src/main/java/com/verdantartifice/primalmagick/datagen/blocks/WandComponentBlockStateProvider.java
@@ -3,6 +3,7 @@ package com.verdantartifice.primalmagick.datagen.blocks;
 import com.verdantartifice.primalmagick.PrimalMagick;
 import com.verdantartifice.primalmagick.common.items.ItemsPM;
 import com.verdantartifice.primalmagick.common.items.wands.StaffCoreItem;
+import com.verdantartifice.primalmagick.common.items.wands.WandCapItem;
 import com.verdantartifice.primalmagick.common.items.wands.WandCoreItem;
 import com.verdantartifice.primalmagick.common.wands.IWandComponent;
 
@@ -35,6 +36,7 @@ public class WandComponentBlockStateProvider extends AbstractSpecialBlockStatePr
         // Generate wand component blockstates and items
         this.wandCoreWithItem(ItemsPM.HEARTWOOD_WAND_CORE_ITEM.get());
         this.staffCoreWithItem(ItemsPM.HEARTWOOD_STAFF_CORE_ITEM.get());
+        this.wandCapWithItem(ItemsPM.IRON_WAND_CAP_ITEM.get());
     }
     
     private ResourceLocation modelLoc(ResourceLocation key) {
@@ -42,25 +44,34 @@ public class WandComponentBlockStateProvider extends AbstractSpecialBlockStatePr
     }
     
     private void wandCoreWithItem(WandCoreItem coreItem) {
-        this.componentWithItem(coreItem, coreItem.getWandCore(), "wand_core");
+        this.componentWithItem(coreItem, coreItem.getWandCore(), "wand_core", "core");
     }
     
     private void staffCoreWithItem(StaffCoreItem coreItem) {
-        this.componentWithItem(coreItem, coreItem.getWandCore(), "staff_core", "wand_core");    // Staff cores use the same model texture as wand cores
+        this.componentWithItem(coreItem, coreItem.getWandCore(), "staff_core", "core", "wand_core");    // Staff cores use the same model texture as wand cores
     }
     
-    private void componentWithItem(Item item, IWandComponent component, String componentSuffix) {
-        this.componentWithItem(item, component, componentSuffix, componentSuffix);
+    private void wandCapWithItem(WandCapItem capItem) {
+        this.componentWithItem(capItem, capItem.getWandCap(), "wand_cap", "cap");
+        this.component(capItem.getWandCap(), "staff_cap", "cap", "wand_cap");   // Staff caps use the same model texture as wand caps and have no corresponding item
     }
     
-    private void componentWithItem(Item item, IWandComponent component, String modelComponentSuffix, String textureComponentSuffix) {
+    private void componentWithItem(Item item, IWandComponent component, String componentSuffix, String textureComponentName) {
+        this.componentWithItem(item, component, componentSuffix, textureComponentName, componentSuffix);
+    }
+    
+    private void component(IWandComponent component, String modelComponentSuffix, String textureComponentName, String textureComponentSuffix) {
         ResourceLocation modelParent = PrimalMagick.resource("item/" + modelComponentSuffix);
         ResourceLocation modelKey = PrimalMagick.resource(component.getTag() + "_" + modelComponentSuffix);
         ResourceLocation textureKey = PrimalMagick.resource(component.getTag() + "_" + textureComponentSuffix);
         String modelName = modelKey.getPath();
         ResourceLocation textureLoc = this.modelLoc(textureKey);
-        ModelFile model = this.models().withExistingParent(modelName, modelParent).texture("core", textureLoc);
+        ModelFile model = this.models().withExistingParent(modelName, modelParent).texture(textureComponentName, textureLoc);
         this.getSpecialBuilder(modelKey).setModels(new ConfiguredModel(model));
+    }
+    
+    private void componentWithItem(Item item, IWandComponent component, String modelComponentSuffix, String textureComponentName, String textureComponentSuffix) {
+        this.component(component, modelComponentSuffix, textureComponentName, textureComponentSuffix);
         this.itemModels().basicItem(item);
     }
 }
