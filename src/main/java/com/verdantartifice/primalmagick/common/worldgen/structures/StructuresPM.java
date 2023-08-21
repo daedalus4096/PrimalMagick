4167e2ed11af21ec5ecc6c9e3e8c465d8a131c2e
daedalus4096 <michael.a.bunting@gmail.com>
2023-08-20 20:23:00 -0700

    Generate remaining shrine structure data

--- src/main/java/com/verdantartifice/primalmagick/common/worldgen/structures/StructuresPM.java
+++ src/main/java/com/verdantartifice/primalmagick/common/worldgen/structures/StructuresPM.java
@@ -24,6 +24,10 @@ import net.minecraft.world.level.levelgen.structure.TerrainAdjustment;
  */
 public class StructuresPM {
     public static final ResourceKey<Structure> EARTH_SHRINE = registryKey("earth_shrine");
+    public static final ResourceKey<Structure> SEA_SHRINE = registryKey("sea_shrine");
+    public static final ResourceKey<Structure> SKY_SHRINE = registryKey("sky_shrine");
+    public static final ResourceKey<Structure> SUN_SHRINE = registryKey("sun_shrine");
+    public static final ResourceKey<Structure> MOON_SHRINE = registryKey("moon_shrine");
     
     private static ResourceKey<Structure> registryKey(String name) {
         return ResourceKey.create(Registries.STRUCTURE, PrimalMagick.resource(name));
@@ -43,6 +47,10 @@ public class StructuresPM {
     
     public static void bootstrap(BootstapContext<Structure> context) {
         HolderGetter<Biome> biomeGetter = context.lookup(Registries.BIOME);
-        register(context, EARTH_SHRINE, biomeGetter.getOrThrow(BiomeTagsPM.HAS_EARTH_SHRINE), ShrineStructure.Type.EARTH);
+        register(context, StructuresPM.EARTH_SHRINE, biomeGetter.getOrThrow(BiomeTagsPM.HAS_EARTH_SHRINE), ShrineStructure.Type.EARTH);
+        register(context, StructuresPM.SEA_SHRINE, biomeGetter.getOrThrow(BiomeTagsPM.HAS_SEA_SHRINE), ShrineStructure.Type.SEA);
+        register(context, StructuresPM.SKY_SHRINE, biomeGetter.getOrThrow(BiomeTagsPM.HAS_SKY_SHRINE), ShrineStructure.Type.SKY);
+        register(context, StructuresPM.SUN_SHRINE, biomeGetter.getOrThrow(BiomeTagsPM.HAS_SUN_SHRINE), ShrineStructure.Type.SUN);
+        register(context, StructuresPM.MOON_SHRINE, biomeGetter.getOrThrow(BiomeTagsPM.HAS_MOON_SHRINE), ShrineStructure.Type.MOON);
     }
 }
