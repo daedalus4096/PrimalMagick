529242c924de9d80e016a6034e115d829859b7aa
daedalus4096 <michael.a.bunting@gmail.com>
2023-08-27 14:59:41 -0700

    Fix loot tables

--- src/main/java/com/verdantartifice/primalmagick/datagen/loot_tables/BlockLootTables.java
+++ src/main/java/com/verdantartifice/primalmagick/datagen/loot_tables/BlockLootTables.java
@@ -77,8 +77,8 @@ public class BlockLootTables extends AbstractBlockLootTableProvider {
         this.registerBasicTable(BlocksPM.ESSENCE_CASK_FORBIDDEN.get());
         this.registerBasicTable(BlocksPM.ESSENCE_CASK_HEAVENLY.get());
         this.registerBasicTable(BlocksPM.WAND_GLAMOUR_TABLE.get());
-        this.registerBasicTable(BlocksPM.INFERNAL_FURNACE.get());
-        this.registerBasicTable(BlocksPM.MANA_NEXUS.get());
+        this.registerManaBearingDeviceTable(BlocksPM.INFERNAL_FURNACE.get());
+        this.registerManaBearingDeviceTable(BlocksPM.MANA_NEXUS.get());
 
         // Register misc loot tables
         this.registerBasicTable(BlocksPM.SALT_TRAIL.get());
