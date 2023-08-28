8589d7a411a8edec3123a0f1c87c67df5806d09b
daedalus4096 <michael.a.bunting@gmail.com>
2023-08-27 18:42:47 -0700

    Only show mana tooltips for discovered sources

--- src/main/java/com/verdantartifice/primalmagick/common/sources/ManaContainerHelper.java
+++ src/main/java/com/verdantartifice/primalmagick/common/sources/ManaContainerHelper.java
@@ -2,12 +2,17 @@ package com.verdantartifice.primalmagick.common.sources;
 
 import java.util.List;
 
+import com.verdantartifice.primalmagick.client.util.ClientUtils;
+
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 /**
  * Static class containing helper methods to assist with common mana container tasks.
@@ -16,11 +21,12 @@ import net.minecraft.world.level.block.entity.BlockEntity;
  */
 public class ManaContainerHelper {
     public static void appendHoverText(ItemStack stack, List<Component> tooltip) {
+        Player player = (FMLEnvironment.dist == Dist.CLIENT) ? ClientUtils.getCurrentPlayer() : null;
         CompoundTag nbt = stack.getTagElement("ManaContainerTag");
         if (nbt != null) {
             SourceList mana = new SourceList();
             mana.deserializeNBT(nbt);
-            Source.SORTED_SOURCES.stream().filter(source -> mana.getAmount(source) > 0).forEach(source ->
+            Source.SORTED_SOURCES.stream().filter(source -> source.isDiscovered(player) && mana.getAmount(source) > 0).forEach(source ->
                 tooltip.add(Component.translatable("tooltip.primalmagick.source.mana_container", source.getNameText(), (mana.getAmount(source) / 100.0D)))
             );
         }
