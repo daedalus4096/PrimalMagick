cfec5eb2a23eb8c7350d132a7981786afa9e2448
daedalus4096 <michael.a.bunting@gmail.com>
2023-09-03 15:19:07 -0700

    Mana bar fixes

--- src/main/java/com/verdantartifice/primalmagick/client/gui/hud/ManaStorageItemDecorator.java
+++ src/main/java/com/verdantartifice/primalmagick/client/gui/hud/ManaStorageItemDecorator.java
@@ -32,16 +32,22 @@ public class ManaStorageItemDecorator implements IItemDecorator {
             int width = this.getBarWidth(stack, this.source);
             int color = this.getBarColor(stack, this.source);
             int xPos = xOffset + 2;
-            int yPos = yOffset + 13 - (stack.isBarVisible() ? 1 : 0);   // Bump up the mana bar by a pixel if the durability bar is visible
-            guiGraphics.fill(RenderType.guiOverlay(), xPos, yPos, xPos + 13, yPos + 2, -16777216);
-            guiGraphics.fill(RenderType.guiOverlay(), xPos, yPos, xPos + width, yPos + 1, color | -16777216);
+            int yPos = yOffset + 13;
+            if (stack.isBarVisible()) {
+                // Bump up the mana bar by a pixel if the durability bar is visible
+                guiGraphics.fill(RenderType.guiOverlay(), xPos, yPos - 1, xPos + 13, yPos, -16777216);
+                guiGraphics.fill(RenderType.guiOverlay(), xPos, yPos - 1, xPos + width, yPos, color | -16777216);
+            } else {
+                guiGraphics.fill(RenderType.guiOverlay(), xPos, yPos, xPos + 13, yPos + 2, -16777216);
+                guiGraphics.fill(RenderType.guiOverlay(), xPos, yPos, xPos + width, yPos + 1, color | -16777216);
+            }
         });
         return false;
     }
 
     protected int getBarWidth(ItemStack stack, Source source) {
         return stack.getCapability(PrimalMagickCapabilities.MANA_STORAGE).filter(manaCap -> manaCap.canStore(this.source)).map(manaCap -> {
-            return Math.round(13.0F - (float)manaCap.getManaStored(source) * 13.0F / (float)manaCap.getMaxManaStored(source));
+            return Math.round((float)manaCap.getManaStored(source) * 13.0F / (float)manaCap.getMaxManaStored(source));
         }).orElse(0);
     }
     
