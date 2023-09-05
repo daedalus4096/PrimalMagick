4a83971c2ccba72db48485267718edb47af9eb2a
daedalus4096 <michael.a.bunting@gmail.com>
2023-09-04 22:36:47 -0700

    Properly initialize books registry

--- src/main/java/com/verdantartifice/primalmagick/common/books/BooksPM.java
+++ src/main/java/com/verdantartifice/primalmagick/common/books/BooksPM.java
@@ -5,6 +5,7 @@ import java.util.function.Supplier;
 import com.verdantartifice.primalmagick.PrimalMagick;
 import com.verdantartifice.primalmagick.common.registries.RegistryKeysPM;
 
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraftforge.registries.RegistryBuilder;
@@ -19,6 +20,10 @@ public class BooksPM {
     private static final DeferredRegister<BookDefinition> DEFERRED_BOOKS = DeferredRegister.create(RegistryKeysPM.BOOKS, PrimalMagick.MODID);
     
     public static final Supplier<IForgeRegistry<BookDefinition>> BOOKS = DEFERRED_BOOKS.makeRegistry(RegistryBuilder::new);
+    
+    public static void init() {
+        DEFERRED_BOOKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
     
     protected static RegistryObject<BookDefinition> register(String name, Supplier<BookDefinition> bookSupplier) {
         return DEFERRED_BOOKS.register(name, bookSupplier);
