701b1f002c4e6facaca61263b263c1a3e9723169
daedalus4096 <michael.a.bunting@gmail.com>
2023-09-04 23:22:32 -0700

    Allow reading enchanted books on vanilla lecterns

--- src/main/java/com/verdantartifice/primalmagick/common/events/BlockEvents.java
+++ src/main/java/com/verdantartifice/primalmagick/common/events/BlockEvents.java
@@ -12,6 +12,7 @@ import com.verdantartifice.primalmagick.common.items.books.StaticBookItem;
 import com.verdantartifice.primalmagick.common.misc.BlockBreaker;
 import com.verdantartifice.primalmagick.common.misc.InteractionRecord;
 import com.verdantartifice.primalmagick.common.network.PacketHandler;
+import com.verdantartifice.primalmagick.common.network.packets.misc.OpenEnchantedBookScreenPacket;
 import com.verdantartifice.primalmagick.common.network.packets.misc.OpenGrimoireScreenPacket;
 import com.verdantartifice.primalmagick.common.network.packets.misc.OpenStaticBookScreenPacket;
 import com.verdantartifice.primalmagick.common.stats.StatsManager;
@@ -20,10 +21,13 @@ import com.verdantartifice.primalmagick.common.stats.StatsPM;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.LecternBlock;
@@ -192,12 +196,19 @@ public class BlockEvents {
                     });
                 }
                 return InteractionResult.SUCCESS;
+            } else if (lecternEntity.getBook().is(Items.ENCHANTED_BOOK)) {
+                // Open the enchanted book screen
+                if (!level.isClientSide && player instanceof ServerPlayer serverPlayer) {
+                    EnchantmentHelper.getEnchantments(lecternEntity.getBook()).entrySet().stream().sorted((e1, e2) -> -Integer.compare(e1.getValue(), e2.getValue())).findFirst().ifPresent(entry -> {
+                        PacketHandler.sendToPlayer(new OpenEnchantedBookScreenPacket(entry.getKey()), serverPlayer);
+                    });
+                }
+                return InteractionResult.SUCCESS;
             }
         } else {
             // Place the grimoire
             ItemStack handStack = player.getItemInHand(hand);
-            if ((handStack.is(ItemsPM.GRIMOIRE.get()) || handStack.is(ItemsPM.STATIC_BOOK.get())) && 
-                    LecternBlock.tryPlaceBook(player, level, lecternEntity.getBlockPos(), lecternEntity.getBlockState(), handStack)) {
+            if (handStack.is(ItemTags.LECTERN_BOOKS) && LecternBlock.tryPlaceBook(player, level, lecternEntity.getBlockPos(), lecternEntity.getBlockState(), handStack)) {
                 return InteractionResult.SUCCESS;
             }
         }
