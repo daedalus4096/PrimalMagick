5dc849de7f4d52efe8db53b71d55ce3d490f0d9b
daedalus4096 <michael.a.bunting@gmail.com>
2023-10-19 16:17:14 -0700

    Add cluster growth logic to bud source

--- src/main/java/com/verdantartifice/primalmagick/common/blocks/minerals/BuddingGemSourceBlock.java
+++ src/main/java/com/verdantartifice/primalmagick/common/blocks/minerals/BuddingGemSourceBlock.java
@@ -5,10 +5,14 @@ import java.util.function.Supplier;
 import org.jetbrains.annotations.Nullable;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.material.PushReaction;
 
 /**
@@ -36,8 +40,33 @@ public class BuddingGemSourceBlock extends Block {
 
     @Override
     public void randomTick(BlockState pState, ServerLevel pLevel, BlockPos pPos, RandomSource pRandom) {
-        // TODO Auto-generated method stub
-        super.randomTick(pState, pLevel, pPos, pRandom);
+        if (pRandom.nextInt(GROWTH_CHANCE) == 0) {
+            Direction dir = Direction.values()[pRandom.nextInt(Direction.values().length)];
+            BlockPos targetPos = pPos.relative(dir);
+            BlockState targetState = pLevel.getBlockState(targetPos);
+            BuddingGemClusterBlock newCluster = null;
+            
+            // Determine what the new cluster should be
+            if (targetState.isAir() || (targetState.is(Blocks.WATER) && targetState.getFluidState().getAmount() == FluidState.AMOUNT_FULL)) {
+                newCluster = this.initialBudSupplier.get();
+            } else if (targetState.getBlock() instanceof BuddingGemClusterBlock cluster && cluster.getGemBudType() == this.gemType && 
+                    targetState.getValue(BuddingGemClusterBlock.FACING) == dir && cluster.getNextGemBlock().isPresent()) {
+                newCluster = cluster.getNextGemBlock().get().get();
+            }
+            
+            if (newCluster != null) {
+                // Update the world with the new cluster
+                BlockState newClusterState = newCluster.defaultBlockState()
+                        .setValue(BuddingGemClusterBlock.FACING, dir)
+                        .setValue(BuddingGemClusterBlock.WATERLOGGED, targetState.getFluidState().getType() == Fluids.WATER);
+                pLevel.setBlockAndUpdate(targetPos, newClusterState);
+                
+                if (pRandom.nextFloat() < this.decayChance) {
+                    // Decay the source block
+                    pLevel.setBlockAndUpdate(pPos, this.decayBlockSupplier.get().defaultBlockState());
+                }
+            }
+        }
     }
 
     @Override
