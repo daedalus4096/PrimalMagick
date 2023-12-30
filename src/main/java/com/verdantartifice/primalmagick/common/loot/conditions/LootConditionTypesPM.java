a43664349a6b60f3b626137e3cd9f2251e6e55f4
daedalus4096 <michael.a.bunting@gmail.com>
2023-12-29 21:56:32 -0800

    Migrate to codec usage

--- src/main/java/com/verdantartifice/primalmagick/common/loot/conditions/LootConditionTypesPM.java
+++ src/main/java/com/verdantartifice/primalmagick/common/loot/conditions/LootConditionTypesPM.java
@@ -1,10 +1,10 @@
 package com.verdantartifice.primalmagick.common.loot.conditions;
 
+import com.mojang.serialization.Codec;
 import com.verdantartifice.primalmagick.PrimalMagick;
 
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.level.storage.loot.Serializer;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemConditionType;
 
@@ -14,13 +14,13 @@ import net.minecraft.world.level.storage.loot.predicates.LootItemConditionType;
  * @author Daedalus4096
  */
 public class LootConditionTypesPM {
-    public static final LootItemConditionType MATCH_BLOCK_TAG = register("match_block_tag", new MatchBlockTag.ConditionSerializer());
+    public static final LootItemConditionType MATCH_BLOCK_TAG = register("match_block_tag", MatchBlockTag.CODEC);
     
     public static void register() {
         // No-op method to ensure that the class is loaded
     }
     
-    protected static LootItemConditionType register(String name, Serializer<? extends LootItemCondition> serializer) {
-        return Registry.register(BuiltInRegistries.LOOT_CONDITION_TYPE, PrimalMagick.resource(name), new LootItemConditionType(serializer));
+    protected static LootItemConditionType register(String name, Codec<? extends LootItemCondition> codec) {
+        return Registry.register(BuiltInRegistries.LOOT_CONDITION_TYPE, PrimalMagick.resource(name), new LootItemConditionType(codec));
     }
 }
--- src/main/java/com/verdantartifice/primalmagick/common/loot/conditions/MatchBlockTag.java
+++ src/main/java/com/verdantartifice/primalmagick/common/loot/conditions/MatchBlockTag.java
@@ -1,16 +1,13 @@
 package com.verdantartifice.primalmagick.common.loot.conditions;
 
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonSerializationContext;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.BlockTags;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootContext;
-import net.minecraft.world.level.storage.loot.Serializer;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemConditionType;
@@ -21,6 +18,10 @@ import net.minecraft.world.level.storage.loot.predicates.LootItemConditionType;
  * @author Daedalus4096
  */
 public class MatchBlockTag implements LootItemCondition {
+    public static final Codec<MatchBlockTag> CODEC = RecordCodecBuilder.create(instance -> {
+        return instance.group(TagKey.codec(Registries.BLOCK).fieldOf("tag").forGetter(mbt -> mbt.tag)).apply(instance, MatchBlockTag::new);
+    });
+    
     protected final TagKey<Block> tag;
     
     public MatchBlockTag(TagKey<Block> tag) {
@@ -40,18 +41,5 @@ public class MatchBlockTag implements LootItemCondition {
     @Override
     public LootItemConditionType getType() {
         return LootConditionTypesPM.MATCH_BLOCK_TAG;
-    }
-    
-    public static class ConditionSerializer implements Serializer<MatchBlockTag> {
-        @Override
-        public void serialize(JsonObject obj, MatchBlockTag condition, JsonSerializationContext context) {
-            obj.addProperty("tag", condition.tag.location().toString());
-        }
-
-        @Override
-        public MatchBlockTag deserialize(JsonObject obj, JsonDeserializationContext context) {
-            ResourceLocation name = new ResourceLocation(obj.getAsJsonPrimitive("tag").getAsString());
-            return new MatchBlockTag(BlockTags.create(name));
-        }
     }
 }
