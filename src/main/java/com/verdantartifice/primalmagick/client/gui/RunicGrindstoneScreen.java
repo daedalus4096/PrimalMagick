c3600ecafc017978c1179b6ce1afd6c58c9b0b23
daedalus4096 <michael.a.bunting@gmail.com>
2024-01-02 18:40:42 -0800

    Remove duplicate background render call

--- src/main/java/com/verdantartifice/primalmagick/client/gui/RunicGrindstoneScreen.java
+++ src/main/java/com/verdantartifice/primalmagick/client/gui/RunicGrindstoneScreen.java
@@ -17,7 +17,6 @@ public class RunicGrindstoneScreen extends AbstractContainerScreen<RunicGrindsto
 
     @Override
     public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
-        this.renderBg(guiGraphics, partialTicks, mouseX, mouseY);
         super.render(guiGraphics, mouseX, mouseY, partialTicks);
         this.renderTooltip(guiGraphics, mouseX, mouseY);
     }
