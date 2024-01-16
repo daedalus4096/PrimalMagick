6e2a33b72ec4b3c3004a37b3ebf150af793567f7
daedalus4096 <michael.a.bunting@gmail.com>
2024-01-15 17:04:03 -0800

    Scribe table screen backgrounds

--- src/main/java/com/verdantartifice/primalmagick/client/gui/ScribeStudyVocabularyScreen.java
+++ src/main/java/com/verdantartifice/primalmagick/client/gui/ScribeStudyVocabularyScreen.java
@@ -1,10 +1,12 @@
 package com.verdantartifice.primalmagick.client.gui;
 
+import com.verdantartifice.primalmagick.PrimalMagick;
 import com.verdantartifice.primalmagick.common.menus.ScribeStudyVocabularyMenu;
 
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 
 /**
@@ -13,13 +15,21 @@ import net.minecraft.world.entity.player.Inventory;
  * @author Daedalus4096
  */
 public class ScribeStudyVocabularyScreen extends AbstractContainerScreen<ScribeStudyVocabularyMenu> {
+    protected static final ResourceLocation TEXTURE = PrimalMagick.resource("textures/gui/scribe_study_vocabulary.png");
+    
     public ScribeStudyVocabularyScreen(ScribeStudyVocabularyMenu menu, Inventory inv, Component title) {
         super(menu, inv, title);
     }
 
     @Override
+    public void render(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick) {
+        super.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
+        this.renderTooltip(pGuiGraphics, pMouseX, pMouseY);
+    }
+
+    @Override
     protected void renderBg(GuiGraphics pGuiGraphics, float pPartialTick, int pMouseX, int pMouseY) {
-        // TODO Auto-generated method stub
-        
+        // Render background texture
+        pGuiGraphics.blit(TEXTURE, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight);
     }
 }
