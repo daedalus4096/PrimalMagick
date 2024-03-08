aa7ed7ad70391ba0ad7fa2b24198442079a35695
daedalus4096 <michael.a.bunting@gmail.com>
2024-03-07 23:56:01 -0800

    Add comprehension menu slot

--- src/main/java/com/verdantartifice/primalmagick/common/menus/ScribeGainComprehensionMenu.java
+++ src/main/java/com/verdantartifice/primalmagick/common/menus/ScribeGainComprehensionMenu.java
@@ -2,11 +2,20 @@ package com.verdantartifice.primalmagick.common.menus;
 
 import org.joml.Vector2i;
 
+import com.verdantartifice.primalmagick.common.books.BookLanguage;
+import com.verdantartifice.primalmagick.common.books.BookLanguagesPM;
+import com.verdantartifice.primalmagick.common.items.books.StaticBookItem;
+import com.verdantartifice.primalmagick.common.menus.slots.FilteredSlot;
+import com.verdantartifice.primalmagick.common.tags.ItemTagsPM;
 import com.verdantartifice.primalmagick.common.tiles.devices.ScribeTableTileEntity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.Container;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.DataSlot;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 
 /**
@@ -15,8 +24,14 @@ import net.minecraft.world.item.ItemStack;
  * @author Daedalus4096
  */
 public class ScribeGainComprehensionMenu extends AbstractScribeTableMenu {
+    private final DataSlot languageClue = DataSlot.standalone();
+
+    protected Slot studySlot;
+    
     public ScribeGainComprehensionMenu(int windowId, Inventory inv, BlockPos pos) {
         this(windowId, inv, pos, null);
+        this.addDataSlot(this.languageClue);
+        this.refreshBookData();
     }
     
     public ScribeGainComprehensionMenu(int windowId, Inventory inv, BlockPos pos, ScribeTableTileEntity entity) {
@@ -25,8 +40,9 @@ public class ScribeGainComprehensionMenu extends AbstractScribeTableMenu {
     
     @Override
     protected void createModeSlots() {
-        // TODO Auto-generated method stub
-
+        // Slot 0: Original book
+        this.studySlot = this.addSlot(new FilteredSlot(this.getTileInventory(Direction.UP), 0, 8, 8, 
+                new FilteredSlot.Properties().filter(stack -> stack.is(ItemTagsPM.STATIC_BOOKS) && StaticBookItem.getBookLanguage(stack).isComplex())));
     }
 
     @Override
@@ -35,9 +51,69 @@ public class ScribeGainComprehensionMenu extends AbstractScribeTableMenu {
     }
 
     @Override
-    public ItemStack quickMoveStack(Player pPlayer, int pIndex) {
-        // TODO Auto-generated method stub
-        return null;
+    public void containerChanged(Container pContainer) {
+        super.containerChanged(pContainer);
+        this.refreshBookData();
     }
-
+    
+    protected void refreshBookData() {
+        ItemStack bookStack = this.studySlot.getItem();
+        BookLanguage lang = bookStack.is(ItemTagsPM.STATIC_BOOKS) ? StaticBookItem.getBookLanguage(bookStack) : BookLanguagesPM.DEFAULT.get();
+        this.languageClue.set(BookLanguagesPM.LANGUAGES.get().getKey(lang).hashCode());
+    }
+    
+    @Override
+    public ItemStack quickMoveStack(Player pPlayer, int pIndex) {
+        ItemStack stack = ItemStack.EMPTY;
+        Slot slot = this.slots.get(pIndex);
+        if (slot != null && slot.hasItem()) {
+            ItemStack slotStack = slot.getItem();
+            stack = slotStack.copy();
+            if (pIndex == 0) {
+                // If transferring the study item, move it into the player's backpack or hotbar
+                if (!this.moveItemStackTo(slotStack, 1, 37, true)) {
+                    return ItemStack.EMPTY;
+                }
+                slot.onQuickCraft(slotStack, stack);
+            } else if (pIndex >= 1 && pIndex < 28) {
+                // If transferring from the backpack, move static books to the appropriate slot, and everything else to the hotbar
+                if (this.studySlot.mayPlace(slotStack)) {
+                    if (!this.moveItemStackTo(slotStack, 0, 1, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else {
+                    if (!this.moveItemStackTo(slotStack, 28, 37, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                }
+            } else if (pIndex >= 28 && pIndex < 37) {
+                // If transferring from the hotbar, move static books to the appropriate slot, and everything else to the backpack
+                if (this.studySlot.mayPlace(slotStack)) {
+                    if (!this.moveItemStackTo(slotStack, 0, 1, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else {
+                    if (!this.moveItemStackTo(slotStack, 1, 28, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                }
+            } else if (!this.moveItemStackTo(slotStack, 1, 37, false)) {
+                // Move all other transfers to the backpack or hotbar
+                return ItemStack.EMPTY;
+            }
+            
+            if (slotStack.isEmpty()) {
+                slot.set(ItemStack.EMPTY);
+            } else {
+                slot.setChanged();
+            }
+            
+            if (slotStack.getCount() == stack.getCount()) {
+                return ItemStack.EMPTY;
+            }
+            
+            slot.onTake(pPlayer, slotStack);
+        }
+        return stack;
+    }
 }
