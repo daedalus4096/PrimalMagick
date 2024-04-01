5064cb8fd85f870e406e6a000019e5368df23758
daedalus4096 <michael.a.bunting@gmail.com>
2024-03-31 22:59:08 -0700

    Refactor loot table support to base tile class

--- src/main/java/com/verdantartifice/primalmagick/common/tiles/rituals/OfferingPedestalTileEntity.java
+++ src/main/java/com/verdantartifice/primalmagick/common/tiles/rituals/OfferingPedestalTileEntity.java
@@ -1,5 +1,6 @@
 package com.verdantartifice.primalmagick.common.tiles.rituals;
 
+import java.util.Optional;
 import java.util.OptionalInt;
 import java.util.Set;
 
@@ -9,27 +10,16 @@ import com.google.common.collect.ImmutableSet;
 import com.verdantartifice.primalmagick.common.capabilities.ItemStackHandlerPM;
 import com.verdantartifice.primalmagick.common.tiles.TileEntityTypesPM;
 import com.verdantartifice.primalmagick.common.tiles.base.AbstractTileSidedInventoryPM;
-import com.verdantartifice.primalmagick.common.tiles.base.IRandomizableContents;
 
-import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.storage.loot.LootParams;
-import net.minecraft.world.level.storage.loot.LootTable;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
-import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.items.IItemHandlerModifiable;
 import net.minecraftforge.items.ItemStackHandler;
-import net.minecraftforge.items.wrapper.RecipeWrapper;
 
 /**
  * Definition of an offering pedestal tile entity.  Holds the pedestal's inventory.
@@ -37,12 +27,10 @@ import net.minecraftforge.items.wrapper.RecipeWrapper;
  * @author Daedalus4096
  * @see {@link com.verdantartifice.primalmagick.common.blocks.rituals.OfferingPedestalBlock}
  */
-public class OfferingPedestalTileEntity extends AbstractTileSidedInventoryPM implements IRandomizableContents {
+public class OfferingPedestalTileEntity extends AbstractTileSidedInventoryPM {
     public static final int INPUT_INV_INDEX = 0;
     
     protected BlockPos altarPos = null;
-    protected ResourceLocation lootTable;
-    protected long lootTableSeed;
 
     public OfferingPedestalTileEntity(BlockPos pos, BlockState state) {
         super(TileEntityTypesPM.OFFERING_PEDESTAL.get(), pos, state);
@@ -67,14 +55,12 @@ public class OfferingPedestalTileEntity extends AbstractTileSidedInventoryPM imp
     @Override
     public void load(CompoundTag compound) {
         super.load(compound);
-        this.tryLoadLootTable(compound);
         this.altarPos = compound.contains("AltarPos", Tag.TAG_LONG) ? BlockPos.of(compound.getLong("AltarPos")) : null;
     }
 
     @Override
     protected void saveAdditional(CompoundTag compound) {
         super.saveAdditional(compound);
-        this.trySaveLootTable(compound);
         if (this.altarPos != null) {
             compound.putLong("AltarPos", this.altarPos.asLong());
         }
@@ -139,46 +125,7 @@ public class OfferingPedestalTileEntity extends AbstractTileSidedInventoryPM imp
     }
 
     @Override
-    public void setLootTable(ResourceLocation lootTable, long lootTableSeed) {
-        this.lootTable = lootTable;
-        this.lootTableSeed = lootTableSeed;
-    }
-
-    @Override
-    public void unpackLootTable(Player player) {
-        if (this.lootTable != null && this.level.getServer() != null) {
-            LootTable loot = this.level.getServer().getLootData().getLootTable(this.lootTable);
-            if (player instanceof ServerPlayer serverPlayer) {
-                CriteriaTriggers.GENERATE_LOOT.trigger(serverPlayer, this.lootTable);
-            }
-            this.lootTable = null;
-            LootParams.Builder paramsBuilder = new LootParams.Builder((ServerLevel)this.level).withParameter(LootContextParams.ORIGIN, Vec3.atCenterOf(this.worldPosition));
-            if (player != null) {
-                paramsBuilder.withLuck(player.getLuck()).withParameter(LootContextParams.THIS_ENTITY, player);
-            }
-            loot.fill(new RecipeWrapper(this.itemHandlers.get(INPUT_INV_INDEX)), paramsBuilder.create(LootContextParamSets.CHEST), this.lootTableSeed);
-        }
-    }
-
-    protected boolean tryLoadLootTable(CompoundTag pTag) {
-        if (pTag.contains("LootTable", Tag.TAG_STRING)) {
-            this.lootTable = new ResourceLocation(pTag.getString("LootTable"));
-            this.lootTableSeed = pTag.getLong("LootTableSeed");
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    protected boolean trySaveLootTable(CompoundTag pTag) {
-        if (this.lootTable == null) {
-            return false;
-        } else {
-            pTag.putString("LootTable", this.lootTable.toString());
-            if (this.lootTableSeed != 0L) {
-                pTag.putLong("LootTableSeed", this.lootTableSeed);
-            }
-            return true;
-        }
+    public Optional<IItemHandlerModifiable> getTargetRandomizedInventory() {
+        return Optional.ofNullable(this.itemHandlers.get(INPUT_INV_INDEX));
     }
 }
