1e08b671746b3dac42eecdfe680922cb16f01660
daedalus4096 <michael.a.bunting@gmail.com>
2024-04-12 23:08:37 -0700

    Add builder for shapeless arcane tag recipes

---
+++ src/main/java/com/verdantartifice/primalmagick/datagen/recipes/ArcaneShapelessTagRecipeBuilder.java
@@ -0,0 +1,225 @@
+package com.verdantartifice.primalmagick.datagen.recipes;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.json.stream.JsonGenerationException;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.mojang.serialization.JsonOps;
+import com.verdantartifice.primalmagick.common.crafting.RecipeSerializersPM;
+import com.verdantartifice.primalmagick.common.research.CompoundResearchKey;
+import com.verdantartifice.primalmagick.common.sources.SourceList;
+
+import net.minecraft.Util;
+import net.minecraft.advancements.AdvancementHolder;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.level.ItemLike;
+
+/**
+ * Definition of a recipe data file builder for shapeless arcane tag recipes.
+ * 
+ * @author Daedalus4096
+ * @see {@link net.minecraft.data.ShapelessRecipeBuilder}
+ */
+public class ArcaneShapelessTagRecipeBuilder {
+    protected final TagKey<Item> resultTag;
+    protected final int resultAmount;
+    protected final List<Ingredient> ingredients = new ArrayList<>();
+    protected String group;
+    protected CompoundResearchKey research;
+    protected SourceList manaCosts;
+
+    protected ArcaneShapelessTagRecipeBuilder(TagKey<Item> result, int count) {
+        this.resultTag = result;
+        this.resultAmount = count;
+    }
+    
+    /**
+     * Creates a new builder for a shapeless arcane recipe.
+     * 
+     * @param result the output item type
+     * @param count the output item quantity
+     * @return a new builder for a shapeless arcane recipe
+     */
+    public static ArcaneShapelessTagRecipeBuilder arcaneShapelessTagRecipe(TagKey<Item> result, int count) {
+        return new ArcaneShapelessTagRecipeBuilder(result, count);
+    }
+    
+    /**
+     * Creates a new builder for a shapeless arcane recipe.
+     * 
+     * @param result the output item type
+     * @return a new builder for a shapeless arcane recipe
+     */
+    public static ArcaneShapelessTagRecipeBuilder arcaneShapelessTagRecipe(TagKey<Item> result) {
+        return arcaneShapelessTagRecipe(result, 1);
+    }
+    
+    /**
+     * Add an ingredient to the recipe multiple times.
+     * 
+     * @param ingredient the ingredient to be added
+     * @param quantity the number of the ingredient to add
+     * @return the modified builder
+     */
+    public ArcaneShapelessTagRecipeBuilder addIngredient(Ingredient ingredient, int quantity) {
+        for (int index = 0; index < quantity; index++) {
+            this.ingredients.add(ingredient);
+        }
+        return this;
+    }
+    
+    /**
+     * Add an ingredient to the recipe.
+     * 
+     * @param ingredient the ingredient to be added
+     * @return the modified builder
+     */
+    public ArcaneShapelessTagRecipeBuilder addIngredient(Ingredient ingredient) {
+        return this.addIngredient(ingredient, 1);
+    }
+    
+    /**
+     * Add an ingredient of the given item to the recipe multiple times.
+     * 
+     * @param item the item to be added
+     * @param quantity the number of the item to add
+     * @return the modified builder
+     */
+    public ArcaneShapelessTagRecipeBuilder addIngredient(ItemLike item, int quantity) {
+        return this.addIngredient(Ingredient.of(item), quantity);
+    }
+    
+    /**
+     * Add an ingredient of the given item to the recipe.
+     * 
+     * @param item the item to be added
+     * @return the modified builder
+     */
+    public ArcaneShapelessTagRecipeBuilder addIngredient(ItemLike item) {
+        return this.addIngredient(item, 1);
+    }
+    
+    /**
+     * Add an ingredient to the recipe that can be any item in the given tag.
+     * 
+     * @param tag the tag of items to be added
+     * @return the modified builder
+     */
+    public ArcaneShapelessTagRecipeBuilder addIngredient(TagKey<Item> tag) {
+        return this.addIngredient(Ingredient.of(tag));
+    }
+    
+    /**
+     * Adds a group to this recipe.
+     * 
+     * @param group the group to add
+     * @return the modified builder
+     */
+    public ArcaneShapelessTagRecipeBuilder setGroup(String group) {
+        this.group = group;
+        return this;
+    }
+    
+    /**
+     * Adds a research requirement to this recipe.
+     * 
+     * @param research the research requirement to add
+     * @return the modified builder
+     */
+    public ArcaneShapelessTagRecipeBuilder research(CompoundResearchKey research) {
+        this.research = research.copy();
+        return this;
+    }
+    
+    /**
+     * Adds a research requirement to this recipe.  Throws if the optional is empty.
+     * 
+     * @param researchOpt the research requirement to add
+     * @return the modified builder
+     */
+    public ArcaneShapelessTagRecipeBuilder research(Optional<CompoundResearchKey> researchOpt) {
+        return this.research(researchOpt.orElseThrow());
+    }
+    
+    /**
+     * Adds a mana cost to this recipe.
+     * 
+     * @param mana the mana cost to add
+     * @return the modified builder
+     */
+    public ArcaneShapelessTagRecipeBuilder manaCost(SourceList mana) {
+        this.manaCosts = mana.copy();
+        return this;
+    }
+    
+    /**
+     * Builds this recipe into an {@link IFinishedRecipe}.
+     * 
+     * @param output a consumer for the finished recipe
+     * @param id the ID of the finished recipe
+     */
+    public void build(RecipeOutput output, ResourceLocation id) {
+        this.validate(id);
+        output.accept(new ArcaneShapelessTagRecipeBuilder.Result(id, this.resultTag, this.resultAmount, this.group == null ? "" : this.group, this.ingredients, this.research, this.manaCosts));
+    }
+
+    /**
+     * Makes sure that this recipe is valid.
+     * 
+     * @param id the ID of the recipe
+     */
+    protected void validate(ResourceLocation id) {
+        if (this.ingredients.isEmpty()) {
+            throw new IllegalStateException("No ingredients defined for arcane shapeless tag recipe " + id + "!");
+        }
+        if (this.research == null) {
+            throw new IllegalStateException("No research is defined for arcane shapeless tag recipe " + id + "!");
+        }
+    }
+    
+    public static record Result(ResourceLocation id, TagKey<Item> resultTag, int resultAmount, String group, List<Ingredient> ingredients, CompoundResearchKey research, SourceList manaCosts) implements FinishedRecipe {
+        @Override
+        public void serializeRecipeData(JsonObject json) {
+            if (this.group != null && !this.group.isEmpty()) {
+                json.addProperty("group", this.group);
+            }
+            if (this.research != null) {
+                json.addProperty("research", this.research.toString());
+            }
+            
+            if (this.manaCosts != null && !this.manaCosts.isEmpty()) {
+                json.add("mana", Util.getOrThrow(SourceList.CODEC.encodeStart(JsonOps.INSTANCE, this.manaCosts), JsonGenerationException::new));
+            }
+            
+            JsonArray ingredientsJson = new JsonArray();
+            for (Ingredient ingredient : this.ingredients) {
+                ingredientsJson.add(ingredient.toJson(true));
+            }
+            json.add("ingredients", ingredientsJson);
+            
+            json.addProperty("result_tag", this.resultTag.location().toString());
+            json.addProperty("result_amount", this.resultAmount);
+        }
+
+        @Override
+        public RecipeSerializer<?> type() {
+            return RecipeSerializersPM.ARCANE_CRAFTING_SHAPELESS_TAG.get();
+        }
+
+        @Override
+        public AdvancementHolder advancement() {
+            // Arcane recipes don't use the vanilla advancement unlock system, so return null
+            return null;
+        }
+    }
+}
