bda9cea611b312a6e918936dd0eaebb1ce6f9795
daedalus4096 <michael.a.bunting@gmail.com>
2024-04-18 23:46:06 -0700

    Add codecs to sunlamp blocks

--- src/main/java/com/verdantartifice/primalmagick/common/blocks/BlockReferencesPM.java
+++ src/main/java/com/verdantartifice/primalmagick/common/blocks/BlockReferencesPM.java
@@ -10,6 +10,8 @@ public class BlockReferencesPM {
     public static final ResourceKey<Block> HYDROMELON = createKey("hydromelon");
     public static final ResourceKey<Block> HYDROMELON_STEM = createKey("hydromelon_stem");
     public static final ResourceKey<Block> ATTACHED_HYDROMELON_STEM = createKey("attached_hydromelon_stem");
+    public static final ResourceKey<Block> GLOW_FIELD = createKey("glow_field");
+    public static final ResourceKey<Block> SOUL_GLOW_FIELD = createKey("soul_glow_field");
     
     private static ResourceKey<Block> createKey(String id) {
         return ResourceKey.create(Registries.BLOCK, PrimalMagick.resource(id));
