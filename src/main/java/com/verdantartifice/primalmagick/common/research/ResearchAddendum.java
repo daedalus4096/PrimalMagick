daa3743662987b540dff61b276de28449a7fd710
daedalus4096 <michael.a.bunting@gmail.com>
2024-05-04 12:13:01 -0700

    Add builder to ResearchAddendum

--- src/main/java/com/verdantartifice/primalmagick/common/research/ResearchAddendum.java
+++ src/main/java/com/verdantartifice/primalmagick/common/research/ResearchAddendum.java
@@ -1,5 +1,6 @@
 package com.verdantartifice.primalmagick.common.research;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,12 +8,17 @@ import javax.annotation.Nonnull;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import com.verdantartifice.primalmagick.PrimalMagick;
 import com.verdantartifice.primalmagick.common.research.keys.ResearchEntryKey;
 import com.verdantartifice.primalmagick.common.research.requirements.AbstractRequirement;
+import com.verdantartifice.primalmagick.common.research.requirements.AndRequirement;
+import com.verdantartifice.primalmagick.common.sources.Source;
 import com.verdantartifice.primalmagick.common.sources.SourceList;
 
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.ItemLike;
+import net.minecraftforge.registries.ForgeRegistries;
 
 /**
  * Definition of a research addendum.  An addendum is an addon to a research entry that is separately
@@ -50,5 +56,91 @@ public record ResearchAddendum(ResearchEntryKey parentKey, String textTranslatio
         buf.writeCollection(addendum.siblings, (b, s) -> s.toNetwork(b));
         buf.writeOptional(addendum.completionRequirementOpt, (b, r) -> r.toNetwork(b));
         SourceList.toNetwork(buf, addendum.attunements);
+    }
+    
+    public static class Builder {
+        protected final String modId;
+        protected final ResearchEntryKey parentKey;
+        protected final int addendumIndex;
+        protected final List<ResourceLocation> recipes = new ArrayList<>();
+        protected final List<ResearchEntryKey> siblings = new ArrayList<>();
+        protected final List<AbstractRequirement<?>> requirements = new ArrayList<>();
+        protected final SourceList.Builder attunements = SourceList.builder();
+        
+        public Builder(String modId, ResearchEntryKey parentKey, int addendumIndex) {
+            this.modId = modId;
+            this.parentKey = parentKey;
+            this.addendumIndex = addendumIndex;
+        }
+        
+        public Builder(ResearchEntryKey parentKey, int addendumIndex) {
+            this(PrimalMagick.MODID, parentKey, addendumIndex);
+        }
+        
+        public Builder recipe(String name) {
+            return this.recipe(PrimalMagick.resource(name));
+        }
+        
+        public Builder recipe(String modId, String name) {
+            return this.recipe(new ResourceLocation(modId, name));
+        }
+        
+        public Builder recipe(ItemLike itemLike) {
+            return this.recipe(ForgeRegistries.ITEMS.getKey(itemLike.asItem()));
+        }
+        
+        public Builder recipe(ResourceLocation recipe) {
+            this.recipes.add(recipe);
+            return this;
+        }
+        
+        public Builder sibling(ResearchEntryKey siblingKey) {
+            this.siblings.add(siblingKey);
+            return this;
+        }
+        
+        public Builder requirement(AbstractRequirement<?> req) {
+            this.requirements.add(req);
+            return this;
+        }
+        
+        public Builder attunement(SourceList sources) {
+            this.attunements.with(sources);
+            return this;
+        }
+        
+        public Builder attunement(Source source, int amount) {
+            this.attunements.with(source, amount);
+            return this;
+        }
+        
+        protected String getTextTranslationKey() {
+            return String.join(".", "research", this.modId.toLowerCase(), this.parentKey.getRootKey().toLowerCase(), "text", "addenda", Integer.toString(this.addendumIndex));
+        }
+        
+        protected Optional<AbstractRequirement<?>> getFinalRequirement() {
+            if (this.requirements.isEmpty()) {
+                return Optional.empty();
+            } else if (this.requirements.size() == 1) {
+                return Optional.of(this.requirements.get(0));
+            } else {
+                return Optional.of(new AndRequirement(this.requirements));
+            }
+        }
+        
+        private void validate() {
+            if (this.modId == null || this.modId.isBlank()) {
+                throw new IllegalStateException("Mod ID may not be empty");
+            } else if (this.parentKey == null) {
+                throw new IllegalStateException("Parent entry key may not be null");
+            } else if (this.addendumIndex < 0) {
+                throw new IllegalStateException("Addendum index must be non-negative");
+            }
+        }
+        
+        public ResearchAddendum build() {
+            this.validate();
+            return new ResearchAddendum(this.parentKey, this.getTextTranslationKey(), this.recipes, this.siblings, this.getFinalRequirement(), this.attunements.build());
+        }
     }
 }
