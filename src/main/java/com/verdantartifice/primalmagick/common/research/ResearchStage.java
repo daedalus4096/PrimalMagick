7df95a49833c81f1359cb9453e1f5409bceb22a6
daedalus4096 <michael.a.bunting@gmail.com>
2024-05-04 12:32:49 -0700

    Add builder to ResearchStage

--- src/main/java/com/verdantartifice/primalmagick/common/research/ResearchStage.java
+++ src/main/java/com/verdantartifice/primalmagick/common/research/ResearchStage.java
@@ -1,5 +1,6 @@
 package com.verdantartifice.primalmagick.common.research;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -12,15 +13,20 @@ import org.apache.commons.lang3.mutable.MutableBoolean;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import com.verdantartifice.primalmagick.PrimalMagick;
 import com.verdantartifice.primalmagick.common.research.keys.AbstractResearchKey;
 import com.verdantartifice.primalmagick.common.research.keys.ResearchEntryKey;
 import com.verdantartifice.primalmagick.common.research.requirements.AbstractRequirement;
+import com.verdantartifice.primalmagick.common.research.requirements.AndRequirement;
 import com.verdantartifice.primalmagick.common.research.requirements.RequirementCategory;
+import com.verdantartifice.primalmagick.common.sources.Source;
 import com.verdantartifice.primalmagick.common.sources.SourceList;
 
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.ItemLike;
+import net.minecraftforge.registries.ForgeRegistries;
 
 /**
  * Definition of a research stage, a portion of a research entry.  A research stage contains text to be
@@ -102,6 +108,104 @@ public record ResearchStage(ResearchEntryKey parentKey, String textTranslationKe
             } else {
                 return reqStream.map(req -> req.isMetBy(player)).toList();
             }
+        }
+    }
+    
+    public static class Builder {
+        protected final String modId;
+        protected final ResearchEntryKey parentKey;
+        protected final int stageIndex;
+        protected final List<AbstractRequirement<?>> requirements = new ArrayList<>();
+        protected final List<ResourceLocation> recipes = new ArrayList<>();
+        protected final List<ResearchEntryKey> siblings = new ArrayList<>();
+        protected final List<ResearchEntryKey> revelations = new ArrayList<>();
+        protected final List<AbstractResearchKey<?>> hints = new ArrayList<>();
+        protected final SourceList.Builder attunements = SourceList.builder();
+        
+        public Builder(String modId, ResearchEntryKey parentKey, int stageIndex) {
+            this.modId = modId;
+            this.parentKey = parentKey;
+            this.stageIndex = stageIndex;
+        }
+        
+        public Builder(ResearchEntryKey parentKey, int stageIndex) {
+            this(PrimalMagick.MODID, parentKey, stageIndex);
+        }
+        
+        public Builder requirement(AbstractRequirement<?> req) {
+            this.requirements.add(req);
+            return this;
+        }
+        
+        public Builder recipe(String name) {
+            return this.recipe(PrimalMagick.resource(name));
+        }
+        
+        public Builder recipe(String modId, String name) {
+            return this.recipe(new ResourceLocation(modId, name));
+        }
+        
+        public Builder recipe(ItemLike itemLike) {
+            return this.recipe(ForgeRegistries.ITEMS.getKey(itemLike.asItem()));
+        }
+        
+        public Builder recipe(ResourceLocation recipe) {
+            this.recipes.add(recipe);
+            return this;
+        }
+        
+        public Builder sibling(ResearchEntryKey siblingKey) {
+            this.siblings.add(siblingKey);
+            return this;
+        }
+        
+        public Builder reveals(ResearchEntryKey revelationKey) {
+            this.revelations.add(revelationKey);
+            return this;
+        }
+        
+        public Builder hint(AbstractResearchKey<?> hintKey) {
+            this.hints.add(hintKey);
+            return this;
+        }
+        
+        public Builder attunement(SourceList sources) {
+            this.attunements.with(sources);
+            return this;
+        }
+        
+        public Builder attunement(Source source, int amount) {
+            this.attunements.with(source, amount);
+            return this;
+        }
+
+        private String getTextTranslationKey() {
+            return String.join(".", "research", this.modId.toLowerCase(), this.parentKey.getRootKey().toLowerCase(), "text", "stage", Integer.toString(this.stageIndex));
+        }
+        
+        protected Optional<AbstractRequirement<?>> getFinalRequirement() {
+            if (this.requirements.isEmpty()) {
+                return Optional.empty();
+            } else if (this.requirements.size() == 1) {
+                return Optional.of(this.requirements.get(0));
+            } else {
+                return Optional.of(new AndRequirement(this.requirements));
+            }
+        }
+        
+        private void validate() {
+            if (this.modId == null || this.modId.isBlank()) {
+                throw new IllegalStateException("Mod ID may not be empty");
+            } else if (this.parentKey == null) {
+                throw new IllegalStateException("Parent entry key may not be null");
+            } else if (this.stageIndex < 0) {
+                throw new IllegalStateException("Stage index must be non-negative");
+            }
+        }
+        
+        public ResearchStage build() {
+            this.validate();
+            return new ResearchStage(this.parentKey, this.getTextTranslationKey(), this.getFinalRequirement(), this.recipes, this.siblings, this.revelations, this.hints, this.attunements.build());
         }
     }
 }
