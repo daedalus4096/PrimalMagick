f7cd61c3ba37ee52301cdb9f2f399b51f2c6ca97
daedalus4096 <michael.a.bunting@gmail.com>
2024-05-04 12:16:00 -0700

    Reorder fields

--- src/main/java/com/verdantartifice/primalmagick/common/research/ResearchAddendum.java
+++ src/main/java/com/verdantartifice/primalmagick/common/research/ResearchAddendum.java
@@ -27,14 +27,14 @@ import net.minecraftforge.registries.ForgeRegistries;
  * 
  * @author Daedalus4096
  */
-public record ResearchAddendum(ResearchEntryKey parentKey, String textTranslationKey, List<ResourceLocation> recipes, List<ResearchEntryKey> siblings,
-        Optional<AbstractRequirement<?>> completionRequirementOpt, SourceList attunements) {
+public record ResearchAddendum(ResearchEntryKey parentKey, String textTranslationKey, Optional<AbstractRequirement<?>> completionRequirementOpt, 
+        List<ResourceLocation> recipes, List<ResearchEntryKey> siblings, SourceList attunements) {
     public static final Codec<ResearchAddendum> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             ResearchEntryKey.CODEC.fieldOf("parentKey").forGetter(ResearchAddendum::parentKey),
             Codec.STRING.fieldOf("textTranslationKey").forGetter(ResearchAddendum::textTranslationKey),
+            AbstractRequirement.CODEC.optionalFieldOf("completionRequirementOpt").forGetter(ResearchAddendum::completionRequirementOpt),
             ResourceLocation.CODEC.listOf().fieldOf("recipes").forGetter(ResearchAddendum::recipes),
             ResearchEntryKey.CODEC.listOf().fieldOf("siblings").forGetter(ResearchAddendum::siblings),
-            AbstractRequirement.CODEC.optionalFieldOf("completionRequirementOpt").forGetter(ResearchAddendum::completionRequirementOpt),
             SourceList.CODEC.optionalFieldOf("attunements", SourceList.EMPTY).forGetter(ResearchAddendum::attunements)
         ).apply(instance, ResearchAddendum::new));
     
@@ -42,19 +42,19 @@ public record ResearchAddendum(ResearchEntryKey parentKey, String textTranslatio
     public static ResearchAddendum fromNetwork(FriendlyByteBuf buf) {
         ResearchEntryKey parentKey = ResearchEntryKey.fromNetwork(buf);
         String textKey = buf.readUtf();
+        Optional<AbstractRequirement<?>> compReqOpt = buf.readOptional(AbstractRequirement::fromNetwork);
         List<ResourceLocation> recipes = buf.readList(b -> b.readResourceLocation());
         List<ResearchEntryKey> siblings = buf.readList(ResearchEntryKey::fromNetwork);
-        Optional<AbstractRequirement<?>> compReqOpt = buf.readOptional(AbstractRequirement::fromNetwork);
         SourceList attunements = SourceList.fromNetwork(buf);
-        return new ResearchAddendum(parentKey, textKey, recipes, siblings, compReqOpt, attunements);
+        return new ResearchAddendum(parentKey, textKey, compReqOpt, recipes, siblings, attunements);
     }
     
     public static void toNetwork(FriendlyByteBuf buf, ResearchAddendum addendum) {
         addendum.parentKey.toNetwork(buf);
         buf.writeUtf(addendum.textTranslationKey);
+        buf.writeOptional(addendum.completionRequirementOpt, (b, r) -> r.toNetwork(b));
         buf.writeCollection(addendum.recipes, (b, l) -> b.writeResourceLocation(l));
         buf.writeCollection(addendum.siblings, (b, s) -> s.toNetwork(b));
-        buf.writeOptional(addendum.completionRequirementOpt, (b, r) -> r.toNetwork(b));
         SourceList.toNetwork(buf, addendum.attunements);
     }
     
@@ -140,7 +140,7 @@ public record ResearchAddendum(ResearchEntryKey parentKey, String textTranslatio
         
         public ResearchAddendum build() {
             this.validate();
-            return new ResearchAddendum(this.parentKey, this.getTextTranslationKey(), this.recipes, this.siblings, this.getFinalRequirement(), this.attunements.build());
+            return new ResearchAddendum(this.parentKey, this.getTextTranslationKey(), this.getFinalRequirement(), this.recipes, this.siblings, this.attunements.build());
         }
     }
 }
