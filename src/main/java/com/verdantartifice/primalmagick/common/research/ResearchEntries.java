a79902fd4102fd834161541092632b0919226596
daedalus4096 <michael.a.bunting@gmail.com>
2024-05-18 14:37:20 -0700

    Progress commit for research entry definitions

--- src/main/java/com/verdantartifice/primalmagick/common/research/ResearchEntries.java
+++ src/main/java/com/verdantartifice/primalmagick/common/research/ResearchEntries.java
@@ -42,28 +42,141 @@ public class ResearchEntries {
     public static final ResourceKey<ResearchEntry> SOURCE_SKY = create("source_sky");
     public static final ResourceKey<ResearchEntry> SOURCE_SUN = create("source_sun");
     public static final ResourceKey<ResearchEntry> SOURCE_MOON = create("source_moon");
+    public static final ResourceKey<ResearchEntry> FORBIDDEN_MAGICK = create("forbidden_magick");
+    public static final ResourceKey<ResearchEntry> SOURCE_BLOOD = create("source_blood");
+    public static final ResourceKey<ResearchEntry> SOURCE_INFERNAL = create("source_infernal");
+    public static final ResourceKey<ResearchEntry> SOURCE_VOID = create("source_void");
+    public static final ResourceKey<ResearchEntry> HEAVENLY_MAGICK = create("heavenly_magick");
+    public static final ResourceKey<ResearchEntry> SOURCE_HALLOWED = create("source_hallowed");
     public static final ResourceKey<ResearchEntry> SECRETS_OF_THE_UNIVERSE = create("secrets_of_the_universe");
+    public static final ResourceKey<ResearchEntry> COMPLETE_BASICS = create("complete_basics");
+    public static final ResourceKey<ResearchEntry> THEORY_OF_EVERYTHING = create("theory_of_everything");
     
     // Manaweaving research entries
     // TODO Place in correct order after bootstrapping
+    public static final ResourceKey<ResearchEntry> BASIC_MANAWEAVING = create("basic_manaweaving");
+    public static final ResourceKey<ResearchEntry> EXPERT_MANAWEAVING = create("expert_manaweaving");
+    public static final ResourceKey<ResearchEntry> MASTER_MANAWEAVING = create("master_manaweaving");
+    public static final ResourceKey<ResearchEntry> SUPREME_MANAWEAVING = create("supreme_manaweaving");
+    public static final ResourceKey<ResearchEntry> COMPLETE_MANAWEAVING = create("complete_manaweaving");
     public static final ResourceKey<ResearchEntry> MANA_ARROWS = create("mana_arrows");
     public static final ResourceKey<ResearchEntry> WAND_CHARGER = create("wand_charger");
     public static final ResourceKey<ResearchEntry> MANA_SALTS = create("mana_salts");
-    public static final ResourceKey<ResearchEntry> IMBUED_WOOL = create("imbued_wool");
+    public static final ResourceKey<ResearchEntry> ADVANCED_WANDMAKING = create("advanced_wandmaking");
     public static final ResourceKey<ResearchEntry> STAVES = create("staves");
+    public static final ResourceKey<ResearchEntry> EARTHSHATTER_HAMMER = create("earthshatter_hammer");
+    public static final ResourceKey<ResearchEntry> SUNLAMP = create("sunlamp");
+    public static final ResourceKey<ResearchEntry> IMBUED_WOOL = create("imbued_wool");
+    public static final ResourceKey<ResearchEntry> SPELLCLOTH = create("spellcloth");
+    public static final ResourceKey<ResearchEntry> HEXWEAVE = create("hexweave");
+    public static final ResourceKey<ResearchEntry> SAINTSWOOL = create("saintswool");
+    public static final ResourceKey<ResearchEntry> ARTIFICIAL_MANA_FONTS = create("artificial_mana_fonts");
+    public static final ResourceKey<ResearchEntry> FORBIDDEN_MANA_FONTS = create("forbidden_mana_fonts");
+    public static final ResourceKey<ResearchEntry> HEAVENLY_MANA_FONTS = create("heavenly_mana_fonts");
+    public static final ResourceKey<ResearchEntry> ESSENCE_CASK_ENCHANTED = create("essence_cask_enchanted");
+    public static final ResourceKey<ResearchEntry> ESSENCE_CASK_FORBIDDEN = create("essence_cask_forbidden");
+    public static final ResourceKey<ResearchEntry> ESSENCE_CASK_HEAVENLY = create("essence_cask_heavenly");
+    public static final ResourceKey<ResearchEntry> WAND_GLAMOUR_TABLE = create("wand_glamour_table");
+    public static final ResourceKey<ResearchEntry> ATTUNEMENT_SHACKLES = create("attunement_shackles");
+    public static final ResourceKey<ResearchEntry> WAND_CORE_HEARTWOOD = create("wand_core_heartwood");
+    public static final ResourceKey<ResearchEntry> WAND_CORE_OBSIDIAN = create("wand_core_obsidian");
+    public static final ResourceKey<ResearchEntry> WAND_CORE_CORAL = create("wand_core_coral");
+    public static final ResourceKey<ResearchEntry> WAND_CORE_BAMBOO = create("wand_core_bamboo");
+    public static final ResourceKey<ResearchEntry> WAND_CORE_SUNWOOD = create("wand_core_sunwood");
+    public static final ResourceKey<ResearchEntry> WAND_CORE_MOONWOOD = create("wand_core_moonwood");
+    public static final ResourceKey<ResearchEntry> WAND_CORE_BONE = create("wand_core_bone");
+    public static final ResourceKey<ResearchEntry> WAND_CORE_BLAZE_ROD = create("wand_core_blaze_rod");
+    public static final ResourceKey<ResearchEntry> WAND_CORE_PURPUR = create("wand_core_purpur");
+    public static final ResourceKey<ResearchEntry> WAND_CAP_IRON = create("wand_cap_iron");
+    public static final ResourceKey<ResearchEntry> WAND_CAP_GOLD = create("wand_cap_gold");
+    public static final ResourceKey<ResearchEntry> WAND_CAP_PRIMALITE = create("wand_cap_primalite");
+    public static final ResourceKey<ResearchEntry> WAND_CAP_HEXIUM = create("wand_cap_hexium");
+    public static final ResourceKey<ResearchEntry> WAND_CAP_HALLOWSTEEL = create("wand_cap_hallowsteel");
+    public static final ResourceKey<ResearchEntry> WAND_GEM_APPRENTICE = create("wand_gem_apprentice");
+    public static final ResourceKey<ResearchEntry> WAND_GEM_ADEPT = create("wand_gem_adept");
+    public static final ResourceKey<ResearchEntry> WAND_GEM_WIZARD = create("wand_gem_wizard");
+    public static final ResourceKey<ResearchEntry> WAND_GEM_ARCHMAGE = create("wand_gem_archmage");
 
     // Alchemy research entries
     // TODO Place in correct order after bootstrapping
+    public static final ResourceKey<ResearchEntry> BASIC_ALCHEMY = create("basic_alchemy");
+    public static final ResourceKey<ResearchEntry> EXPERT_ALCHEMY = create("expert_alchemy");
+    public static final ResourceKey<ResearchEntry> MASTER_ALCHEMY = create("master_alchemy");
+    public static final ResourceKey<ResearchEntry> SUPREME_ALCHEMY = create("supreme_alchemy");
+    public static final ResourceKey<ResearchEntry> COMPLETE_ALCHEMY = create("complete_alchemy");
+    public static final ResourceKey<ResearchEntry> STONEMELDING = create("stonemelding");
+    public static final ResourceKey<ResearchEntry> SKYGLASS = create("skyglass");
+    public static final ResourceKey<ResearchEntry> SHARD_SYNTHESIS = create("shard_synthesis");
+    public static final ResourceKey<ResearchEntry> SHARD_DESYNTHESIS = create("shard_desynthesis");
+    public static final ResourceKey<ResearchEntry> PRIMALITE = create("primalite");
+    public static final ResourceKey<ResearchEntry> CRYSTAL_SYNTHESIS = create("crystal_synthesis");
+    public static final ResourceKey<ResearchEntry> CRYSTAL_DESYNTHESIS = create("crystal_desynthesis");
+    public static final ResourceKey<ResearchEntry> HEXIUM = create("hexium");
+    public static final ResourceKey<ResearchEntry> CLUSTER_SYNTHESIS = create("cluster_synthesis");
+    public static final ResourceKey<ResearchEntry> CLUSTER_DESYNTHESIS = create("cluster_desynthesis");
+    public static final ResourceKey<ResearchEntry> HALLOWSTEEL = create("hallowsteel");
     public static final ResourceKey<ResearchEntry> CALCINATOR_BASIC = create("calcinator_basic");
-    
+    public static final ResourceKey<ResearchEntry> CALCINATOR_ENCHANTED = create("calcinator_enchanted");
+    public static final ResourceKey<ResearchEntry> CALCINATOR_FORBIDDEN = create("calcinator_forbidden");
+    public static final ResourceKey<ResearchEntry> CALCINATOR_HEAVENLY = create("calcinator_heavenly");
+    public static final ResourceKey<ResearchEntry> CRYOTREATMENT = create("cryotreatment");
+    public static final ResourceKey<ResearchEntry> SANGUINE_CRUCIBLE = create("sanguine_crucible");
+    public static final ResourceKey<ResearchEntry> SANGUINE_CORE_LAND_ANIMALS = create("sanguine_core_land_animals");
+    public static final ResourceKey<ResearchEntry> SANGUINE_CORE_SEA_CREATURES = create("sanguine_core_sea_creatures");
+    public static final ResourceKey<ResearchEntry> SANGUINE_CORE_FLYING_CREATURES = create("sanguine_core_flying_creatures");
+    public static final ResourceKey<ResearchEntry> SANGUINE_CORE_PLANTS = create("sanguine_core_plants");
+    public static final ResourceKey<ResearchEntry> SANGUINE_CORE_UNDEAD = create("sanguine_core_undead");
+    public static final ResourceKey<ResearchEntry> SANGUINE_CORE_SAPIENTS = create("sanguine_core_sapients");
+    public static final ResourceKey<ResearchEntry> SANGUINE_CORE_DEMONS = create("sanguine_core_demons");
+    public static final ResourceKey<ResearchEntry> SANGUINE_CORE_ALIENS = create("sanguine_core_aliens");
+    public static final ResourceKey<ResearchEntry> IGNYX = create("ignyx");
+    public static final ResourceKey<ResearchEntry> SYNTHETIC_GEM_BUDS = create("synthetic_gem_buds");
+
     // Sorcery research entries
     // TODO Place in correct order after bootstrapping
+    public static final ResourceKey<ResearchEntry> BASIC_SORCERY = create("basic_sorcery");
+    public static final ResourceKey<ResearchEntry> EXPERT_SORCERY = create("expert_sorcery");
+    public static final ResourceKey<ResearchEntry> MASTER_SORCERY = create("master_sorcery");
+    public static final ResourceKey<ResearchEntry> SUPREME_SORCERY = create("supreme_sorcery");
+    public static final ResourceKey<ResearchEntry> COMPLETE_SORCERY = create("complete_sorcery");
     public static final ResourceKey<ResearchEntry> WAND_INSCRIPTION = create("wand_inscription");
+    public static final ResourceKey<ResearchEntry> SPELL_VEHICLE_PROJECTILE = create("spell_vehicle_projectile");
+    public static final ResourceKey<ResearchEntry> SPELL_VEHICLE_BOLT = create("spell_vehicle_bolt");
+    public static final ResourceKey<ResearchEntry> SPELL_PAYLOAD_FROST = create("spell_payload_frost");
+    public static final ResourceKey<ResearchEntry> SPELL_PAYLOAD_LIGHTNING = create("spell_payload_lightning");
+    public static final ResourceKey<ResearchEntry> SPELL_PAYLOAD_SOLAR = create("spell_payload_solar");
+    public static final ResourceKey<ResearchEntry> SPELL_PAYLOAD_LUNAR = create("spell_payload_lunar");
+    public static final ResourceKey<ResearchEntry> SPELL_PAYLOAD_BLOOD = create("spell_payload_blood");
+    public static final ResourceKey<ResearchEntry> SPELL_PAYLOAD_FLAME = create("spell_payload_flame");
+    public static final ResourceKey<ResearchEntry> SPELL_PAYLOAD_VOID = create("spell_payload_void");
+    public static final ResourceKey<ResearchEntry> SPELL_PAYLOAD_HOLY = create("spell_payload_holy");
+    public static final ResourceKey<ResearchEntry> SPELL_PAYLOAD_BREAK = create("spell_payload_break");
+    public static final ResourceKey<ResearchEntry> SPELL_PAYLOAD_CONJURE_STONE = create("spell_payload_conjure_stone");
+    public static final ResourceKey<ResearchEntry> SPELL_PAYLOAD_CONJURE_WATER = create("spell_payload_conjure_water");
+    public static final ResourceKey<ResearchEntry> SPELL_PAYLOAD_SHEAR = create("spell_payload_shear");
+    public static final ResourceKey<ResearchEntry> SPELL_PAYLOAD_FLIGHT = create("spell_payload_flight");
+    public static final ResourceKey<ResearchEntry> SPELL_PAYLOAD_CONJURE_LIGHT = create("spell_payload_conjure_light");
+    public static final ResourceKey<ResearchEntry> SPELL_PAYLOAD_HEALING = create("spell_payload_healing");
+    public static final ResourceKey<ResearchEntry> SPELL_PAYLOAD_POLYMORPH = create("spell_payload_polymorph");
     public static final ResourceKey<ResearchEntry> SPELL_PAYLOAD_POLYMORPH_SHEEP = create("spell_payload_polymorph_sheep");
+    public static final ResourceKey<ResearchEntry> SPELL_PAYLOAD_CONJURE_ANIMAL = create("spell_payload_conjure_animal");
+    public static final ResourceKey<ResearchEntry> SPELL_PAYLOAD_CONJURE_LAVA = create("spell_payload_conjure_lava");
+    public static final ResourceKey<ResearchEntry> SPELL_PAYLOAD_DRAIN_SOUL = create("spell_payload_drain_soul");
+    public static final ResourceKey<ResearchEntry> SPELL_PAYLOAD_TELEPORT = create("spell_payload_teleport");
+    public static final ResourceKey<ResearchEntry> SPELL_PAYLOAD_CONSECRATE = create("spell_payload_consecrate");
+    public static final ResourceKey<ResearchEntry> SPELL_MOD_AMPLIFY = create("spell_mod_amplify");
+    public static final ResourceKey<ResearchEntry> SPELL_MOD_BURST = create("spell_mod_burst");
+    public static final ResourceKey<ResearchEntry> SPELL_MOD_QUICKEN = create("spell_mod_quicken");
+    public static final ResourceKey<ResearchEntry> SPELL_MOD_MINE = create("spell_mod_mine");
+    public static final ResourceKey<ResearchEntry> SPELL_MOD_FORK = create("spell_mod_fork");
     
     // Runeworking research entries
     // TODO Place in correct order after bootstrapping
     public static final ResourceKey<ResearchEntry> BASIC_RUNEWORKING = create("basic_runeworking");
+    public static final ResourceKey<ResearchEntry> EXPERT_RUNEWORKING = create("expert_runeworking");
+    public static final ResourceKey<ResearchEntry> MASTER_RUNEWORKING = create("master_runeworking");
+    public static final ResourceKey<ResearchEntry> SUPREME_RUNEWORKING = create("supreme_runeworking");
+    public static final ResourceKey<ResearchEntry> COMPLETE_RUNEWORKING = create("complete_runeworking");
     public static final ResourceKey<ResearchEntry> RUNE_EARTH = create("rune_earth");
     public static final ResourceKey<ResearchEntry> RUNE_SEA = create("rune_sea");
     public static final ResourceKey<ResearchEntry> RUNE_SKY = create("rune_sky");
@@ -85,10 +198,18 @@ public class ResearchEntries {
     public static final ResourceKey<ResearchEntry> RUNE_INSIGHT = create("rune_insight");
     public static final ResourceKey<ResearchEntry> RUNE_POWER = create("rune_power");
     public static final ResourceKey<ResearchEntry> RUNE_GRACE = create("rune_grace");
+    public static final ResourceKey<ResearchEntry> RUNIC_GRINDSTONE = create("runic_grindstone");
+    public static final ResourceKey<ResearchEntry> RECALL_STONE = create("recall_stone");
+    public static final ResourceKey<ResearchEntry> RUNIC_TRIM = create("runic_trim");
+    public static final ResourceKey<ResearchEntry> ENDERWARD = create("enderward");
     
     // Ritual Magick research entries
     // TODO Place in correct order after bootstrapping
     public static final ResourceKey<ResearchEntry> BASIC_RITUAL = create("basic_ritual");
+    public static final ResourceKey<ResearchEntry> EXPERT_RITUAL = create("expert_ritual");
+    public static final ResourceKey<ResearchEntry> MASTER_RITUAL = create("master_ritual");
+    public static final ResourceKey<ResearchEntry> SUPREME_RITUAL = create("supreme_ritual");
+    public static final ResourceKey<ResearchEntry> COMPLETE_RITUAL = create("complete_ritual");
     public static final ResourceKey<ResearchEntry> MANAFRUIT = create("manafruit");
     public static final ResourceKey<ResearchEntry> AMBROSIA = create("ambrosia");
 
