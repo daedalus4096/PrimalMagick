91c14c32cca2083123acadfe74a1fa408cb5ba9e
daedalus4096 <michael.a.bunting@gmail.com>
2024-06-11 22:45:28 -0700

    Specify default recipe expertise values

--- src/main/java/com/verdantartifice/primalmagick/common/research/ResearchTier.java
+++ src/main/java/com/verdantartifice/primalmagick/common/research/ResearchTier.java
@@ -5,17 +5,29 @@ import com.mojang.serialization.Codec;
 import net.minecraft.util.StringRepresentable;
 
 public enum ResearchTier implements StringRepresentable {
-    BASIC("basic"),
-    EXPERT("expert"),
-    MASTER("master"),
-    SUPREME("supreme");
+    BASIC("basic", 1, 4),
+    EXPERT("expert", 5, 20),
+    MASTER("master", 25, 100),
+    SUPREME("supreme", 125, 500);
     
     public static final Codec<ResearchTier> CODEC = StringRepresentable.fromEnum(ResearchTier::values);
     
     private final String name;
+    private final int defaultExpertise;
+    private final int defaultBonusExpertise;
     
-    private ResearchTier(String name) {
+    private ResearchTier(String name, int defaultExp, int defaultBonusExp) {
         this.name = name;
+        this.defaultExpertise = defaultExp;
+        this.defaultBonusExpertise = defaultBonusExp;
+    }
+    
+    public int getDefaultExpertise() {
+        return this.defaultExpertise;
+    }
+    
+    public int getDefaultBonusExpertise() {
+        return this.defaultBonusExpertise;
     }
 
     @Override
