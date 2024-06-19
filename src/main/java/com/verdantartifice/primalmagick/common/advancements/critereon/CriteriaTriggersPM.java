a1a97dfdf39c31a9111aa13668436bcf362e27aa
daedalus4096 <michael.a.bunting@gmail.com>
2024-06-18 22:45:07 -0700

    Add stat value advancement criterion

--- src/main/java/com/verdantartifice/primalmagick/common/advancements/critereon/CriteriaTriggersPM.java
+++ src/main/java/com/verdantartifice/primalmagick/common/advancements/critereon/CriteriaTriggersPM.java
@@ -2,4 +2,5 @@ package com.verdantartifice.primalmagick.common.advancements.critereon;
 
 public class CriteriaTriggersPM {
     public static final ResearchCompletedTrigger RESEARCH_COMPLETED = new ResearchCompletedTrigger();
+    public static final StatValueTrigger STAT_VALUE = new StatValueTrigger();
 }
