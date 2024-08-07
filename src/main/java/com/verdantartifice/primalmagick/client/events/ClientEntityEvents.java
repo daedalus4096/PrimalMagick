b339f9cf50dad776da10f91204f092c76cfc7b7e
daedalus4096 <michael.a.bunting@gmail.com>
2024-08-06 19:02:32 -0700

    Change event when client lexicons are updated

---
+++ src/main/java/com/verdantartifice/primalmagick/client/events/ClientEntityEvents.java
@@ -0,0 +1,28 @@
+package com.verdantartifice.primalmagick.client.events;
+
+import com.verdantartifice.primalmagick.PrimalMagick;
+import com.verdantartifice.primalmagick.client.books.LexiconLoader;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Respond to client-side Forge entity events.
+ * 
+ * @author Daedalus4096
+ */
+@Mod.EventBusSubscriber(modid=PrimalMagick.MODID, value=Dist.CLIENT)
+public class ClientEntityEvents {
+    @SubscribeEvent
+    public static void onEntityJoinLevel(EntityJoinLevelEvent event) {
+        Minecraft mc = Minecraft.getInstance();
+        if (event.getEntity() instanceof Player player && player.getId() == mc.player.getId()) {
+            // Update client lexicons with tag-delineated book data
+            LexiconLoader.getInstance().updateWithTagData(event.getLevel().registryAccess());
+        }
+    }
+}
