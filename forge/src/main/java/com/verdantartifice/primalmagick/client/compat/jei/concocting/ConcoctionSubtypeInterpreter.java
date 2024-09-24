ea23112e32dfd954f6288c378faf7c80cf63d0a0
daedalus4096 <michael.a.bunting@gmail.com>
2024-09-23 23:53:49 -0700

    Update concoction subtype interpreter

--- forge/src/main/java/com/verdantartifice/primalmagick/client/compat/jei/concocting/ConcoctionSubtypeInterpreter.java
+++ forge/src/main/java/com/verdantartifice/primalmagick/client/compat/jei/concocting/ConcoctionSubtypeInterpreter.java
@@ -4,22 +4,24 @@ import com.verdantartifice.primalmagick.common.concoctions.ConcoctionType;
 import com.verdantartifice.primalmagick.common.concoctions.ConcoctionUtils;
 
 import mezz.jei.api.ingredients.subtypes.IIngredientSubtypeInterpreter;
+import mezz.jei.api.ingredients.subtypes.ISubtypeInterpreter;
 import mezz.jei.api.ingredients.subtypes.UidContext;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.alchemy.Potion;
 import net.minecraft.world.item.alchemy.PotionContents;
+import org.jetbrains.annotations.Nullable;
 
-public class ConcoctionSubtypeInterpreter implements IIngredientSubtypeInterpreter<ItemStack> {
+public class ConcoctionSubtypeInterpreter implements ISubtypeInterpreter<ItemStack> {
     public static final ConcoctionSubtypeInterpreter INSTANCE = new ConcoctionSubtypeInterpreter();
     
     private ConcoctionSubtypeInterpreter() {}
 
     @Override
-    public String apply(ItemStack itemStack, UidContext context) {
+    public Object getSubtypeData(ItemStack itemStack, UidContext context) {
         ConcoctionType type = ConcoctionUtils.getConcoctionType(itemStack);
-        if (type == null || !itemStack.has(DataComponents.POTION_CONTENTS)) {
-            return IIngredientSubtypeInterpreter.NONE;
+        if (!itemStack.has(DataComponents.POTION_CONTENTS)) {
+            return null;
         }
         
         PotionContents contents = itemStack.get(DataComponents.POTION_CONTENTS);
@@ -30,5 +32,10 @@ public class ConcoctionSubtypeInterpreter implements IIngredientSubtypeInterpret
         });
 
         return stringBuilder.toString();
+    }
+
+    @Override
+    public String getLegacyStringSubtypeInfo(ItemStack ingredient, UidContext context) {
+        return "";
     }
 }
