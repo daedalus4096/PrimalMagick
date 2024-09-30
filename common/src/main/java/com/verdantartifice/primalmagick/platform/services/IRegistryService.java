419a6516de16e3337779f94ec41d3a82f8b4bcb2
daedalus4096 <michael.a.bunting@gmail.com>
2024-09-30 09:15:57 -0700

    Fix block registration to use registry item interface

--- common/src/main/java/com/verdantartifice/primalmagick/platform/services/IRegistryService.java
+++ common/src/main/java/com/verdantartifice/primalmagick/platform/services/IRegistryService.java
@@ -1,5 +1,6 @@
 package com.verdantartifice.primalmagick.platform.services;
 
+import com.verdantartifice.primalmagick.common.registries.IRegistryItem;
 import net.minecraft.resources.ResourceLocation;
 
 import javax.annotation.Nullable;
@@ -9,10 +10,10 @@ import java.util.function.Supplier;
  * Base interface for a service which provides access to a Minecraft object registry. Meant to be extended with a
  * parameterized type for implementation.
  *
- * @param <T> the type of object contained in the registry
+ * @param <R> the type of object contained in the registry
  * @author Daedalus4096
  */
-public interface IRegistryService<T> {
+public interface IRegistryService<R> {
     /**
      * Register the supplied value with this registry under the given name.
      *
@@ -20,7 +21,7 @@ public interface IRegistryService<T> {
      * @param supplier a supplier which provides the value to be registered
      * @return the resource location with which the value can be retrieved
      */
-    ResourceLocation register(String name, Supplier<? extends T> supplier);
+    <T extends R> IRegistryItem<R, T> register(String name, Supplier<T> supplier);
 
     /**
      * Retrieve the registered value with the given name.
@@ -28,8 +29,7 @@ public interface IRegistryService<T> {
      * @param id the ID of the value to be retrieved
      * @return the named value, or null if not present in this registry
      */
-    @Nullable
-    T get(ResourceLocation id);
+    @Nullable R get(ResourceLocation id);
 
     /**
      * Determine whether an object is present in this registry for the given resource location.
