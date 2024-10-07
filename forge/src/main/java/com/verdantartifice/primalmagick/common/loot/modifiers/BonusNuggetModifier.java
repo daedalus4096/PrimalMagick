bf2aca4c0d1b0963406b2befdb5d10970499b82f
daedalus4096 <michael.a.bunting@gmail.com>
2024-10-07 00:37:26 -0700

    Progress commit for global loot modifiers

--- forge/src/main/java/com/verdantartifice/primalmagick/common/loot/modifiers/BonusNuggetModifier.java
+++ forge/src/main/java/com/verdantartifice/primalmagick/common/loot/modifiers/BonusNuggetModifier.java
@@ -3,24 +3,19 @@ package com.verdantartifice.primalmagick.common.loot.modifiers;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import com.verdantartifice.primalmagick.common.enchantments.EnchantmentsPM;
+import com.verdantartifice.primalmagick.common.loot.LootModifiers;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootContext;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.common.loot.LootModifier;
-import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.Map;
-import java.util.Optional;
-import java.util.stream.IntStream;
 
 /**
  * Global loot modifier that gives a chance for bonus nuggets when mining quartz or metal ores.
@@ -44,21 +39,7 @@ public class BonusNuggetModifier extends LootModifier {
 
     @Override
     protected ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
-        BlockState state = context.getParamOrNull(LootContextParams.BLOCK_STATE);
-        ItemStack tool = context.getParamOrNull(LootContextParams.TOOL);
-        int enchantmentLevel = tool == null ? 0 : tool.getEnchantments().getLevel(context.getResolver().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(EnchantmentsPM.LUCKY_STRIKE));
-        if (state != null && enchantmentLevel > 0) {
-            this.nuggetMap.forEach((blockTag, itemTag) -> {
-                if (state.is(blockTag) && ForgeRegistries.ITEMS.tags().isKnownTagName(itemTag)) {
-                    Optional<Item> nuggetOpt = ForgeRegistries.ITEMS.tags().getTag(itemTag).stream().findFirst();
-                    nuggetOpt.ifPresent(nugget -> {
-                        int nuggetCount = IntStream.range(0, enchantmentLevel).map(i -> context.getRandom().nextFloat() < this.chance ? 1 : 0).sum();
-                        generatedLoot.add(new ItemStack(nugget, nuggetCount));
-                    });
-                }
-            });
-        }
-        return generatedLoot;
+        return LootModifiers.bonusNugget(generatedLoot, context, this.chance, this.nuggetMap);
     }
 
     @Override
--- neoforge/src/main/java/com/verdantartifice/primalmagick/common/loot/modifiers/BountyFarmingModifier.java
+++ neoforge/src/main/java/com/verdantartifice/primalmagick/common/loot/modifiers/BountyFarmingModifier.java
@@ -3,21 +3,13 @@ package com.verdantartifice.primalmagick.common.loot.modifiers;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import com.verdantartifice.primalmagick.common.enchantments.EnchantmentsPM;
-import com.verdantartifice.primalmagick.common.util.ItemUtils;
+import com.verdantartifice.primalmagick.common.loot.LootModifiers;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.storage.loot.LootContext;
-import net.minecraft.world.level.storage.loot.LootTable;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.neoforged.neoforge.common.loot.IGlobalLootModifier;
 import net.neoforged.neoforge.common.loot.LootModifier;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Global loot modifier that gives a chance for bonus rolls on crops.  Unlike Fortune, this
@@ -36,19 +28,9 @@ public class BountyFarmingModifier extends LootModifier {
         this.chance = chance;
     }
     
-    @SuppressWarnings("deprecation")
     @Override
     protected ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
-        LootTable table = context.getLevel().getServer().reloadableRegistries().getLootTable(context.getParamOrNull(LootContextParams.BLOCK_STATE).getBlock().getLootTable());
-        int enchantmentLevel = EnchantmentHelper.getItemEnchantmentLevel(context.getResolver().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(EnchantmentsPM.BOUNTY), context.getParamOrNull(LootContextParams.TOOL));
-        for (int index = 0; index < enchantmentLevel; index++) {
-            if (context.getRandom().nextFloat() < this.chance) {
-                List<ItemStack> bonusList = new ArrayList<>();
-                table.getRandomItemsRaw(context, bonusList::add);   // Use deprecated method to avoid recursive modification of loot generated
-                generatedLoot = ItemUtils.mergeItemStackLists(generatedLoot, bonusList).stream().collect(ObjectArrayList.toList());
-            }
-        }
-        return generatedLoot;
+        return LootModifiers.bountyFarming(generatedLoot, context, this.chance);
     }
 
     @Override
