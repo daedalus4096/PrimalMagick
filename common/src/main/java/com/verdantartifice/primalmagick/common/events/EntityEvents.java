48eea7306df3c3ffb86c31fa1c1a7cd277e8800a
daedalus4096 <michael.a.bunting@gmail.com>
2024-10-12 00:00:34 -0700

    Finish entity events

--- common/src/main/java/com/verdantartifice/primalmagick/common/events/EntityEvents.java
+++ common/src/main/java/com/verdantartifice/primalmagick/common/events/EntityEvents.java
@@ -1,6 +1,5 @@
 package com.verdantartifice.primalmagick.common.events;
 
-import com.verdantartifice.primalmagick.Constants;
 import com.verdantartifice.primalmagick.common.blocks.BlocksPM;
 import com.verdantartifice.primalmagick.common.blocks.misc.EnderwardBlock;
 import com.verdantartifice.primalmagick.common.capabilities.PrimalMagickCapabilities;
@@ -12,16 +11,13 @@ import com.verdantartifice.primalmagick.common.research.ResearchEntries;
 import com.verdantartifice.primalmagick.common.research.ResearchManager;
 import com.verdantartifice.primalmagick.common.stats.StatsManager;
 import com.verdantartifice.primalmagick.common.stats.StatsPM;
-import com.verdantartifice.primalmagick.common.tags.DamageTypeTagsPM;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.Mth;
-import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.animal.Wolf;
@@ -30,15 +26,6 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ShieldItem;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
-import net.minecraftforge.event.entity.EntityTeleportEvent;
-import net.minecraftforge.event.entity.living.AnimalTameEvent;
-import net.minecraftforge.event.entity.living.BabyEntitySpawnEvent;
-import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
-import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
-import net.minecraftforge.event.entity.living.LootingLevelEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 
 /**
  * Handlers for miscellaneous entity events.
@@ -90,12 +77,8 @@ public class EntityEvents {
         }
     }
     
-    @SubscribeEvent
-    public static void onLivingEntityUseItemTick(LivingEntityUseItemEvent.Tick event) {
+    public static void onLivingEntityUseItemTick(LivingEntity entity, ItemStack stack, int currentDuration) {
         // Stack up resistance on the wielders of shields with the Bulwark enchantment
-        LivingEntity entity = event.getEntity();
-        ItemStack stack = event.getItem();
-        int currentDuration = event.getDuration();
         int maxDuration = stack.getUseDuration(entity);
         int delta = maxDuration - currentDuration;
         int enchantLevel = EnchantmentHelperPM.getEnchantmentLevel(stack, EnchantmentsPM.BULWARK, entity.registryAccess());
@@ -106,24 +89,10 @@ public class EntityEvents {
         }
     }
     
-    @SubscribeEvent
-    public static void onLootingLevel(LootingLevelEvent event) {
-        // If the damage was magickal, apply the Treasure enchantment as a looting modifier if greater than what's already there
-        DamageSource source = event.getDamageSource();
-        if (source != null && source.is(DamageTypeTagsPM.IS_MAGIC)) {
-            Entity caster = source.getEntity();
-            if (caster != null && caster instanceof LivingEntity living) {
-                int treasureLevel = EnchantmentHelperPM.getEquippedEnchantmentLevel(living, EnchantmentsPM.TREASURE);
-                event.setLootingLevel(Math.max(event.getLootingLevel(), treasureLevel));
-            }
-        }
-    }
-    
-    @SubscribeEvent
-    public static void onLivingEquipmentChange(LivingEquipmentChangeEvent event) {
+    public static void onLivingEquipmentChange(LivingEntity livingEntity, ItemStack fromStack, ItemStack toStack) {
         // If a player is donning or removing a warded piece of armor, update their max ward level
-        if (event.getEntity() instanceof ServerPlayer serverPlayer &&
-                (WardingModuleItem.hasWardAttached(event.getFrom()) || WardingModuleItem.hasWardAttached(event.getTo()) ) ) {
+        if (livingEntity instanceof ServerPlayer serverPlayer &&
+                (WardingModuleItem.hasWardAttached(fromStack) || WardingModuleItem.hasWardAttached(toStack) ) ) {
             PrimalMagickCapabilities.getWard(serverPlayer).ifPresent(playerWard -> {
                 int newMax = playerWard.getApplicableSlots().stream().map(slot -> serverPlayer.getItemBySlot(slot)).filter(WardingModuleItem::hasWardAttached)
                         .mapToInt(stack -> 1 + WardingModuleItem.getAttachedWardLevel(stack)).sum();
--- forge/src/main/java/com/verdantartifice/primalmagick/common/events/EntityEventListeners.java
+++ forge/src/main/java/com/verdantartifice/primalmagick/common/events/EntityEventListeners.java
@@ -1,40 +1,12 @@
 package com.verdantartifice.primalmagick.common.events;
 
 import com.verdantartifice.primalmagick.Constants;
-import com.verdantartifice.primalmagick.common.blocks.BlocksPM;
-import com.verdantartifice.primalmagick.common.blocks.misc.EnderwardBlock;
-import com.verdantartifice.primalmagick.common.capabilities.PrimalMagickCapabilities;
-import com.verdantartifice.primalmagick.common.effects.EffectsPM;
-import com.verdantartifice.primalmagick.common.enchantments.EnchantmentHelperPM;
-import com.verdantartifice.primalmagick.common.enchantments.EnchantmentsPM;
-import com.verdantartifice.primalmagick.common.items.armor.WardingModuleItem;
-import com.verdantartifice.primalmagick.common.research.ResearchEntries;
-import com.verdantartifice.primalmagick.common.research.ResearchManager;
-import com.verdantartifice.primalmagick.common.stats.StatsManager;
-import com.verdantartifice.primalmagick.common.stats.StatsPM;
-import com.verdantartifice.primalmagick.common.tags.DamageTypeTagsPM;
-import net.minecraft.ChatFormatting;
-import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.util.Mth;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.animal.Wolf;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.ShieldItem;
-import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.entity.EntityTeleportEvent;
 import net.minecraftforge.event.entity.living.AnimalTameEvent;
 import net.minecraftforge.event.entity.living.BabyEntitySpawnEvent;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
-import net.minecraftforge.event.entity.living.LootingLevelEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -95,44 +67,11 @@ public class EntityEventListeners {
     
     @SubscribeEvent
     public static void onLivingEntityUseItemTick(LivingEntityUseItemEvent.Tick event) {
-        // Stack up resistance on the wielders of shields with the Bulwark enchantment
-        LivingEntity entity = event.getEntity();
-        ItemStack stack = event.getItem();
-        int currentDuration = event.getDuration();
-        int maxDuration = stack.getUseDuration(entity);
-        int delta = maxDuration - currentDuration;
-        int enchantLevel = EnchantmentHelperPM.getEnchantmentLevel(stack, EnchantmentsPM.BULWARK, entity.registryAccess());
-        if (stack.getItem() instanceof ShieldItem && delta > 0 && delta % 5 == 0 && enchantLevel > 0) {
-            MobEffectInstance effectInstance = entity.getEffect(MobEffects.DAMAGE_RESISTANCE);
-            int amplifier = (effectInstance == null) ? 0 : Mth.clamp(1 + effectInstance.getAmplifier(), 0, enchantLevel - 1);
-            entity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 10, amplifier));
-        }
-    }
-    
-    @SubscribeEvent
-    public static void onLootingLevel(LootingLevelEvent event) {
-        // If the damage was magickal, apply the Treasure enchantment as a looting modifier if greater than what's already there
-        DamageSource source = event.getDamageSource();
-        if (source != null && source.is(DamageTypeTagsPM.IS_MAGIC)) {
-            Entity caster = source.getEntity();
-            if (caster != null && caster instanceof LivingEntity living) {
-                int treasureLevel = EnchantmentHelperPM.getEquippedEnchantmentLevel(living, EnchantmentsPM.TREASURE);
-                event.setLootingLevel(Math.max(event.getLootingLevel(), treasureLevel));
-            }
-        }
+        EntityEvents.onLivingEntityUseItemTick(event.getEntity(), event.getItem(), event.getDuration());
     }
     
     @SubscribeEvent
     public static void onLivingEquipmentChange(LivingEquipmentChangeEvent event) {
-        // If a player is donning or removing a warded piece of armor, update their max ward level
-        if (event.getEntity() instanceof ServerPlayer serverPlayer &&
-                (WardingModuleItem.hasWardAttached(event.getFrom()) || WardingModuleItem.hasWardAttached(event.getTo()) ) ) {
-            PrimalMagickCapabilities.getWard(serverPlayer).ifPresent(playerWard -> {
-                int newMax = playerWard.getApplicableSlots().stream().map(slot -> serverPlayer.getItemBySlot(slot)).filter(WardingModuleItem::hasWardAttached)
-                        .mapToInt(stack -> 1 + WardingModuleItem.getAttachedWardLevel(stack)).sum();
-                playerWard.setMaxWard(newMax);
-                playerWard.sync(serverPlayer);
-            });
-        }
+        EntityEvents.onLivingEquipmentChange(event.getEntity(), event.getFrom(), event.getTo());
     }
 }
