971060b55a865b7c7a349c15e6562e0b81a913dd
daedalus4096 <michael.a.bunting@gmail.com>
2024-10-13 20:47:20 -0700

    Villager events

--- common/src/main/java/com/verdantartifice/primalmagick/common/events/VillagerEvents.java
+++ common/src/main/java/com/verdantartifice/primalmagick/common/events/VillagerEvents.java
@@ -1,37 +1,34 @@
 package com.verdantartifice.primalmagick.common.events;
 
-import com.verdantartifice.primalmagick.Constants;
-import com.verdantartifice.primalmagick.common.items.ItemRegistration;
+import com.verdantartifice.primalmagick.common.items.ItemsPM;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.entity.npc.VillagerTrades;
 import net.minecraft.world.entity.npc.VillagerTrades.EmeraldForItems;
+import net.minecraft.world.entity.npc.VillagerTrades.ItemListing;
 import net.minecraft.world.entity.npc.VillagerTrades.ItemsForEmeralds;
-import net.minecraftforge.event.village.VillagerTradesEvent;
-import net.minecraftforge.event.village.WandererTradesEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
 
 /**
  * Handlers for villager related events.
  * 
  * @author Daedalus4096
  */
-@Mod.EventBusSubscriber(modid= Constants.MOD_ID)
 public class VillagerEvents {
-    @SubscribeEvent
-    public static void onVillagerTradeSetup(VillagerTradesEvent event) {
+    public static void onVillagerTradeSetup(VillagerProfession type, Int2ObjectMap<List<ItemListing>> trades) {
         // Allow librarians to trade certain knowledge granting items
-        if (VillagerProfession.LIBRARIAN.equals(event.getType())) {
-            event.getTrades().get(1).add(new EmeraldForItems(ItemsPM.MYSTICAL_RELIC_FRAGMENT.get(), 3, 12, 2, 1));
-            event.getTrades().get(2).add(new ItemsForEmeralds(ItemsPM.OBSERVATION_NOTES.get(), 8, 1, 5));
-            event.getTrades().get(3).add(new EmeraldForItems(ItemsPM.MYSTICAL_RELIC.get(), 1, 12, 20, 5));
-            event.getTrades().get(4).add(new ItemsForEmeralds(ItemsPM.THEORY_NOTES.get(), 16, 1, 15));
+        if (VillagerProfession.LIBRARIAN.equals(type)) {
+            trades.get(1).add(new EmeraldForItems(ItemsPM.MYSTICAL_RELIC_FRAGMENT.get(), 3, 12, 2, 1));
+            trades.get(2).add(new ItemsForEmeralds(ItemsPM.OBSERVATION_NOTES.get(), 8, 1, 5));
+            trades.get(3).add(new EmeraldForItems(ItemsPM.MYSTICAL_RELIC.get(), 1, 12, 20, 5));
+            trades.get(4).add(new ItemsForEmeralds(ItemsPM.THEORY_NOTES.get(), 16, 1, 15));
         }
     }
     
-    @SubscribeEvent
-    public static void onWandererTradeSetup(WandererTradesEvent event) {
+    public static void onWandererTradeSetup(List<VillagerTrades.ItemListing> genericTrades, List<ItemListing> rareTrades) {
         // Allow wandering traders to sell certain knowledge granting items
-        event.getGenericTrades().add(new ItemsForEmeralds(ItemsPM.OBSERVATION_NOTES.get(), 8, 1, 5));
-        event.getRareTrades().add(new ItemsForEmeralds(ItemsPM.THEORY_NOTES.get(), 16, 1, 15));
+        genericTrades.add(new ItemsForEmeralds(ItemsPM.OBSERVATION_NOTES.get(), 8, 1, 5));
+        rareTrades.add(new ItemsForEmeralds(ItemsPM.THEORY_NOTES.get(), 16, 1, 15));
     }
 }
