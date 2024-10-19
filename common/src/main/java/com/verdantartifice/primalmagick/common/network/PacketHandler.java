93cdae63e951e059bd6dcfbeec4567dbb3ca8f40
daedalus4096 <michael.a.bunting@gmail.com>
2024-10-19 11:56:04 -0700

    Clean up packet handler

--- common/src/main/java/com/verdantartifice/primalmagick/common/network/PacketHandler.java
+++ common/src/main/java/com/verdantartifice/primalmagick/common/network/PacketHandler.java
@@ -1,80 +1,15 @@
 package com.verdantartifice.primalmagick.common.network;
 
-import com.verdantartifice.primalmagick.common.util.ResourceUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import com.verdantartifice.primalmagick.PrimalMagick;
 import com.verdantartifice.primalmagick.common.network.packets.IMessageToClient;
 import com.verdantartifice.primalmagick.common.network.packets.IMessageToServer;
-import com.verdantartifice.primalmagick.common.network.packets.config.AcknowledgementPacket;
-import com.verdantartifice.primalmagick.common.network.packets.config.UpdateAffinitiesConfigPacket;
-import com.verdantartifice.primalmagick.common.network.packets.config.UpdateLinguisticsGridsConfigPacket;
-import com.verdantartifice.primalmagick.common.network.packets.data.ContainerSetVarintDataPacket;
-import com.verdantartifice.primalmagick.common.network.packets.data.SetResearchTopicHistoryPacket;
-import com.verdantartifice.primalmagick.common.network.packets.data.SyncArcaneRecipeBookPacket;
-import com.verdantartifice.primalmagick.common.network.packets.data.SyncAttunementsPacket;
-import com.verdantartifice.primalmagick.common.network.packets.data.SyncCompanionsPacket;
-import com.verdantartifice.primalmagick.common.network.packets.data.SyncCooldownsPacket;
-import com.verdantartifice.primalmagick.common.network.packets.data.SyncKnowledgePacket;
-import com.verdantartifice.primalmagick.common.network.packets.data.SyncLinguisticsPacket;
-import com.verdantartifice.primalmagick.common.network.packets.data.SyncProgressPacket;
-import com.verdantartifice.primalmagick.common.network.packets.data.SyncResearchFlagsPacket;
-import com.verdantartifice.primalmagick.common.network.packets.data.SyncStatsPacket;
-import com.verdantartifice.primalmagick.common.network.packets.data.SyncWardPacket;
-import com.verdantartifice.primalmagick.common.network.packets.data.TileToClientPacket;
-import com.verdantartifice.primalmagick.common.network.packets.data.TileToServerPacket;
-import com.verdantartifice.primalmagick.common.network.packets.data.UpdateAffinitiesPacket;
-import com.verdantartifice.primalmagick.common.network.packets.data.UpdateLinguisticsGridsPacket;
-import com.verdantartifice.primalmagick.common.network.packets.fx.ManaSparklePacket;
-import com.verdantartifice.primalmagick.common.network.packets.fx.OfferingChannelPacket;
-import com.verdantartifice.primalmagick.common.network.packets.fx.PlayClientSoundPacket;
-import com.verdantartifice.primalmagick.common.network.packets.fx.PotionExplosionPacket;
-import com.verdantartifice.primalmagick.common.network.packets.fx.PropMarkerPacket;
-import com.verdantartifice.primalmagick.common.network.packets.fx.RemovePropMarkerPacket;
-import com.verdantartifice.primalmagick.common.network.packets.fx.SpellBoltPacket;
-import com.verdantartifice.primalmagick.common.network.packets.fx.SpellImpactPacket;
-import com.verdantartifice.primalmagick.common.network.packets.fx.SpellTrailPacket;
-import com.verdantartifice.primalmagick.common.network.packets.fx.SpellcraftingRunePacket;
-import com.verdantartifice.primalmagick.common.network.packets.fx.TeleportArrivalPacket;
-import com.verdantartifice.primalmagick.common.network.packets.fx.WandPoofPacket;
-import com.verdantartifice.primalmagick.common.network.packets.misc.AnalysisActionPacket;
-import com.verdantartifice.primalmagick.common.network.packets.misc.CycleActiveSpellPacket;
-import com.verdantartifice.primalmagick.common.network.packets.misc.OpenEnchantedBookScreenPacket;
-import com.verdantartifice.primalmagick.common.network.packets.misc.OpenGrimoireScreenPacket;
-import com.verdantartifice.primalmagick.common.network.packets.misc.OpenStaticBookScreenPacket;
-import com.verdantartifice.primalmagick.common.network.packets.misc.ResetFallDistancePacket;
-import com.verdantartifice.primalmagick.common.network.packets.misc.ScanEntityPacket;
-import com.verdantartifice.primalmagick.common.network.packets.misc.ScanItemPacket;
-import com.verdantartifice.primalmagick.common.network.packets.misc.ScanPositionPacket;
-import com.verdantartifice.primalmagick.common.network.packets.misc.SetActiveSpellPacket;
-import com.verdantartifice.primalmagick.common.network.packets.misc.WithdrawCaskEssencePacket;
-import com.verdantartifice.primalmagick.common.network.packets.recipe_book.ChangeArcaneRecipeBookSettingsPacket;
-import com.verdantartifice.primalmagick.common.network.packets.recipe_book.PlaceArcaneRecipePacket;
-import com.verdantartifice.primalmagick.common.network.packets.recipe_book.PlaceGhostArcaneRecipePacket;
-import com.verdantartifice.primalmagick.common.network.packets.recipe_book.SeenArcaneRecipePacket;
-import com.verdantartifice.primalmagick.common.network.packets.scribe_table.ChangeScribeTableModePacket;
-import com.verdantartifice.primalmagick.common.network.packets.scribe_table.StudyVocabularyActionPacket;
-import com.verdantartifice.primalmagick.common.network.packets.scribe_table.TranscribeActionPacket;
-import com.verdantartifice.primalmagick.common.network.packets.scribe_table.UnlockGridNodeActionPacket;
-import com.verdantartifice.primalmagick.common.network.packets.spellcrafting.SetSpellComponentPropertyPacket;
-import com.verdantartifice.primalmagick.common.network.packets.spellcrafting.SetSpellComponentTypeIndexPacket;
-import com.verdantartifice.primalmagick.common.network.packets.spellcrafting.SetSpellNamePacket;
-import com.verdantartifice.primalmagick.common.network.packets.theorycrafting.CompleteProjectPacket;
-import com.verdantartifice.primalmagick.common.network.packets.theorycrafting.SetProjectMaterialSelectionPacket;
-import com.verdantartifice.primalmagick.common.network.packets.theorycrafting.StartProjectPacket;
-
+import commonnetwork.api.Dispatcher;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.Connection;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.event.network.CustomPayloadEvent;
-import net.minecraftforge.network.Channel.VersionTest;
-import net.minecraftforge.network.ChannelBuilder;
 import net.minecraftforge.network.PacketDistributor;
-import net.minecraftforge.network.SimpleChannel;
 
 /**
  * Handler class for processing packets.  Responsible for all custom communication between the client and the server.
@@ -82,26 +17,14 @@ import net.minecraftforge.network.SimpleChannel;
  * @author Daedalus4096
  */
 public class PacketHandler {
-    private static final Logger LOGGER = LogManager.getLogger();
-    private static final ResourceLocation CHANNEL_NAME = ResourceUtils.loc("main_channel");
-    private static final int PROTOCOL_VERSION = 1;
-    
-    private static final SimpleChannel CHANNEL = ChannelBuilder
-            .named(CHANNEL_NAME)
-            .clientAcceptedVersions(VersionTest.exact(PROTOCOL_VERSION))
-            .serverAcceptedVersions(VersionTest.exact(PROTOCOL_VERSION))
-            .networkProtocolVersion(PROTOCOL_VERSION)
-            .simpleChannel()
-            .build();
-
     public static void sendToServer(IMessageToServer message) {
         // Send a packet from a client to the server
-        CHANNEL.send(message, PacketDistributor.SERVER.noArg());
+        Dispatcher.sendToServer(message);
     }
     
     public static void sendToPlayer(IMessageToClient message, ServerPlayer player) {
         // Send a message from the server to a specific player's client
-        CHANNEL.send(message, PacketDistributor.PLAYER.with(player));
+        Dispatcher.sendToClient(message, player);
     }
     
     public static void sendToAllAround(IMessageToClient message, ResourceKey<Level> dimension, BlockPos center, double radius) {
