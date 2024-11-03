c54e4434a1bfdd5f430d0661e91f863e8d39bc8e
daedalus4096 <michael.a.bunting@gmail.com>
2024-11-02 23:27:27 -0700

    Remove Forge dependency

--- common/src/main/java/com/verdantartifice/primalmagick/client/gui/widgets/InactiveWidget.java
+++ common/src/main/java/com/verdantartifice/primalmagick/client/gui/widgets/InactiveWidget.java
@@ -1,22 +1,28 @@
 package com.verdantartifice.primalmagick.client.gui.widgets;
 
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.network.chat.Component;
-import net.minecraftforge.client.gui.widget.ExtendedButton;
 
 /**
- * An unclickable version of a Forge ExtendedButton.
+ * An unclickable version of a button.
  * 
  * @author Daedalus4096
  */
-public class InactiveWidget extends ExtendedButton {
+public class InactiveWidget extends Button {
     public InactiveWidget(int x, int y, int width, int height, Component msg) {
         super(x, y, width, height, msg, new Button.OnPress() {
             @Override
             public void onPress(Button pButton) {
                 // Do nothing
             }
-        });
+        }, Button.DEFAULT_NARRATION);
         this.active = false;
+    }
+
+    @Override
+    protected void renderWidget(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick) {
+        // FIXME Does this need to mimic the renderWidget method of Forge's ExtendedButton?
+        super.renderWidget(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
     }
 }
