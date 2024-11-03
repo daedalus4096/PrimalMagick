116c47bb78adc759b50a1755ac79182996ca38ca
daedalus4096 <michael.a.bunting@gmail.com>
2024-11-02 20:31:30 -0700

    Remove unused imports

--- common/src/main/java/com/verdantartifice/primalmagick/common/spells/vehicles/SpellVehiclesPM.java
+++ common/src/main/java/com/verdantartifice/primalmagick/common/spells/vehicles/SpellVehiclesPM.java
@@ -1,19 +1,12 @@
 package com.verdantartifice.primalmagick.common.spells.vehicles;
 
 import com.mojang.serialization.MapCodec;
-import com.verdantartifice.primalmagick.Constants;
-import com.verdantartifice.primalmagick.PrimalMagick;
 import com.verdantartifice.primalmagick.common.registries.IRegistryItem;
-import com.verdantartifice.primalmagick.common.registries.RegistryKeysPM;
 import com.verdantartifice.primalmagick.common.research.requirements.AbstractRequirement;
 import com.verdantartifice.primalmagick.common.util.ResourceUtils;
 import com.verdantartifice.primalmagick.platform.Services;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.IForgeRegistry;
-import net.minecraftforge.registries.RegistryBuilder;
-import net.minecraftforge.registries.RegistryObject;
 
 import java.util.function.Supplier;
 
