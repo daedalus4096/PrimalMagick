39662661e059c442c8dee8918453fd00b5cf6d2c
Michael Bunting <michael.a.bunting@gmail.com>
2025-01-30 20:06:50 -0800

    Fix calcinator item handlers for Forge

--- forge/src/main/java/com/verdantartifice/primalmagick/common/tiles/crafting/AbstractCalcinatorTileEntityForge.java
+++ forge/src/main/java/com/verdantartifice/primalmagick/common/tiles/crafting/AbstractCalcinatorTileEntityForge.java
@@ -2,19 +2,30 @@ package com.verdantartifice.primalmagick.common.tiles.crafting;
 
 import com.verdantartifice.primalmagick.common.capabilities.CapabilitiesForge;
 import com.verdantartifice.primalmagick.common.capabilities.ITileResearchCache;
+import com.verdantartifice.primalmagick.common.tiles.IHasItemHandlerCapabilityForge;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.NonNullList;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.IItemHandler;
 
-public abstract class AbstractCalcinatorTileEntityForge extends AbstractCalcinatorTileEntity {
+public abstract class AbstractCalcinatorTileEntityForge extends AbstractCalcinatorTileEntity implements IHasItemHandlerCapabilityForge {
     protected LazyOptional<ITileResearchCache> researchCacheOpt = LazyOptional.of(() -> this.researchCache);
+    protected final NonNullList<LazyOptional<IItemHandler>> itemHandlerOpts;
 
     public AbstractCalcinatorTileEntityForge(BlockEntityType<? extends AbstractCalcinatorTileEntity> tileEntityType, BlockPos pos, BlockState state) {
         super(tileEntityType, pos, state);
+        this.itemHandlerOpts = this.makeItemHandlerOpts(this.itemHandlers);
+    }
+
+    @Override
+    public NonNullList<LazyOptional<IItemHandler>> getItemHandlerOpts() {
+        return this.itemHandlerOpts;
     }
 
     @Override
@@ -38,16 +49,23 @@ public abstract class AbstractCalcinatorTileEntityForge extends AbstractCalcinat
 
     @Override
     public <T> LazyOptional<T> getCapability(Capability<T> cap, Direction side) {
-        if (!this.remove && cap == CapabilitiesForge.RESEARCH_CACHE) {
-            return this.researchCacheOpt.cast();
-        } else {
+        if (this.remove) {
             return super.getCapability(cap, side);
+        } else {
+            if (cap == CapabilitiesForge.RESEARCH_CACHE) {
+                return this.researchCacheOpt.cast();
+            } else if (cap == ForgeCapabilities.ITEM_HANDLER) {
+                return this.getItemHandlerCapability(side).cast();
+            } else {
+                return super.getCapability(cap, side);
+            }
         }
     }
 
     @Override
     public void invalidateCaps() {
         super.invalidateCaps();
+        this.itemHandlerOpts.forEach(LazyOptional::invalidate);
         this.researchCacheOpt.invalidate();
     }
 }
