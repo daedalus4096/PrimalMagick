53787f31c5ae37c523adb1cd5662a0642896ff7c
Michael Bunting <michael.a.bunting@gmail.com>
2025-01-30 20:38:38 -0800

    Fix essence transmuter item handlers for Forge

--- forge/src/main/java/com/verdantartifice/primalmagick/common/tiles/devices/EssenceTransmuterTileEntityForge.java
+++ forge/src/main/java/com/verdantartifice/primalmagick/common/tiles/devices/EssenceTransmuterTileEntityForge.java
@@ -3,18 +3,29 @@ package com.verdantartifice.primalmagick.common.tiles.devices;
 import com.verdantartifice.primalmagick.common.capabilities.CapabilitiesForge;
 import com.verdantartifice.primalmagick.common.capabilities.IManaStorage;
 import com.verdantartifice.primalmagick.common.capabilities.ITileResearchCache;
+import com.verdantartifice.primalmagick.common.tiles.IHasItemHandlerCapabilityForge;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.NonNullList;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.IItemHandler;
 
-public class EssenceTransmuterTileEntityForge extends EssenceTransmuterTileEntity {
+public class EssenceTransmuterTileEntityForge extends EssenceTransmuterTileEntity implements IHasItemHandlerCapabilityForge {
     protected LazyOptional<IManaStorage<?>> manaStorageOpt = LazyOptional.of(() -> this.manaStorage);
     protected LazyOptional<ITileResearchCache> researchCacheOpt = LazyOptional.of(() -> this.researchCache);
+    protected final NonNullList<LazyOptional<IItemHandler>> itemHandlerOpts;
 
     public EssenceTransmuterTileEntityForge(BlockPos pos, BlockState state) {
         super(pos, state);
+        this.itemHandlerOpts = this.makeItemHandlerOpts(this.itemHandlers);
+    }
+
+    @Override
+    public NonNullList<LazyOptional<IItemHandler>> getItemHandlerOpts() {
+        return this.itemHandlerOpts;
     }
 
     @Override
@@ -28,18 +39,25 @@ public class EssenceTransmuterTileEntityForge extends EssenceTransmuterTileEntit
 
     @Override
     public <T> LazyOptional<T> getCapability(Capability<T> cap, Direction side) {
-        if (!this.remove && cap == CapabilitiesForge.MANA_STORAGE) {
-            return this.manaStorageOpt.cast();
-        } else if (!this.remove && cap == CapabilitiesForge.RESEARCH_CACHE) {
-            return this.researchCacheOpt.cast();
-        } else {
+        if (this.remove) {
             return super.getCapability(cap, side);
+        } else {
+            if (cap == CapabilitiesForge.MANA_STORAGE) {
+                return this.manaStorageOpt.cast();
+            } else if (cap == CapabilitiesForge.RESEARCH_CACHE) {
+                return this.researchCacheOpt.cast();
+            } else if (cap == ForgeCapabilities.ITEM_HANDLER) {
+                return this.getItemHandlerCapability(side).cast();
+            } else {
+                return super.getCapability(cap, side);
+            }
         }
     }
 
     @Override
     public void invalidateCaps() {
         super.invalidateCaps();
+        this.itemHandlerOpts.forEach(LazyOptional::invalidate);
         this.manaStorageOpt.invalidate();
         this.researchCacheOpt.invalidate();
     }
