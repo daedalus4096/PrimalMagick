51e3480671347f6da0d240b04bb9b8a6dc3c56e9
Michael Bunting <michael.a.bunting@gmail.com>
2025-01-30 21:59:50 -0800

    Fix carved bookshelf item handlers for Forge

--- forge/src/main/java/com/verdantartifice/primalmagick/common/tiles/misc/CarvedBookshelfTileEntityForge.java
+++ forge/src/main/java/com/verdantartifice/primalmagick/common/tiles/misc/CarvedBookshelfTileEntityForge.java
@@ -1,6 +1,6 @@
 package com.verdantartifice.primalmagick.common.tiles.misc;
 
-import com.verdantartifice.primalmagick.common.capabilities.IItemHandlerPM;
+import com.verdantartifice.primalmagick.common.tiles.IHasItemHandlerCapabilityForge;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.NonNullList;
@@ -8,35 +8,39 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.IItemHandler;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class CarvedBookshelfTileEntityForge extends CarvedBookshelfTileEntity {
-    protected final NonNullList<LazyOptional<IItemHandlerPM>> itemHandlerOpts;
+public class CarvedBookshelfTileEntityForge extends CarvedBookshelfTileEntity implements IHasItemHandlerCapabilityForge {
+    protected final NonNullList<LazyOptional<IItemHandler>> itemHandlerOpts;
 
     public CarvedBookshelfTileEntityForge(BlockPos pos, BlockState state) {
         super(pos, state);
-        this.itemHandlerOpts = NonNullList.withSize(this.getInventoryCount(), LazyOptional.empty());
-        for (int index = 0; index < this.itemHandlers.size(); index++) {
-            final int optIndex = index;
-            this.itemHandlerOpts.set(index, LazyOptional.of(() -> this.itemHandlers.get(optIndex)));
-        }
+        this.itemHandlerOpts = this.makeItemHandlerOpts(this.itemHandlers);
+    }
+
+    @Override
+    public NonNullList<LazyOptional<IItemHandler>> getItemHandlerOpts() {
+        return this.itemHandlerOpts;
     }
 
     @Override
     public void invalidateCaps() {
         super.invalidateCaps();
-        this.itemHandlerOpts.forEach(opt -> opt.invalidate());
+        this.itemHandlerOpts.forEach(LazyOptional::invalidate);
     }
 
     @Override
-    public <T> LazyOptional<T> getCapability(Capability<T> cap, Direction face) {
-        if (!this.remove && cap == ForgeCapabilities.ITEM_HANDLER) {
-            if (face != null && this.getInventoryIndexForFace(face).isPresent()) {
-                return this.itemHandlerOpts.get(this.getInventoryIndexForFace(face).get()).cast();
-            } else {
-                return LazyOptional.empty();
-            }
+    public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
+        if (this.remove) {
+            return super.getCapability(cap, side);
         } else {
-            return super.getCapability(cap, face);
+            if (cap == ForgeCapabilities.ITEM_HANDLER) {
+                return this.getItemHandlerCapability(side).cast();
+            } else {
+                return super.getCapability(cap, side);
+            }
         }
     }
 
