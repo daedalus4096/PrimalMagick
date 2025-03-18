c2ece1a5d9b1470fb200a657b49f7275511cacdc
Michael Bunting <michael.a.bunting@gmail.com>
2025-03-17 20:42:43 -0700

    Add optional name key field to research entries

--- common/src/main/java/com/verdantartifice/primalmagick/common/util/StreamCodecUtils.java
+++ common/src/main/java/com/verdantartifice/primalmagick/common/util/StreamCodecUtils.java
@@ -1,5 +1,6 @@
 package com.verdantartifice.primalmagick.common.util;
 
+import com.mojang.datafixers.util.Function10;
 import com.mojang.datafixers.util.Function7;
 import com.mojang.datafixers.util.Function8;
 import com.mojang.datafixers.util.Function9;
@@ -169,6 +170,61 @@ public class StreamCodecUtils {
                     pCodec7.encode(buf, pGetter7.apply(value));
                     pCodec8.encode(buf, pGetter8.apply(value));
                     pCodec9.encode(buf, pGetter9.apply(value));
+                }
+            };
+        }
+
+    public static <B, C, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> StreamCodec<B, C> composite(
+            final StreamCodec<? super B, T1> pCodec1,
+            final Function<C, T1> pGetter1,
+            final StreamCodec<? super B, T2> pCodec2,
+            final Function<C, T2> pGetter2,
+            final StreamCodec<? super B, T3> pCodec3,
+            final Function<C, T3> pGetter3,
+            final StreamCodec<? super B, T4> pCodec4,
+            final Function<C, T4> pGetter4,
+            final StreamCodec<? super B, T5> pCodec5,
+            final Function<C, T5> pGetter5,
+            final StreamCodec<? super B, T6> pCodec6,
+            final Function<C, T6> pGetter6,
+            final StreamCodec<? super B, T7> pCodec7,
+            final Function<C, T7> pGetter7,
+            final StreamCodec<? super B, T8> pCodec8,
+            final Function<C, T8> pGetter8,
+            final StreamCodec<? super B, T9> pCodec9,
+            final Function<C, T9> pGetter9,
+            final StreamCodec<? super B, T10> pCodec10,
+            final Function<C, T10> pGetter10,
+            final Function10<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, C> pFactory
+        ) {
+            return new StreamCodec<B, C>() {
+                @Override
+                public C decode(B buf) {
+                    T1 t1 = pCodec1.decode(buf);
+                    T2 t2 = pCodec2.decode(buf);
+                    T3 t3 = pCodec3.decode(buf);
+                    T4 t4 = pCodec4.decode(buf);
+                    T5 t5 = pCodec5.decode(buf);
+                    T6 t6 = pCodec6.decode(buf);
+                    T7 t7 = pCodec7.decode(buf);
+                    T8 t8 = pCodec8.decode(buf);
+                    T9 t9 = pCodec9.decode(buf);
+                    T10 t10 = pCodec10.decode(buf);
+                    return pFactory.apply(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10);
+                }
+
+                @Override
+                public void encode(B buf, C value) {
+                    pCodec1.encode(buf, pGetter1.apply(value));
+                    pCodec2.encode(buf, pGetter2.apply(value));
+                    pCodec3.encode(buf, pGetter3.apply(value));
+                    pCodec4.encode(buf, pGetter4.apply(value));
+                    pCodec5.encode(buf, pGetter5.apply(value));
+                    pCodec6.encode(buf, pGetter6.apply(value));
+                    pCodec7.encode(buf, pGetter7.apply(value));
+                    pCodec8.encode(buf, pGetter8.apply(value));
+                    pCodec9.encode(buf, pGetter9.apply(value));
+                    pCodec10.encode(buf, pGetter10.apply(value));
                 }
             };
         }
