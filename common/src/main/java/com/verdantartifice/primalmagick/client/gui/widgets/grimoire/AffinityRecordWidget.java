f2ee3b1a6d5c07c151afc7ae52ba7afd500e2dd2
Michael Bunting <michael.a.bunting@gmail.com>
2025-04-02 17:02:37 -0700

    Show full affinity data in tooltip

--- common/src/main/java/com/verdantartifice/primalmagick/client/gui/widgets/grimoire/AffinityRecordWidget.java
+++ common/src/main/java/com/verdantartifice/primalmagick/client/gui/widgets/grimoire/AffinityRecordWidget.java
@@ -1,15 +1,23 @@
 package com.verdantartifice.primalmagick.client.gui.widgets.grimoire;
 
+import com.google.common.collect.ImmutableList;
+import com.mojang.datafixers.util.Either;
 import com.verdantartifice.primalmagick.client.gui.GrimoireScreen;
+import com.verdantartifice.primalmagick.client.util.GuiUtils;
 import com.verdantartifice.primalmagick.common.affinities.AffinityIndexEntry;
+import com.verdantartifice.primalmagick.common.affinities.AffinityTooltipComponent;
 import com.verdantartifice.primalmagick.common.sources.Source;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.sounds.SoundManager;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.FormattedText;
+import net.minecraft.world.inventory.tooltip.TooltipComponent;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AffinityRecordWidget extends AbstractTopicButton {
     public static final int WIDGET_HEIGHT = 17;
@@ -17,27 +25,35 @@ public class AffinityRecordWidget extends AbstractTopicButton {
     protected final AffinityIndexEntry entry;
     protected final Source source;
     protected final Component amountText;
+    protected final List<Either<FormattedText, TooltipComponent>> elements;
 
     public AffinityRecordWidget(int x, int y, @NotNull AffinityIndexEntry entry, Source source, GrimoireScreen screen) {
         super(x, y, 123, WIDGET_HEIGHT, entry.stack().getHoverName(), screen, ItemIndexIcon.of(entry.stack().getItem(), true), button -> {});
         this.entry = entry;
         this.source = source;
         this.amountText = Component.literal(Integer.toString(this.entry.affinities().join().getAmount(this.source)));
-        // TODO Set tooltip data to be full affinity source list
+        this.elements = ImmutableList.of(
+                Either.left(Component.translatable("tooltip.primalmagick.affinities.label")),
+                Either.right(new AffinityTooltipComponent(this.entry.affinities().join())));
     }
 
     @Override
-    public void renderWidget(GuiGraphics guiGraphics, int p_renderButton_1_, int p_renderButton_2_, float p_renderButton_3_) {
-        super.renderWidget(guiGraphics, p_renderButton_1_, p_renderButton_2_, p_renderButton_3_);
+    public void renderWidget(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick) {
+        super.renderWidget(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
 
         // Draw the relevant affinity rating where the stack count would normally be
         Minecraft mc = Minecraft.getInstance();
         int width = mc.font.width(this.amountText);
-        guiGraphics.pose().pushPose();
-        guiGraphics.pose().translate(this.getX() + 16 - width / 2, this.getY() + 12, 200.0F);
-        guiGraphics.pose().scale(0.5F, 0.5F, 0.5F);
-        guiGraphics.drawString(mc.font, this.amountText, 0, 0, Color.WHITE.getRGB());
-        guiGraphics.pose().popPose();
+        pGuiGraphics.pose().pushPose();
+        pGuiGraphics.pose().translate(this.getX() + 16 - width / 2, this.getY() + 12, 200.0F);
+        pGuiGraphics.pose().scale(0.5F, 0.5F, 0.5F);
+        pGuiGraphics.drawString(mc.font, this.amountText, 0, 0, Color.WHITE.getRGB());
+        pGuiGraphics.pose().popPose();
+
+        // Draw the tooltip if applicable
+        if (this.isHoveredOrFocused()) {
+            GuiUtils.renderComponentTooltipFromElements(pGuiGraphics, this.elements, pMouseX, pMouseY);
+        }
     }
 
     @Override
--- forge/src/main/java/com/verdantartifice/primalmagick/platform/GuiGraphicsServiceForge.java
+++ forge/src/main/java/com/verdantartifice/primalmagick/platform/GuiGraphicsServiceForge.java
@@ -1,9 +1,11 @@
 package com.verdantartifice.primalmagick.platform;
 
+import com.mojang.datafixers.util.Either;
 import com.verdantartifice.primalmagick.platform.services.IGuiGraphicsService;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.FormattedText;
+import net.minecraft.world.inventory.tooltip.TooltipComponent;
 import net.minecraft.world.item.ItemStack;
 
 import java.util.List;
@@ -12,5 +14,10 @@ public class GuiGraphicsServiceForge implements IGuiGraphicsService {
     @Override
     public void renderComponentTooltip(GuiGraphics pGuiGraphics, Font pFont, List<? extends FormattedText> pTooltipLines, int pMouseX, int pMouseY, ItemStack stack) {
         pGuiGraphics.renderComponentTooltip(pFont, pTooltipLines, pMouseX, pMouseY, stack);
+    }
+
+    @Override
+    public void renderComponentTooltipFromElements(GuiGraphics pGuiGraphics, Font pFont, List<Either<FormattedText, TooltipComponent>> pElements, int pMouseX, int pMouseY, ItemStack stack) {
+        pGuiGraphics.renderComponentTooltipFromElements(pFont, pElements, pMouseX, pMouseY, stack);
     }
 }
