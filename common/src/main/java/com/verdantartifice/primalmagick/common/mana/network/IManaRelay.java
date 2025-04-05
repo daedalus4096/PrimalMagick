7a757103b00ebeac8964c8db3863be152cb7107f
Michael Bunting <michael.a.bunting@gmail.com>
2025-04-05 12:50:03 -0700

    Progress commit for route propagation

--- common/src/main/java/com/verdantartifice/primalmagick/common/mana/network/IManaRelay.java
+++ common/src/main/java/com/verdantartifice/primalmagick/common/mana/network/IManaRelay.java
@@ -1,7 +1,14 @@
 package com.verdantartifice.primalmagick.common.mana.network;
 
 import com.verdantartifice.primalmagick.common.sources.Source;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 
 /**
  * Interface identifying a relay in a mana network, a device which can both transmit and receive mana but cannot
@@ -33,7 +40,51 @@ public interface IManaRelay extends IManaSupplier, IManaConsumer {
     boolean canRelay(Source source);
 
     @Override
-    default void onPlaced(Level leve) {
-        // TODO Stub
+    default void onPlaced(Level level) {
+        int range = this.getNetworkRange();
+        int rangeSqr = range * range;
+
+        // Search for mana network nodes in range of this one
+        List<IManaNetworkNode> nodes = BlockPos.betweenClosedStream(new AABB(this.getBlockPos()).inflate(range))
+                .filter(pos -> pos.distSqr(this.getBlockPos()) <= rangeSqr)
+                .map(pos -> level.getBlockEntity(pos) instanceof IManaNetworkNode node ? node : null)
+                .filter(Objects::nonNull)
+                .toList();
+
+        // Create direct routes to this relay for origin suppliers
+        nodes.stream().map(node -> node instanceof IManaSupplier supplier ? supplier : null)
+                .filter(supplier -> supplier != null && supplier.isOrigin())
+                .map(supplier -> new Route(supplier, this))
+                .filter(Route::isValid)
+                .forEach(this.getRouteTable()::addRoute);
+
+        // Create direct routes to this relay for terminus consumers
+        nodes.stream().map(node -> node instanceof IManaConsumer consumer ? consumer : null)
+                .filter(consumer -> consumer != null && consumer.isTerminus())
+                .map(consumer -> new Route(this, consumer))
+                .filter(Route::isValid)
+                .forEach(this.getRouteTable()::addRoute);
+
+        // For nodes that are actually relays, create bidirectional routes connecting this and that
+        List<IManaRelay> relays = nodes.stream().map(node -> node instanceof IManaRelay relay ? relay : null)
+                .filter(Objects::nonNull)
+                .toList();
+        relays.stream().flatMap(relay -> relay.getRouteTable().getRoutesForOrigin(relay).stream())
+                .map(route -> route.pushOrigin(this))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .filter(Route::isValid)
+                .forEach(this.getRouteTable()::addRoute);
+        relays.stream().flatMap(relay -> relay.getRouteTable().getRoutesForTerminus(relay).stream())
+                .map(route -> route.pushTerminus(this))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .filter(Route::isValid)
+                .forEach(this.getRouteTable()::addRoute);
+
+        // TODO Connect existing routes that can use this relay as a bridge
+
+        // Update connected nodes on the newly created routes
+        this.getRouteTable().propagateRoutes(Set.of(this));
     }
 }
