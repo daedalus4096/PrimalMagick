9eb6c84edb1bcab46b3b903e6bd5c07efa186079
Michael Bunting <michael.a.bunting@gmail.com>
2025-05-21 17:25:18 -0700

    Update spelltome model textures

--- common/src/main/java/com/verdantartifice/primalmagick/client/renderers/itemstack/SpelltomeISTER.java
+++ common/src/main/java/com/verdantartifice/primalmagick/client/renderers/itemstack/SpelltomeISTER.java
@@ -3,6 +3,8 @@ package com.verdantartifice.primalmagick.client.renderers.itemstack;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.verdantartifice.primalmagick.common.items.tools.SpelltomeItem;
+import com.verdantartifice.primalmagick.common.misc.DeviceTier;
+import com.verdantartifice.primalmagick.common.util.ResourceUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.BookModel;
 import net.minecraft.client.model.geom.ModelLayers;
@@ -15,7 +17,10 @@ import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 
 public class SpelltomeISTER extends BlockEntityWithoutLevelRenderer {
-    protected static final ResourceLocation TEXTURE = ResourceLocation.withDefaultNamespace("textures/entity/enchanting_table_book.png");
+    protected static final ResourceLocation TEXTURE_APPRENTICE = ResourceUtils.loc("textures/entity/spelltome/apprentice.png");
+    protected static final ResourceLocation TEXTURE_ADEPT = ResourceUtils.loc("textures/entity/spelltome/adept.png");
+    protected static final ResourceLocation TEXTURE_WIZARD = ResourceUtils.loc("textures/entity/spelltome/wizard.png");
+    protected static final ResourceLocation TEXTURE_ARCHMAGE = ResourceUtils.loc("textures/entity/spelltome/archmage.png");
 
     protected BookModel model;
 
@@ -31,18 +36,23 @@ public class SpelltomeISTER extends BlockEntityWithoutLevelRenderer {
 
     @Override
     public void renderByItem(ItemStack pStack, ItemDisplayContext pDisplayContext, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight, int pPackedOverlay) {
-        if (pStack.getItem() instanceof SpelltomeItem) {
+        if (pStack.getItem() instanceof SpelltomeItem spelltomeItem) {
             float open = (pDisplayContext == ItemDisplayContext.FIRST_PERSON_LEFT_HAND || pDisplayContext == ItemDisplayContext.FIRST_PERSON_RIGHT_HAND) ? 1.0F : 0.0F;
             pPoseStack.pushPose();
             pPoseStack.scale(1.0F, -1.0F, -1.0F);
             this.model.setupAnim(0F, 0.1F, 0.9F, open);
-            VertexConsumer vertexConsumer = ItemRenderer.getFoilBufferDirect(pBuffer, this.model.renderType(this.getTextureLocation()), false, pStack.hasFoil());
+            VertexConsumer vertexConsumer = ItemRenderer.getFoilBufferDirect(pBuffer, this.model.renderType(getTextureLocation(spelltomeItem.getDeviceTier())), false, pStack.hasFoil());
             this.model.renderToBuffer(pPoseStack, vertexConsumer, pPackedLight, pPackedOverlay, -1);
             pPoseStack.popPose();
         }
     }
 
-    private ResourceLocation getTextureLocation() {
-        return TEXTURE;
+    private static ResourceLocation getTextureLocation(DeviceTier tier) {
+        return switch (tier) {
+            case BASIC -> TEXTURE_APPRENTICE;
+            case ENCHANTED -> TEXTURE_ADEPT;
+            case FORBIDDEN -> TEXTURE_WIZARD;
+            case HEAVENLY, CREATIVE -> TEXTURE_ARCHMAGE;
+        };
     }
 }
