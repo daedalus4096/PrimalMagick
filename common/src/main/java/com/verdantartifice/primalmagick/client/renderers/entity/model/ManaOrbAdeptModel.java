ce5461efca5d0f17e705209a23af22448ea44c7a
Michael Bunting <michael.a.bunting@gmail.com>
2025-05-22 22:29:46 -0700

    Initial models and textures for mana orbs

---
+++ common/src/main/java/com/verdantartifice/primalmagick/client/renderers/entity/model/ManaOrbAdeptModel.java
@@ -0,0 +1,47 @@
+package com.verdantartifice.primalmagick.client.renderers.entity.model;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.model.Model;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.CubeDeformation;
+import net.minecraft.client.model.geom.builders.CubeListBuilder;
+import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.client.model.geom.builders.MeshDefinition;
+import net.minecraft.client.model.geom.builders.PartDefinition;
+import net.minecraft.client.renderer.RenderType;
+
+/**
+ * Definition of a 3D model of the core of an adept-grade mana orb.
+ *
+ * @author Daedalus4096
+ */
+public class ManaOrbAdeptModel extends Model {
+	// Made with Blockbench 4.12.4
+	// Exported for Minecraft version 1.17 or later with Mojang mappings
+
+	private final ModelPart bb_main;
+
+	public ManaOrbAdeptModel(ModelPart root) {
+		super(RenderType::entitySolid);
+		this.bb_main = root.getChild("bb_main");
+	}
+
+	public static LayerDefinition createBodyLayer() {
+		MeshDefinition meshdefinition = new MeshDefinition();
+		PartDefinition partdefinition = meshdefinition.getRoot();
+
+		PartDefinition bb_main = partdefinition.addOrReplaceChild("bb_main", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, -2.0F, -1.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
+		.texOffs(0, 4).addBox(-1.0F, -4.0F, -1.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
+		.texOffs(4, 4).addBox(0.0F, -3.0F, -1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
+		.texOffs(4, 6).addBox(-1.0F, -3.0F, 0.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+
+		return LayerDefinition.create(meshdefinition, 16, 16);
+	}
+
+	@Override
+	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, int color) {
+		bb_main.render(poseStack, vertexConsumer, packedLight, packedOverlay, color);
+	}
+}
