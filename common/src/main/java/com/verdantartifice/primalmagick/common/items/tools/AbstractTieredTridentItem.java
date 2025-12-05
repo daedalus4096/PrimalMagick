37b769ed15f79a22628ad0c9cad7432ff371ed8e
Michael Bunting <michael.a.bunting@gmail.com>
2025-12-04 23:18:34 -0800

    Fix tiered trident errors

--- common/src/main/java/com/verdantartifice/primalmagick/common/items/tools/AbstractTieredTridentItem.java
+++ common/src/main/java/com/verdantartifice/primalmagick/common/items/tools/AbstractTieredTridentItem.java
@@ -18,7 +18,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.ToolMaterial;
 import net.minecraft.world.item.TridentItem;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
@@ -27,6 +26,7 @@ import net.minecraft.world.item.enchantment.EnchantmentEffectComponents;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
@@ -50,13 +50,13 @@ public abstract class AbstractTieredTridentItem extends TridentItem implements I
     
     public static ItemAttributeModifiers createAttributes(ToolMaterial material) {
         return ItemAttributeModifiers.builder()
-            .add(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_ID, 7.0D + tier.getAttackDamageBonus(), AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND)
+            .add(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_ID, 7.0D + material.attackDamageBonus(), AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND)
             .add(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_ID, -2.9D, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND)
             .build();
     }
 
     public static Tool createToolProperties() {
-        return new Tool(List.of(), 1.0F, 2);
+        return new Tool(List.of(), 1.0F, 2, false);
     }
 
     public ToolMaterial getMaterial() {
@@ -66,28 +66,26 @@ public abstract class AbstractTieredTridentItem extends TridentItem implements I
     protected abstract AbstractTridentEntity getThrownEntity(Level world, LivingEntity thrower, ItemStack stack);
 
     @Override
-    public void releaseUsing(ItemStack stack, Level worldIn, LivingEntity entityLiving, int timeLeft) {
+    public boolean releaseUsing(@NotNull ItemStack stack, @NotNull Level worldIn, @NotNull LivingEntity entityLiving, int timeLeft) {
         if (entityLiving instanceof Player player) {
             int duration = this.getUseDuration(stack, entityLiving) - timeLeft;
             if (duration >= 10) {
                 float riptide = EnchantmentHelper.getTridentSpinAttackStrength(stack, player);
                 if (riptide <= 0F || player.isInWaterOrRain()) {
-                    if (!isTooDamagedToUse(stack)) {
+                    if (!stack.nextDamageWillBreak()) {
                         Holder<SoundEvent> soundHolder = EnchantmentHelper.pickHighestLevel(stack, EnchantmentEffectComponents.TRIDENT_SOUND).orElse(SoundEvents.TRIDENT_THROW);
                         if (!worldIn.isClientSide()) {
-                            stack.hurtAndBreak(1, player, LivingEntity.getSlotForHand(entityLiving.getUsedItemHand()));
+                            stack.hurtAndBreak(1, player, entityLiving.getUsedItemHand());
                             if (riptide == 0) {
-                                AbstractTridentEntity trident = this.getThrownEntity(worldIn, player, stack);
-                                trident.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 2.5F + (float)riptide * 0.5F, 1.0F);
+                                AbstractTridentEntity trident = this.getThrownEntity(worldIn, player, stack);   // This is the difference from the base class version
+                                trident.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 2.5F + riptide * 0.5F, 1.0F);
                                 if (player.hasInfiniteMaterials()) {
                                     trident.pickup = AbstractArrow.Pickup.CREATIVE_ONLY;
                                 }
                                 
                                 worldIn.addFreshEntity(trident);
-                                worldIn.playSound((Player)null, trident, soundHolder.value(), SoundSource.PLAYERS, 1.0F, 1.0F);
-                                if (!player.hasInfiniteMaterials()) {
-                                    player.getInventory().removeItem(stack);
-                                }
+                                worldIn.playSound(null, trident, soundHolder.value(), SoundSource.PLAYERS, 1.0F, 1.0F);
+                                return true;
                             }
                         }
                         
@@ -99,25 +97,23 @@ public abstract class AbstractTieredTridentItem extends TridentItem implements I
                             float f2 = -Mth.sin(f * ((float)Math.PI / 180F));
                             float f3 = Mth.cos(f7 * ((float)Math.PI / 180F)) * Mth.cos(f * ((float)Math.PI / 180F));
                             float f4 = Mth.sqrt(f1 * f1 + f2 * f2 + f3 * f3);
-                            float f5 = 3.0F * ((1.0F + (float)riptide) / 4.0F);
+                            float f5 = 3.0F * ((1.0F + riptide) / 4.0F);
                             f1 = f1 * (f5 / f4);
                             f2 = f2 * (f5 / f4);
                             f3 = f3 * (f5 / f4);
-                            player.push((double)f1, (double)f2, (double)f3);
-                            player.startAutoSpinAttack(20, 7.0F + this.tier.getAttackDamageBonus(), stack);
+                            player.push(f1, f2, f3);
+                            player.startAutoSpinAttack(20, 7.0F + this.material.attackDamageBonus(), stack);
                             if (player.onGround()) {
                                 player.move(MoverType.SELF, new Vec3(0.0D, 1.1999999D, 0.0D));
                             }
                             
-                            worldIn.playSound((Player)null, player, soundHolder.value(), SoundSource.PLAYERS, 1.0F, 1.0F);
+                            worldIn.playSound(null, player, soundHolder.value(), SoundSource.PLAYERS, 1.0F, 1.0F);
+                            return true;
                         }
                     }
                 }
             }
         }
-    }
-    
-    private static boolean isTooDamagedToUse(ItemStack pStack) {
-        return pStack.getDamageValue() >= pStack.getMaxDamage() - 1;
+        return false;
     }
 }
