2dc518ce71fcd4ebfc750f9da10a4480140baa79
Michael Bunting <michael.a.bunting@gmail.com>
2025-12-22 22:57:06 -0800

    Fix linguistics grid resource loader errors

--- neoforge/src/main/java/com/verdantartifice/primalmagick/common/events/ResourceEventListeners.java
+++ neoforge/src/main/java/com/verdantartifice/primalmagick/common/events/ResourceEventListeners.java
@@ -3,6 +3,7 @@ package com.verdantartifice.primalmagick.common.events;
 import com.verdantartifice.primalmagick.Constants;
 import com.verdantartifice.primalmagick.common.affinities.AffinityManager;
 import com.verdantartifice.primalmagick.common.books.grids.GridDefinitionLoader;
+import com.verdantartifice.primalmagick.common.util.ResourceUtils;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.AddServerReloadListenersEvent;
@@ -12,6 +13,6 @@ public class ResourceEventListeners {
     @SubscribeEvent
     public static void onResourceReload(AddServerReloadListenersEvent event) {
         event.addListener(AffinityManager.getOrCreateInstance());
-        event.addListener(GridDefinitionLoader.getOrCreateInstance());
+        event.addListener(ResourceUtils.loc("linguistics_grids"), GridDefinitionLoader.getOrCreateInstance());
     }
 }
