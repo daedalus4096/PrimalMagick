62e1ff5380516f07b943d8ad0f677b7d090b523f
Michael Bunting <michael.a.bunting@gmail.com>
2025-12-25 22:15:20 -0800

    Fix damage type tag provider errors

--- forge/src/main/java/com/verdantartifice/primalmagick/datagen/tags/DamageTypeTagsProviderPMForge.java
+++ forge/src/main/java/com/verdantartifice/primalmagick/datagen/tags/DamageTypeTagsProviderPMForge.java
@@ -6,28 +6,29 @@ import com.verdantartifice.primalmagick.common.tags.DamageTypeTagsPM;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.tags.TagsProvider;
+import net.minecraft.data.tags.KeyTagProvider;
 import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.CompletableFuture;
 
 /**
- * Data provider for all of the mod's damage type tags, both original tags and modifications to vanilla tags.
+ * Data provider for all the mod's damage type tags, both original tags and modifications to vanilla tags.
  * 
  * @author Daedalus4096
  */
-public class DamageTypeTagsProviderPMForge extends TagsProvider<DamageType> {
+public class DamageTypeTagsProviderPMForge extends KeyTagProvider<DamageType> {
     public DamageTypeTagsProviderPMForge(PackOutput output, CompletableFuture<HolderLookup.Provider> future, ExistingFileHelper helper) {
         super(output, Registries.DAMAGE_TYPE, future, Constants.MOD_ID, helper);
     }
 
     @Override
-    protected void addTags(HolderLookup.Provider provider) {
-        this.tag(DamageTypeTags.BYPASSES_ARMOR).add(DamageTypesPM.BLEEDING, DamageTypesPM.SORCERY_BLOOD);
-        this.tag(DamageTypeTags.IS_FIRE).add(DamageTypesPM.HELLISH_CHAIN, DamageTypesPM.SORCERY_INFERNAL);
+    protected void addTags(@NotNull HolderLookup.Provider provider) {
+        this.tag(DamageTypeTags.BYPASSES_ARMOR).add(DamageTypesPM.BLEEDING).add(DamageTypesPM.SORCERY_BLOOD);
+        this.tag(DamageTypeTags.IS_FIRE).add(DamageTypesPM.HELLISH_CHAIN).add(DamageTypesPM.SORCERY_INFERNAL);
 
         this.tag(DamageTypeTagsPM.IS_MAGIC).add(DamageTypes.MAGIC).addTag(DamageTypeTagsPM.IS_SORCERY);
         this.tag(DamageTypeTagsPM.IS_SORCERY).addTag(DamageTypeTagsPM.IS_SORCERY_EARTH).addTag(DamageTypeTagsPM.IS_SORCERY_SEA).addTag(DamageTypeTagsPM.IS_SORCERY_SKY).addTag(DamageTypeTagsPM.IS_SORCERY_SUN).addTag(DamageTypeTagsPM.IS_SORCERY_MOON).addTag(DamageTypeTagsPM.IS_SORCERY_BLOOD).addTag(DamageTypeTagsPM.IS_SORCERY_INFERNAL).addTag(DamageTypeTagsPM.IS_SORCERY_VOID).addTag(DamageTypeTagsPM.IS_SORCERY_HALLOWED);
@@ -41,9 +42,9 @@ public class DamageTypeTagsProviderPMForge extends TagsProvider<DamageType> {
         this.tag(DamageTypeTagsPM.IS_SORCERY_VOID).add(DamageTypesPM.SORCERY_VOID);
         this.tag(DamageTypeTagsPM.IS_SORCERY_HALLOWED).add(DamageTypesPM.SORCERY_HALLOWED);
 
-        this.tag(DamageTypeTagsPM.IGNITES_PIXIE_HOUSES).add(DamageTypes.IN_FIRE, DamageTypes.CAMPFIRE);
+        this.tag(DamageTypeTagsPM.IGNITES_PIXIE_HOUSES).add(DamageTypes.IN_FIRE).add(DamageTypes.CAMPFIRE);
         this.tag(DamageTypeTagsPM.BURNS_PIXIE_HOUSES).add(DamageTypes.ON_FIRE);
-        this.tag(DamageTypeTagsPM.ALWAYS_KILLS_PIXIE_HOUSES).add(DamageTypes.FIREBALL, DamageTypes.WITHER_SKULL, DamageTypes.WIND_CHARGE);
-        this.tag(DamageTypeTagsPM.CAN_BREAK_PIXIE_HOUSES).add(DamageTypes.PLAYER_ATTACK, DamageTypes.PLAYER_EXPLOSION);
+        this.tag(DamageTypeTagsPM.ALWAYS_KILLS_PIXIE_HOUSES).add(DamageTypes.FIREBALL).add(DamageTypes.WITHER_SKULL).add(DamageTypes.WIND_CHARGE);
+        this.tag(DamageTypeTagsPM.CAN_BREAK_PIXIE_HOUSES).add(DamageTypes.PLAYER_ATTACK).add(DamageTypes.PLAYER_EXPLOSION);
     }
 }
