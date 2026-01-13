cfdea435cb357cd91ff1a7b721020342b431f081
Michael Bunting <michael.a.bunting@gmail.com>
2026-01-12 16:56:03 -0800

    Fix import errors

--- common/src/main/java/com/verdantartifice/primalmagick/common/entities/projectiles/ManaArrowEntity.java
+++ common/src/main/java/com/verdantartifice/primalmagick/common/entities/projectiles/ManaArrowEntity.java
@@ -18,7 +18,7 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.projectile.arrow.AbstractArrow;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
