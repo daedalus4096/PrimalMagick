59e6a4855a448fdba24a259224e61f4acd6ef139
Michael Bunting <michael.a.bunting@gmail.com>
2026-01-14 23:08:13 -0800

    Fix window errors

--- forge/src/main/java/com/verdantartifice/primalmagick/platform/InputServiceForge.java
+++ forge/src/main/java/com/verdantartifice/primalmagick/platform/InputServiceForge.java
@@ -19,8 +19,8 @@ public class InputServiceForge implements IInputService {
         } else {
             Minecraft mc = Minecraft.getInstance();
             boolean isDown = switch (keybind.getKey().getType()) {
-                case KEYSYM -> InputConstants.isKeyDown(mc.getWindow().getWindow(), keybind.getKey().getValue());
-                case MOUSE -> GLFW.glfwGetMouseButton(mc.getWindow().getWindow(), keybind.getKey().getValue()) == GLFW.GLFW_PRESS;
+                case KEYSYM -> InputConstants.isKeyDown(mc.getWindow(), keybind.getKey().getValue());
+                case MOUSE -> GLFW.glfwGetMouseButton(mc.getWindow().handle(), keybind.getKey().getValue()) == GLFW.GLFW_PRESS;
                 default -> false;
             };
             return isDown && keybind.getKeyConflictContext().isActive() && keybind.getKeyModifier().isActive(keybind.getKeyConflictContext());
