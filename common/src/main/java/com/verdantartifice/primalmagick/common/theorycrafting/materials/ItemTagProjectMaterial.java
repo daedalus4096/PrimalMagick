cd020406a04e01bd358607e14c1d67d41a98d431
Michael Bunting <michael.a.bunting@gmail.com>
2026-01-14 23:46:13 -0800

    Fix identifier errors

--- common/src/main/java/com/verdantartifice/primalmagick/common/theorycrafting/materials/ItemTagProjectMaterial.java
+++ common/src/main/java/com/verdantartifice/primalmagick/common/theorycrafting/materials/ItemTagProjectMaterial.java
@@ -17,8 +17,8 @@ import net.minecraft.util.ExtraCodecs;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import org.jetbrains.annotations.NotNull;
 
-import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -82,7 +82,7 @@ public class ItemTagProjectMaterial extends AbstractProjectMaterial<ItemTagProje
             return true;
         } else if (!this.consumed && this.quantity == 1 && surroundings != null) {
             // Only allow satisfaction from surroundings if not consuming the material and only one item is required
-            TagKey<Block> blockTagKey = TagKey.create(Registries.BLOCK, this.tag.identifier());
+            TagKey<Block> blockTagKey = TagKey.create(Registries.BLOCK, this.tag.location());
             List<Block> tagContents = new ArrayList<>();
             Services.BLOCKS_REGISTRY.getTag(blockTagKey).ifPresent(t -> t.forEach(tagContents::add));
             Set<Block> intersection = new HashSet<>(surroundings);
@@ -102,7 +102,7 @@ public class ItemTagProjectMaterial extends AbstractProjectMaterial<ItemTagProje
         }
     }
     
-    @Nullable
+    @NotNull
     public TagKey<Item> getTag() {
         return this.tag;
     }
@@ -147,7 +147,7 @@ public class ItemTagProjectMaterial extends AbstractProjectMaterial<ItemTagProje
         return true;
     }
 
-    public static Builder builder(TagKey<Item> tag) {
+    public static Builder builder(@NotNull TagKey<Item> tag) {
         return new Builder(tag);
     }
     
