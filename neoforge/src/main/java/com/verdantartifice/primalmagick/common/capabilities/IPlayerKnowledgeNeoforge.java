d69c060381474cef87f1efa1591997f4346a7e07
Michael Bunting <michael.a.bunting@gmail.com>
2026-01-14 23:23:49 -0800

    Remove INBTSerializable references

--- neoforge/src/main/java/com/verdantartifice/primalmagick/common/capabilities/IPlayerKnowledgeNeoforge.java
+++ neoforge/src/main/java/com/verdantartifice/primalmagick/common/capabilities/IPlayerKnowledgeNeoforge.java
@@ -1,7 +1,4 @@
 package com.verdantartifice.primalmagick.common.capabilities;
 
-import net.minecraft.nbt.Tag;
-import net.neoforged.neoforge.common.util.INBTSerializable;
-
-public interface IPlayerKnowledgeNeoforge extends IPlayerKnowledge, INBTSerializable<Tag> {
+public interface IPlayerKnowledgeNeoforge extends IPlayerKnowledge {
 }
--- neoforge/src/main/java/com/verdantartifice/primalmagick/common/capabilities/IPlayerStatsNeoforge.java
+++ neoforge/src/main/java/com/verdantartifice/primalmagick/common/capabilities/IPlayerStatsNeoforge.java
@@ -1,7 +1,4 @@
 package com.verdantartifice.primalmagick.common.capabilities;
 
-import net.minecraft.nbt.CompoundTag;
-import net.neoforged.neoforge.common.util.INBTSerializable;
-
-public interface IPlayerStatsNeoforge extends IPlayerStats, INBTSerializable<CompoundTag> {
+public interface IPlayerStatsNeoforge extends IPlayerStats {
 }
