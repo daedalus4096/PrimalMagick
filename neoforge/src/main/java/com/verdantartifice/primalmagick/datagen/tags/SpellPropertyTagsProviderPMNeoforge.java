12bebf16baa6f98a2bc0a9228fee2183d8e27dee
Michael Bunting <michael.a.bunting@gmail.com>
2026-01-18 14:19:42 -0800

    Fix spell property tag provider errors

--- neoforge/src/main/java/com/verdantartifice/primalmagick/datagen/tags/SpellPropertyTagsProviderPMNeoforge.java
+++ neoforge/src/main/java/com/verdantartifice/primalmagick/datagen/tags/SpellPropertyTagsProviderPMNeoforge.java
@@ -6,25 +6,27 @@ import com.verdantartifice.primalmagick.common.spells.SpellPropertiesPM;
 import com.verdantartifice.primalmagick.common.spells.SpellProperty;
 import com.verdantartifice.primalmagick.common.tags.SpellPropertyTagsPM;
 import com.verdantartifice.primalmagick.platform.Services;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.core.HolderLookup.Provider;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.IntrinsicHolderTagsProvider;
-import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.CompletableFuture;
 
 public class SpellPropertyTagsProviderPMNeoforge extends IntrinsicHolderTagsProvider<SpellProperty> {
-    public SpellPropertyTagsProviderPMNeoforge(PackOutput output, CompletableFuture<Provider> future, ExistingFileHelper helper) {
-        super(output, RegistryKeysPM.SPELL_PROPERTIES, future, p -> Services.SPELL_PROPERTIES_REGISTRY.getResourceKey(p).orElseThrow(), Constants.MOD_ID, helper);
+    public SpellPropertyTagsProviderPMNeoforge(PackOutput output, CompletableFuture<Provider> future) {
+        super(output, RegistryKeysPM.SPELL_PROPERTIES, future, p -> Services.SPELL_PROPERTIES_REGISTRY.getResourceKey(p).orElseThrow(), Constants.MOD_ID);
     }
 
     @Override
+    @NotNull
     public String getName() {
         return "Primal Magick Spell Property Tags";
     }
 
     @Override
-    protected void addTags(Provider pProvider) {
+    protected void addTags(@NotNull HolderLookup.Provider pProvider) {
         this.tag(SpellPropertyTagsPM.AMPLIFIABLE).add(SpellPropertiesPM.POWER.get(), SpellPropertiesPM.DURATION.get(), SpellPropertiesPM.BURST_POWER.get(), SpellPropertiesPM.NON_ZERO_DURATION.get());
     }
 }
