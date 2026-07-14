cf1fb7c63b5d6a82a1157970e4c24c5e18a26916
Michael Bunting <michael.a.bunting@gmail.com>
2026-07-13 17:11:08 -0700

    Fix mana arrow entity renderer errors

--- common/src/main/java/com/verdantartifice/primalmagick/client/renderers/entity/ManaArrowRenderer.java
+++ common/src/main/java/com/verdantartifice/primalmagick/client/renderers/entity/ManaArrowRenderer.java
@@ -4,14 +4,16 @@ import com.verdantartifice.primalmagick.common.entities.projectiles.ManaArrowEnt
 import com.verdantartifice.primalmagick.common.util.ResourceUtils;
 import net.minecraft.client.renderer.entity.ArrowRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.state.ArrowRenderState;
 import net.minecraft.resources.Identifier;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Renderer for a mana-tinged arrow entity.
  * 
  * @author Daedalus4096
  */
-public class ManaArrowRenderer extends ArrowRenderer<ManaArrowEntity> {
+public class ManaArrowRenderer extends ArrowRenderer<ManaArrowEntity, ArrowRenderState> {
     public static final Identifier MANA_ARROW_LOCATION = ResourceUtils.loc("textures/entity/mana_arrow.png");
 
     public ManaArrowRenderer(EntityRendererProvider.Context context) {
@@ -19,7 +21,14 @@ public class ManaArrowRenderer extends ArrowRenderer<ManaArrowEntity> {
      }
 
     @Override
-    public Identifier getTextureLocation(ManaArrowEntity p_114482_) {
+    @NotNull
+    public ArrowRenderState createRenderState() {
+        return new ArrowRenderState();
+    }
+
+    @Override
+    @NotNull
+    public Identifier getTextureLocation(@NotNull ArrowRenderState renderState) {
         return MANA_ARROW_LOCATION;
     }
 }
