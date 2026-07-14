826a6740b69ede1b010155b76820f83361372547
Michael Bunting <michael.a.bunting@gmail.com>
2026-07-13 18:23:44 -0700

    Fix infernal flame particle errors

--- common/src/main/java/com/verdantartifice/primalmagick/client/fx/particles/InfernalFlameParticle.java
+++ common/src/main/java/com/verdantartifice/primalmagick/client/fx/particles/InfernalFlameParticle.java
@@ -3,11 +3,13 @@ package com.verdantartifice.primalmagick.client.fx.particles;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleProvider;
-import net.minecraft.client.particle.ParticleRenderType;
 import net.minecraft.client.particle.RisingParticle;
+import net.minecraft.client.particle.SingleQuadParticle;
 import net.minecraft.client.particle.SpriteSet;
 import net.minecraft.core.particles.SimpleParticleType;
-import net.minecraft.util.Mth;
+import net.minecraft.util.LightCoordsUtil;
+import net.minecraft.util.RandomSource;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Particle type shown when calcinating items in an essence furnace.
@@ -15,13 +17,13 @@ import net.minecraft.util.Mth;
  * @author Daedalus4096
  */
 public class InfernalFlameParticle extends RisingParticle {
-    public InfernalFlameParticle(ClientLevel world, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
-        super(world, x, y, z, xSpeed, ySpeed, zSpeed);
-    }
+    private final SingleQuadParticle.Layer layer;
+    private final SpriteSet spriteSet;
 
-    @Override
-    public ParticleRenderType getRenderType() {
-        return ParticleRenderType.PARTICLE_SHEET_OPAQUE;
+    public InfernalFlameParticle(ClientLevel world, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed, SpriteSet spriteSet) {
+        super(world, x, y, z, xSpeed, ySpeed, zSpeed, spriteSet.first());
+        this.spriteSet = spriteSet;
+        this.layer = SingleQuadParticle.Layer.bySprite(this.sprite);
     }
 
     @Override
@@ -37,32 +39,32 @@ public class InfernalFlameParticle extends RisingParticle {
     }
 
     @Override
-    protected int getLightColor(float partialTicks) {
-        float f = ((float)this.age + partialTicks) / (float)this.lifetime;
-        f = Mth.clamp(f, 0.0F, 1.0F);
-        int i = super.getLightColor(partialTicks);
-        int j = i & 255;
-        int k = i >> 16 & 255;
-        j += (int)(f * 15.0F * 16.0F);
-        if (j > 240) {
-            j = 240;
-        }
-
-        return j | k << 16;
+    @NotNull
+    protected Layer getLayer() {
+        return this.layer;
     }
 
-    public static class Factory implements ParticleProvider<SimpleParticleType> {
+    @Override
+    public void tick() {
+        super.tick();
+        this.setSpriteFromAge(this.spriteSet);
+    }
+
+    @Override
+    public int getLightCoords(float partialTicks) {
+        return LightCoordsUtil.addSmoothBlockEmission(super.getLightCoords(partialTicks), ((float)this.age + partialTicks) / (float)this.lifetime);
+    }
+
+    public static class Provider implements ParticleProvider<SimpleParticleType> {
         private final SpriteSet sprite;
 
-        public Factory(SpriteSet sprite) {
+        public Provider(SpriteSet sprite) {
             this.sprite = sprite;
         }
 
         @Override
-        public Particle createParticle(SimpleParticleType typeIn, ClientLevel worldIn, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
-            InfernalFlameParticle ifp = new InfernalFlameParticle(worldIn, x, y, z, xSpeed, ySpeed, zSpeed);
-            ifp.pickSprite(this.sprite);
-            return ifp;
+        public Particle createParticle(@NotNull SimpleParticleType typeIn, @NotNull ClientLevel worldIn, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed, @NotNull RandomSource randomSource) {
+            return new InfernalFlameParticle(worldIn, x, y, z, xSpeed, ySpeed, zSpeed, this.sprite);
         }
     }
 }
--- neoforge/src/main/java/com/verdantartifice/primalmagick/client/events/ClientRegistrationEventListeners.java
+++ neoforge/src/main/java/com/verdantartifice/primalmagick/client/events/ClientRegistrationEventListeners.java
@@ -52,7 +52,7 @@ public class ClientRegistrationEventListeners {
         event.registerSpriteSet(ParticleTypesPM.SPELLCRAFTING_RUNE_V.get(), SpellcraftingRuneParticle.Factory::new);
         event.registerSpriteSet(ParticleTypesPM.SPELLCRAFTING_RUNE_T.get(), SpellcraftingRuneParticle.Factory::new);
         event.registerSpriteSet(ParticleTypesPM.SPELLCRAFTING_RUNE_D.get(), SpellcraftingRuneParticle.Factory::new);
-        event.registerSpriteSet(ParticleTypesPM.INFERNAL_FLAME.get(), InfernalFlameParticle.Factory::new);
+        event.registerSpriteSet(ParticleTypesPM.INFERNAL_FLAME.get(), InfernalFlameParticle.Provider::new);
         event.registerSpriteSet(ParticleTypesPM.AIR_CURRENT.get(), AirCurrentParticle.Provider::new);
         event.registerSpriteSet(ParticleTypesPM.VOID_SMOKE.get(), AirCurrentParticle.Provider::new);
         event.registerSpriteSet(ParticleTypesPM.LINGUISTICS.get(), FlyTowardsPositionParticle.EnchantProvider::new);
