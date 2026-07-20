c55e0824ea6ba2d48ee574b2b43b0ba0b8ca7d17
Michael Bunting <michael.a.bunting@gmail.com>
2026-07-19 19:08:16 -0700

    Refactor concocter screen

--- common/src/main/java/com/verdantartifice/primalmagick/client/gui/ConcocterScreen.java
+++ common/src/main/java/com/verdantartifice/primalmagick/client/gui/ConcocterScreen.java
@@ -1,23 +1,17 @@
 package com.verdantartifice.primalmagick.client.gui;
 
-import com.verdantartifice.primalmagick.client.gui.recipe_book.ArcaneRecipeBookComponent;
-import com.verdantartifice.primalmagick.client.gui.recipe_book.ArcaneRecipeUpdateListener;
+import com.verdantartifice.primalmagick.client.gui.recipe_book.ConcocterRecipeBookComponent;
 import com.verdantartifice.primalmagick.client.gui.widgets.ManaGaugeWidget;
 import com.verdantartifice.primalmagick.common.menus.ConcocterMenu;
 import com.verdantartifice.primalmagick.common.sources.Sources;
 import com.verdantartifice.primalmagick.common.util.ResourceUtils;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
-import net.minecraft.client.gui.components.ImageButton;
-import net.minecraft.client.gui.screens.recipebook.RecipeBookComponent;
-import net.minecraft.client.input.MouseButtonEvent;
+import net.minecraft.client.gui.navigation.ScreenPosition;
+import net.minecraft.client.gui.screens.inventory.AbstractRecipeBookScreen;
 import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.inventory.ContainerInput;
-import net.minecraft.world.inventory.Slot;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -25,110 +19,52 @@ import org.jetbrains.annotations.NotNull;
  * 
  * @author Daedalus4096
  */
-public class ConcocterScreen extends AbstractContainerScreenPM<ConcocterMenu> implements ArcaneRecipeUpdateListener {
-    protected static final Logger LOGGER = LogManager.getLogger();
+public class ConcocterScreen extends AbstractRecipeBookScreen<ConcocterMenu> {
     protected static final Identifier TEXTURE = ResourceUtils.loc("textures/gui/concocter.png");
     protected static final Identifier PROGRESS_SPRITE = ResourceUtils.loc("progress_arrow");
 
-    protected final ArcaneRecipeBookComponent recipeBookComponent = new ArcaneRecipeBookComponent();
-    protected boolean widthTooNarrow;
     protected ManaGaugeWidget manaGauge;
 
     public ConcocterScreen(ConcocterMenu screenMenu, Inventory inv, Component titleIn) {
-        super(screenMenu, inv, titleIn);
+        super(screenMenu, new ConcocterRecipeBookComponent(screenMenu), inv, titleIn);
         this.titleLabelX = 27;
         this.inventoryLabelX = 27;
     }
 
     @Override
-    public void extractRenderState(@NotNull GuiGraphicsExtractor guiGraphics, int mouseX, int mouseY, float partialTicks) {
-        this.manaGauge.setCurrentMana(this.menu.getCurrentMana());
-        this.manaGauge.setMaxMana(this.menu.getMaxMana());
-        if (this.recipeBookComponent.isVisible() && this.widthTooNarrow) {
-            this.extractBackground(guiGraphics, mouseX, mouseY, partialTicks);
-        } else {
-            super.extractContents(guiGraphics, mouseX, mouseY, partialTicks);
-        }
-        guiGraphics.nextStratum();
-        this.recipeBookComponent.render(guiGraphics, mouseX, mouseY, partialTicks);
-        this.recipeBookComponent.renderGhostRecipe(guiGraphics, this.leftPos, this.topPos, true, partialTicks); // FIXME Is this still a thing?
-        guiGraphics.nextStratum();
-        this.extractCarriedItem(guiGraphics, mouseX, mouseY);
-        this.extractSnapbackItem(guiGraphics);
-        this.extractTooltip(guiGraphics, mouseX, mouseY);
-        this.recipeBookComponent.renderTooltip(guiGraphics, this.leftPos, this.topPos, mouseX, mouseY); // FIXME Conform to new naming scheme
+    @NotNull
+    protected ScreenPosition getRecipeBookButtonPosition() {
+        return new ScreenPosition(this.leftPos + 105, this.topPos + 52);
     }
-    
+
     @Override
     protected void init() {
         super.init();
-        this.widthTooNarrow = this.width < 379;
-        this.recipeBookComponent.init(this.width, this.height, this.minecraft, this.widthTooNarrow, false, this.menu);
-        this.leftPos = this.recipeBookComponent.updateScreenPosition(this.width, this.imageWidth);
-
+        this.titleLabelX = 27;
+        this.inventoryLabelX = 27;
         this.manaGauge = this.addRenderableWidget(new ManaGaugeWidget(this.leftPos + 10, this.topPos + 6, Sources.INFERNAL, this.menu.getCurrentMana(), this.menu.getMaxMana()));
-        
-        // Add arcane recipe book button
-        this.addRenderableWidget(new ImageButton(this.leftPos + 105, this.topPos + 52, 20, 18, RecipeBookComponent.RECIPE_BUTTON_SPRITES, (button) -> {
-            this.recipeBookComponent.toggleVisibility();
-            this.leftPos = this.recipeBookComponent.updateScreenPosition(this.width, this.imageWidth);
-            ((ImageButton)button).setPosition(this.leftPos + 105, this.topPos + 52);
-            this.manaGauge.setPosition(this.leftPos + 10, this.topPos + 6);
-        }));
-        this.addWidget(this.recipeBookComponent);
-        this.setInitialFocus(this.recipeBookComponent);
     }
 
     @Override
-    protected void containerTick() {
-        super.containerTick();
-        this.recipeBookComponent.tick();
+    protected void onRecipeBookButtonClick() {
+        super.onRecipeBookButtonClick();
+        this.manaGauge.setPosition(this.leftPos + 10, this.topPos + 6);
     }
 
     @Override
     public void extractBackground(@NotNull GuiGraphicsExtractor guiGraphics, int x, int y, float partialTicks) {
         // Render background texture
         guiGraphics.blit(RenderPipelines.GUI_TEXTURED, TEXTURE, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight, 256, 256);
-        
+
         // Animate spin progress indicator
         int cook = this.menu.getCookProgressionScaled();
         guiGraphics.blitSprite(RenderPipelines.GUI_TEXTURED, PROGRESS_SPRITE, 24, 16, 0, 0, this.leftPos + 103, this.topPos + 34, cook, 16);
     }
 
     @Override
-    protected boolean isHovering(int p_97768_, int p_97769_, int p_97770_, int p_97771_, double p_97772_, double p_97773_) {
-        return (!this.widthTooNarrow || !this.recipeBookComponent.isVisible()) && super.isHovering(p_97768_, p_97769_, p_97770_, p_97771_, p_97772_, p_97773_);
-    }
-
-    @Override
-    public boolean mouseClicked(@NotNull MouseButtonEvent event, boolean doubleClick) {
-        if (this.recipeBookComponent.mouseClicked(mouseX, mouseY, buttonIndex)) {
-            this.setFocused(this.recipeBookComponent);
-            return true;
-        } else {
-            return this.widthTooNarrow && this.recipeBookComponent.isVisible() ? true : super.mouseClicked(event, doubleClick);
-        }
-    }
-
-    @Override
-    protected boolean hasClickedOutside(double mouseX, double mouseY, int leftPos, int topPos) {
-        boolean flag = mouseX < (double)leftPos || mouseY < (double)topPos || mouseX >= (double)(leftPos + this.imageWidth) || mouseY >= (double)(topPos + this.imageHeight);
-        return this.recipeBookComponent.hasClickedOutside(mouseX, mouseY, this.leftPos, this.topPos, this.imageWidth, this.imageHeight) && flag;
-    }
-
-    @Override
-    protected void slotClicked(@NotNull Slot slot, int slotId, int mouseButton, @NotNull ContainerInput type) {
-        super.slotClicked(slot, slotId, mouseButton, type);
-        this.recipeBookComponent.slotClicked(slot);
-    }
-
-    @Override
-    public void recipesUpdated() {
-        this.recipeBookComponent.recipesUpdated();
-    }
-
-    @Override
-    public ArcaneRecipeBookComponent getRecipeBookComponent() {
-        return this.recipeBookComponent;
+    public void extractRenderState(@NotNull GuiGraphicsExtractor guiGraphics, int mouseX, int mouseY, float partialTicks) {
+        super.extractRenderState(guiGraphics, mouseX, mouseY, partialTicks);
+        this.manaGauge.setCurrentMana(this.menu.getCurrentMana());
+        this.manaGauge.setMaxMana(this.menu.getMaxMana());
     }
 }
---
+++ common/src/main/java/com/verdantartifice/primalmagick/client/gui/recipe_book/ConcocterRecipeBookComponent.java
@@ -0,0 +1,85 @@
+package com.verdantartifice.primalmagick.client.gui.recipe_book;
+
+import com.verdantartifice.primalmagick.common.concoctions.ConcoctionType;
+import com.verdantartifice.primalmagick.common.concoctions.ConcoctionUtils;
+import com.verdantartifice.primalmagick.common.crafting.display.ConcoctingRecipeDisplay;
+import com.verdantartifice.primalmagick.common.crafting.recipe_book.RecipeBookCategoriesPM;
+import com.verdantartifice.primalmagick.common.menus.ConcocterMenu;
+import net.minecraft.client.gui.components.WidgetSprites;
+import net.minecraft.client.gui.screens.recipebook.GhostSlots;
+import net.minecraft.client.gui.screens.recipebook.RecipeBookComponent;
+import net.minecraft.client.gui.screens.recipebook.RecipeCollection;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
+import net.minecraft.util.context.ContextMap;
+import net.minecraft.world.entity.player.StackedItemContents;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.Potions;
+import net.minecraft.world.item.crafting.display.RecipeDisplay;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Recipe book component for the concocter GUI.
+ *
+ * @author Daedalus4096
+ */
+public class ConcocterRecipeBookComponent extends RecipeBookComponent<ConcocterMenu> {
+    private static final WidgetSprites FILTER_BUTTON_SPRITES = new WidgetSprites(
+            Identifier.withDefaultNamespace("recipe_book/filter_enabled"),
+            Identifier.withDefaultNamespace("recipe_book/filter_disabled"),
+            Identifier.withDefaultNamespace("recipe_book/filter_enabled_highlighted"),
+            Identifier.withDefaultNamespace("recipe_book/filter_disabled_highlighted")
+    );
+    private static final Component ONLY_CRAFTABLES_TOOLTIP = Component.translatable("gui.recipebook.toggleRecipes.craftable");
+    private static final List<TabInfo> TABS = List.of(
+            new RecipeBookComponent.TabInfo(Items.COMPASS, RecipeBookCategoriesPM.SEARCH_CONCOCTER.get()),
+            new RecipeBookComponent.TabInfo(ConcoctionUtils.newConcoction(Potions.REGENERATION, ConcoctionType.TINCTURE), Optional.empty(), RecipeBookCategoriesPM.CONCOCTER_DRINKABLE.get()),
+            new RecipeBookComponent.TabInfo(ConcoctionUtils.newBomb(Potions.POISON), Optional.empty(), RecipeBookCategoriesPM.CONCOCTER_BOMB.get())
+    );
+
+    public ConcocterRecipeBookComponent(ConcocterMenu menu) {
+        super(menu, TABS);
+    }
+
+    private boolean canDisplay(RecipeDisplay display) {
+        return display instanceof ConcoctingRecipeDisplay concocting && (this.menu.getGridWidth() * this.menu.getGridHeight() >= concocting.ingredients().size());
+    }
+
+    @Override
+    @NotNull
+    protected WidgetSprites getFilterButtonTextures() {
+        return FILTER_BUTTON_SPRITES;
+    }
+
+    @Override
+    protected boolean isCraftingSlot(Slot slot) {
+        return this.menu.getResultSlot() == slot || this.menu.getInputGridSlots().contains(slot);
+    }
+
+    @Override
+    protected void selectMatchingRecipes(RecipeCollection recipeCollection, StackedItemContents stackedItemContents) {
+        recipeCollection.selectRecipes(stackedItemContents, this::canDisplay);
+    }
+
+    @Override
+    @NotNull
+    protected Component getRecipeFilterName() {
+        return ONLY_CRAFTABLES_TOOLTIP;
+    }
+
+    @Override
+    protected void fillGhostRecipe(GhostSlots ghostSlots, RecipeDisplay recipeDisplay, ContextMap contextMap) {
+        ghostSlots.setResult(this.menu.getResultSlot(), contextMap, recipeDisplay.result());
+        if (recipeDisplay instanceof ConcoctingRecipeDisplay concoctingRecipeDisplay) {
+            List<Slot> inputSlots = this.menu.getInputGridSlots();
+            int slotCount = Math.min(concoctingRecipeDisplay.ingredients().size(), inputSlots.size());
+            for (int i = 0; i < slotCount; i++) {
+                ghostSlots.setInput(inputSlots.get(i), contextMap, concoctingRecipeDisplay.ingredients().get(i));
+            }
+        }
+    }
+}
