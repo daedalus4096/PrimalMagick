04fc1a8d09261afcf889144671bb386ebb8fef84
Michael Bunting <michael.a.bunting@gmail.com>
2026-07-27 16:22:43 -0700

    Fix errors in concocting recipe builder

--- common/src/main/java/com/verdantartifice/primalmagick/datagen/recipes/ConcoctingRecipeBuilder.java
+++ common/src/main/java/com/verdantartifice/primalmagick/datagen/recipes/ConcoctingRecipeBuilder.java
@@ -3,6 +3,8 @@ package com.verdantartifice.primalmagick.datagen.recipes;
 import com.verdantartifice.primalmagick.common.concoctions.ConcoctionType;
 import com.verdantartifice.primalmagick.common.concoctions.ConcoctionUtils;
 import com.verdantartifice.primalmagick.common.crafting.ConcoctingRecipe;
+import com.verdantartifice.primalmagick.common.crafting.IConcoctingRecipe;
+import com.verdantartifice.primalmagick.common.crafting.recipe_book.ConcoctingBookCategory;
 import com.verdantartifice.primalmagick.common.research.ResearchEntry;
 import com.verdantartifice.primalmagick.common.research.keys.ResearchEntryKey;
 import com.verdantartifice.primalmagick.common.research.keys.ResearchStageKey;
@@ -11,18 +13,22 @@ import com.verdantartifice.primalmagick.common.research.requirements.AndRequirem
 import com.verdantartifice.primalmagick.common.research.requirements.ResearchRequirement;
 import com.verdantartifice.primalmagick.common.sources.SourceList;
 import com.verdantartifice.primalmagick.common.sources.Sources;
-import com.verdantartifice.primalmagick.common.util.ResourceUtils;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderGetter;
 import net.minecraft.core.NonNullList;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.recipes.RecipeOutput;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemStackTemplate;
 import net.minecraft.world.item.alchemy.PotionContents;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.level.ItemLike;
 
 import java.util.ArrayList;
@@ -36,19 +42,23 @@ import java.util.Optional;
  * @author Daedalus4096
  */
 public class ConcoctingRecipeBuilder {
+    protected final HolderGetter<Item> itemGetter;
     protected final ItemStack result;
     protected final NonNullList<Ingredient> ingredients = NonNullList.create();
+    protected boolean showNotification = true;
+    protected ConcoctingBookCategory category = ConcoctingBookCategory.DRINKABLE;
     protected String group;
     protected boolean useDefaultGroup = false;
     protected final List<AbstractRequirement<?>> requirements = new ArrayList<>();
     protected SourceList manaCosts;
 
-    protected ConcoctingRecipeBuilder(ItemStack result) {
+    protected ConcoctingRecipeBuilder(HolderGetter<Item> itemGetter, ItemStack result) {
+        this.itemGetter = itemGetter;
         this.result = result.copy();
     }
     
-    public static ConcoctingRecipeBuilder concoctingRecipe(ItemStack result) {
-        return new ConcoctingRecipeBuilder(result);
+    public static ConcoctingRecipeBuilder concoctingRecipe(HolderGetter<Item> itemGetter, ItemStack result) {
+        return new ConcoctingRecipeBuilder(itemGetter, result);
     }
     
     public ConcoctingRecipeBuilder addIngredient(Ingredient ingredient, int quantity) {
@@ -71,9 +81,19 @@ public class ConcoctingRecipeBuilder {
     }
     
     public ConcoctingRecipeBuilder addIngredient(TagKey<Item> tag) {
-        return this.addIngredient(Ingredient.of(tag));
+        return this.addIngredient(Ingredient.of(this.itemGetter.getOrThrow(tag)));
     }
-    
+
+    public ConcoctingRecipeBuilder showNotification(boolean show) {
+        this.showNotification = show;
+        return this;
+    }
+
+    public ConcoctingRecipeBuilder category(ConcoctingBookCategory category) {
+        this.category = category;
+        return this;
+    }
+
     public ConcoctingRecipeBuilder setGroup(String group) {
         this.group = group;
         return this;
@@ -114,13 +134,20 @@ public class ConcoctingRecipeBuilder {
         if (this.requirements.isEmpty()) {
             return Optional.empty();
         } else if (this.requirements.size() == 1) {
-            return Optional.of(this.requirements.get(0));
+            return Optional.of(this.requirements.getFirst());
         } else {
             return Optional.of(new AndRequirement(this.requirements));
         }
     }
     
-    protected void validate(Identifier id) {
+    protected void validate(ResourceKey<Recipe<?>> id) {
+        PotionContents contents = this.result.get(DataComponents.POTION_CONTENTS);
+        if (contents == null || contents.potion().isEmpty()) {
+            throw new IllegalStateException("No potion effect defined for result of concocting recipe with output " + this.result.getHoverName().getString());
+        }
+        if (this.category == null) {
+            throw new IllegalStateException("Null category specified for concocting recipe " + id + "!");
+        }
         if (this.ingredients.isEmpty()) {
             throw new IllegalStateException("No ingredients defined for concocting recipe " + id + "!");
         }
@@ -129,22 +156,33 @@ public class ConcoctingRecipeBuilder {
         }
     }
     
-    public void build(RecipeOutput output, Identifier id) {
+    public void build(RecipeOutput output, ResourceKey<Recipe<?>> id) {
         this.validate(id);
-        String groupStr = this.useDefaultGroup ? BuiltInRegistries.POTION.getKey(this.result.get(DataComponents.POTION_CONTENTS).potion().get().value()).getPath() : this.group;
-        ConcoctingRecipe recipe = new ConcoctingRecipe(Objects.requireNonNullElse(groupStr, ""), this.result, this.ingredients, this.getFinalRequirement(), Objects.requireNonNullElse(this.manaCosts, SourceList.EMPTY));
+        String groupStr = this.getResultPotionKey().map(Identifier::getPath).orElse(this.group);
+        ConcoctingRecipe recipe = new ConcoctingRecipe(
+                new Recipe.CommonInfo(this.showNotification),
+                new IConcoctingRecipe.ConcoctingCraftingBookInfo(
+                        Objects.requireNonNullElse(this.category, ConcoctingBookCategory.DRINKABLE),
+                        Objects.requireNonNullElse(groupStr, "")),
+                ItemStackTemplate.fromNonEmptyStack(this.result),
+                this.ingredients,
+                this.getFinalRequirement(),
+                Objects.requireNonNullElse(this.manaCosts, SourceList.EMPTY));
         output.accept(id, recipe, null);
     }
     
     public void build(RecipeOutput output) {
-        PotionContents contents = this.result.get(DataComponents.POTION_CONTENTS);
-        if (contents == null || contents.potion().isEmpty()) {
-            throw new IllegalStateException("No potion effect defined for result of concocting recipe with output " + this.result.getHoverName().getString());
-        }
         ConcoctionType type = ConcoctionUtils.getConcoctionType(this.result);
-        if (type == null) {
-            throw new IllegalStateException("Output is not a concoction for concocting recipe with output " + this.result.getHoverName().getString());
+        Identifier recipeId = this.getResultPotionKey().map(id -> id.withSuffix("_" + type.getSerializedName())).orElseThrow(() -> new IllegalStateException("Cannot determine concoction ID in recipe builder!"));
+        this.build(output, ResourceKey.create(Registries.RECIPE, recipeId));
+    }
+
+    private Optional<Identifier> getResultPotionKey() {
+        PotionContents contents = this.result.get(DataComponents.POTION_CONTENTS);
+        if (contents == null) {
+            return Optional.empty();
+        } else {
+            return contents.potion().map(Holder::value).map(BuiltInRegistries.POTION::getKey);
         }
-        this.build(output, ResourceUtils.loc(BuiltInRegistries.POTION.getKey(contents.potion().get().value()).getPath() + "_" + type.getSerializedName()));
     }
 }
