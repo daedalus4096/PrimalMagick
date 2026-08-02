3266097c155711946a11c4df29d2c949271ed445
Michael Bunting <michael.a.bunting@gmail.com>
2026-08-01 22:07:44 -0700

    Fix friendly witch renderer

--- common/src/main/java/com/verdantartifice/primalmagick/client/renderers/entity/FriendlyWitchRenderer.java
+++ common/src/main/java/com/verdantartifice/primalmagick/client/renderers/entity/FriendlyWitchRenderer.java
@@ -1,42 +1,50 @@
 package com.verdantartifice.primalmagick.client.renderers.entity;
 
-import com.mojang.blaze3d.vertex.PoseStack;
 import com.verdantartifice.primalmagick.common.entities.misc.FriendlyWitchEntity;
-import net.minecraft.client.model.WitchModel;
 import net.minecraft.client.model.geom.ModelLayers;
-import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.model.monster.witch.WitchModel;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.client.renderer.entity.layers.WitchItemLayer;
+import net.minecraft.client.renderer.entity.state.HoldingEntityRenderState;
+import net.minecraft.client.renderer.entity.state.WitchRenderState;
 import net.minecraft.resources.Identifier;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Entity renderer for a friendly witch.
  * 
  * @author Daedalus4096
  */
-public class FriendlyWitchRenderer extends MobRenderer<FriendlyWitchEntity, WitchModel<FriendlyWitchEntity>> {
+public class FriendlyWitchRenderer extends MobRenderer<FriendlyWitchEntity, WitchRenderState, WitchModel> {
     private static final Identifier WITCH_LOCATION = Identifier.withDefaultNamespace("textures/entity/witch.png");
-    private static final float SCALE = 0.9375F;
 
     public FriendlyWitchRenderer(EntityRendererProvider.Context context) {
-        super(context, new WitchModel<>(context.bakeLayer(ModelLayers.WITCH)), 0.5F);
-        this.addLayer(new WitchItemLayer<>(this, context.getItemInHandRenderer()));
+        super(context, new WitchModel(context.bakeLayer(ModelLayers.WITCH)), 0.5F);
+        this.addLayer(new WitchItemLayer(this));
     }
 
     @Override
-    public Identifier getTextureLocation(FriendlyWitchEntity entity) {
+    @NotNull
+    public WitchRenderState createRenderState() {
+        return new WitchRenderState();
+    }
+
+    @Override
+    @NotNull
+    public Identifier getTextureLocation(@NotNull WitchRenderState witchRenderState) {
         return WITCH_LOCATION;
     }
 
     @Override
-    public void render(FriendlyWitchEntity entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight) {
-        this.model.setHoldingItem(!entity.getMainHandItem().isEmpty());
-        super.render(entity, entityYaw, partialTicks, poseStack, buffer, packedLight);
-    }
-
-    @Override
-    protected void scale(FriendlyWitchEntity entity, PoseStack poseStack, float partialTicks) {
-        poseStack.scale(SCALE, SCALE, SCALE);
+    public void extractRenderState(@NotNull FriendlyWitchEntity entity, @NotNull WitchRenderState state, float partialTicks) {
+        super.extractRenderState(entity, state, partialTicks);
+        HoldingEntityRenderState.extractHoldingEntityRenderState(entity, state, this.itemModelResolver);
+        state.entityId = entity.getId();
+        ItemStack mainHandItem = entity.getMainHandItem();
+        state.isHoldingItem = !mainHandItem.isEmpty();
+        state.isHoldingPotion = mainHandItem.is(Items.POTION);
     }
 }
